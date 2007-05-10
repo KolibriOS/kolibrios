e9dcca24f464312c5ea40a0c0f86bb72bdd99b11
kastigar <kastigar@kolibrios.org>
2007-05-10 14:43:38 +0000

    * update sysfunc.txt files(37.7, 37.2)
    git-svn-id: svn://kolibrios.org@486 a494cfbc-eb01-0410-851d-a64ba20cac60

--- kernel/trunk/drivers/ps2mouse.asm
+++ kernel/trunk/drivers/ps2mouse.asm
@@ -1,10 +1,3 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                                              ;;
-;; Copyright (C) KolibriOS team 2004-2007. All rights reserved. ;;
-;; Distributed under terms of the GNU General Public License    ;;
-;;                                                              ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 format MS COFF
 
 include 'proc32.inc'
