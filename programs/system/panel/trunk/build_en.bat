71e4e59cf3d89d50018e56700a48c59cc21933be
Rus <Rus@kolibrios.org>
2007-08-02 19:03:24 +0000

    @Panel     - added close context menu
    git-svn-id: svn://kolibrios.org@601 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/system/panel/trunk/build_en.bat
+++ programs/system/panel/trunk/build_en.bat
@@ -1,4 +1,3 @@
-@erase lang.inc
 @echo lang fix en >lang.inc
 @fasm @panel.asm @panel
 @erase lang.inc
--- programs/system/panel/trunk/build_ru.bat
+++ programs/system/panel/trunk/build_ru.bat
@@ -1,4 +1,3 @@
-@erase lang.inc
 @echo lang fix ru >lang.inc
 @fasm @panel.asm @panel
 @erase lang.inc
