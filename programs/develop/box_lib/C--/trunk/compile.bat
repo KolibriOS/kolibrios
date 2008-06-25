3e4779ce2094b136d99fd18d781fa95a17381e57
Alexey Teplov ( <Lrz) 200_u_200@mail.ru>
2008-06-25 12:01:00 +0000

    add files :)
    git-svn-id: svn://kolibrios.org@806 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ programs/develop/box_lib/C--/trunk/compile.bat
@@ -0,0 +1,5 @@
+C-- EditBox2.c--
+del EditBox2.kex
+rename EditBox2 EditBox2.kex
+rename EditBox2.com EditBox2.kex
+pause
---
+++ programs/develop/box_lib/trunk/run.bat
@@ -0,0 +1,4 @@
+..\FASM\fasm.exe box_lib.asm
+..\FASM\fasm.exe editbox_ex.asm editbox_ex.kex
+REM ..\FASM\kpack.exe editbox_ex
+..\emul\klbrinwin.exe editbox_ex.kex
