0d893d8b841457ce5aedc96ae32e8573b542f196
Sergey Semyonov (Serge) <ion2@mail.ru>
2008-12-12 12:33:09 +0000

    agp_generic_enable()
    git-svn-id: svn://kolibrios.org@953 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/system/drivers/agp/types.h
+++ programs/system/drivers/agp/types.h
@@ -18,6 +18,24 @@ typedef  unsigned int Bool;
 #define  TRUE  (Bool)1
 #define  FALSE (Bool)0
 
+/*
+ * min()/max() macros that also do
+ * strict type-checking.. See the
+ * "unnecessary" pointer comparison.
+ */
+#define min(x,y) ({ \
+	typeof(x) _x = (x);	\
+	typeof(y) _y = (y);	\
+	(void) (&_x == &_y);		\
+	_x < _y ? _x : _y; })
+
+#define max(x,y) ({ \
+	typeof(x) _x = (x);	\
+	typeof(y) _y = (y);	\
+	(void) (&_x == &_y);		\
+	_x > _y ? _x : _y; })
+
+
 #define min_t(type,x,y) \
 	({ type __x = (x); type __y = (y); __x < __y ? __x: __y; })
 #define max_t(type,x,y) \
