29efd28b74cd9428e350117a1be70d48f805ca17
Marat Zakiyanov (Mario79) <mario79@bk.ru>
2009-02-23 12:26:32 +0000

    1) Example for Box_lib: ScrollBar, MenuBar, DinamicButton 2) Small change of ScrollBar
    git-svn-id: svn://kolibrios.org@1041 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/libraries/box_lib/trunk/run.bat
+++ programs/develop/libraries/box_lib/trunk/run.bat
@@ -1,4 +1,6 @@
 @fasm.exe  -m 16384 box_lib.asm
 @fasm.exe -m 16384 editbox_ex.asm editbox_ex.kex
+@fasm.exe -m 16384 ctrldemo.asm ctrldemo.kex
 REM ..\FASM\kpack.exe editbox_ex
-@klbrinwin.exe editbox_ex.kex
+rem @klbrinwin.exe editbox_ex.kex
+pause
