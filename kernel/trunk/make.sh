0d2b81dd891d96b1c0e79a3d3a8218222650f3a9
Evgeny Grechnikov (Diamond) <diamondz@land.ru>
2010-07-14 17:17:52 +0000

    make.sh from http://board.kolibrios.org/viewtopic.php?f=1&t=1459
    git-svn-id: svn://kolibrios.org@1516 a494cfbc-eb01-0410-851d-a64ba20cac60

--- kernel/trunk/make.sh
+++ kernel/trunk/make.sh
@@ -4,30 +4,48 @@
 
 CLANG=$1;
 
+# set debug=true to print executed bash commands
+debug=true
+
+outDir=bin
+outFileName=kernel.mnt
+outFile=$outDir/$outFileName
+
 usage()
 {
-	echo "Usage: make.sh [en|ru|ge|et]"
-	exit 1
+   echo "Usage: make.sh [en|ru|ge|et]"
+   exit 1
 }
 
 compile()
 {
-	fasm -m 65536 kernel.asm bin/kernel.mnt
-	rm -f lang.inc
-	exit 0
+   if [ -d "$outDir" ]; then
+      $debug && echo "rm -f $outFile"
+            rm -f $outFile
+   else
+      $debug && echo "mkdir $outDir"
+            mkdir $outDir
+   fi
+
+   $debug && echo "fasm -m 65536 kernel.asm $outFile"
+         fasm -m 65536 kernel.asm $outFile
+
+   $debug && echo "rm -f lang.inc"
+         rm -f lang.inc
+
+   $debug && echo "exit 0"
+         exit 0
 }
 
-
 if [ ! $CLANG ] ; then
-	usage
+   usage
 fi
 
 for i in "en" "ru" "ge" "et"; do
-	if [ $i == $CLANG ] ; then
-		echo "lang fix $i" > lang.inc
-		compile
-	fi
+   if [ $i == $CLANG ] ; then
+      $debug && echo "echo \"lang fix \$i\" > lang.inc"
+            echo "lang fix $i" > lang.inc
+      compile
+   fi
 done
 usage
-	
-	
