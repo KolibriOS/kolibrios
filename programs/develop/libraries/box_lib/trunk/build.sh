13628083171eb36412d2481cc820533cf6d42925
Alexey Teplov ( <Lrz) 200_u_200@mail.ru>
2010-09-14 03:54:21 +0000

    Small change
    git-svn-id: svn://kolibrios.org@1618 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/libraries/box_lib/trunk/build.sh
+++ programs/develop/libraries/box_lib/trunk/build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 # This script does for Linux the same as build.bat for DOS,
 # it compiles the current KolibriOS applications
+OutDir=bin
+FileName=box_lib.obj
+OutFile=$OutDir/$FileName
 
-	mkdir bin
-	fasm -m 16384 box_lib.asm ./bin/box_lib.obj
+	if [ -d "$OutDir" ]; then
+	    if [ -e "$OutFile" ]; then
+		echo " >>>> rm -f $OutFile delete old version"
+		rm -f $OutFile
+	    fi
+	else
+	    echo "mkdir $OutDir"
+	    mkdir $OutDir
+	fi
+	fasm -m 16384 box_lib.asm $OutFile
 	exit 0
 
 
