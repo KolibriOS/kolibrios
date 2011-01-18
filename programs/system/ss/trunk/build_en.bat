af2042f7e85922f7a4781bcdefc0af2b0800af10
Marat Zakiyanov (Mario79) <mario79@bk.ru>
2011-01-18 22:02:28 +0000

    interim audit for @SS N2
    git-svn-id: svn://kolibrios.org@1746 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ programs/system/ss/trunk/build_en.bat
@@ -0,0 +1,5 @@
+@erase lang.inc
+@echo lang fix en >lang.inc
+@fasm @ss.asm @ss
+@erase lang.inc
+@pause
---
+++ programs/system/ss/trunk/build_ru.bat
@@ -0,0 +1,5 @@
+@erase lang.inc
+@echo lang fix ru >lang.inc
+@fasm @ss.asm @ss
+@erase lang.inc
+@pause
