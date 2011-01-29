1ba8b5fb96094a58883ea2a14ee317c654c2e632
Yogev Ezra <pr@kolibrios.org>
2011-01-29 22:35:52 +0000

    Added source code of games 'kox', 'lights', 'sw'. Changed MakeFile to have them compile from sources. Removed pre-compiled versions.
    git-svn-id: svn://kolibrios.org@1803 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ programs/games/kox/trunk/kox.asm
@@ -0,0 +1,20 @@
+;************************************************
+;*  X-0-X   / Kolibri OS /
+;************************** 17/10/2007 **********
+include 'main.inc'
+include 'data.inc'
+include 'macs.inc'
+
+  BeginProgram
+
+   SetEventMask 100111b
+   call rndseed
+
+  main_loop
+
+   Subroutines
+
+  DATA_SECTION
+
+  EndProgram
+
---
+++ programs/games/lights/trunk/lights.asm
@@ -0,0 +1,15 @@
+;#**************************
+;#*        LIGHTS          *  Kolibri OS
+;#**************************  15/10/2007
+include 'main.inc'
+include 'data.inc'
+include 'macs.inc'
+
+  BeginProgram
+
+  main_loop
+
+  DATA_SECTION
+
+  EndProgram
+
