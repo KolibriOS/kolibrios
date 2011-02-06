0be14cc9281ba14d2e793317c61f6a555ad7a3f3
Marat Zakiyanov (Mario79) <mario79@bk.ru>
2011-02-06 13:55:34 +0000

    KFM 1) Added license text to kfm.asm 2) Compilation with default system macros.inc 3) Add build.sh for Linux 4) Corrected kfm.ini
    git-svn-id: svn://kolibrios.org@1851 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/fs/kfm/trunk/kfm.asm
+++ programs/fs/kfm/trunk/kfm.asm
@@ -1,7 +1,33 @@
-;**************************************************************
+;*****************************************************************************
 ; KFM - Kolibri File Manager
-;**************************************************************
+; Copyright (c) 2006 - 2010, Marat Zakiyanov aka Mario79, aka Mario
+; All rights reserved.
+;
+; Redistribution and use in source and binary forms, with or without
+; modification, are permitted provided that the following conditions are met:
+;        * Redistributions of source code must retain the above copyright
+;          notice, this list of conditions and the following disclaimer.
+;        * Redistributions in binary form must reproduce the above copyright
+;          notice, this list of conditions and the following disclaimer in the
+;          documentation and/or other materials provided with the distribution.
+;        * Neither the name of the <organization> nor the
+;          names of its contributors may be used to endorse or promote products
+;          derived from this software without specific prior written permission.
+;
+; THIS SOFTWARE IS PROVIDED BY Marat Zakiyanov ''AS IS'' AND ANY
+; EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+; DISCLAIMED. IN NO EVENT SHALL <copyright holder> BE LIABLE FOR ANY
+; DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+; (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+; ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;*****************************************************************************
 use32
+org	0x0
+
     db	  'MENUET01'
     dd	  0x01
     dd	  START
@@ -13,7 +39,10 @@ use32
 
 ;include   'lang.inc'
 ;include   'kglobals.inc'
-include   'macros.inc'
+;include   'macros.inc'
+include '../../../macros.inc'
+include '../../../config.inc'		;for nightbuild
+
 include   'editbox.inc'
 ;use_edit_box
 use_edit_box procinfo
@@ -118,7 +147,7 @@ draw_window:
 ;    mov   ecx,[temp_esi]
     test  [window_status],10b
     jnz   @f
-    mcall 71, 1 , header
+    mcall 71, 1 , header_text
     ; create_dir_name
     ; start_parameter
     ; file_name
