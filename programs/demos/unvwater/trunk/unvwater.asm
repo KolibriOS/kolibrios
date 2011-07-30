c908564ea711f9b6237660ddd559426fc8c3acf5
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2011-07-30 14:33:03 +0000

    unvwater: right version
    git-svn-id: svn://kolibrios.org@2030 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/demos/unvwater/trunk/unvwater.asm
+++ programs/demos/unvwater/trunk/unvwater.asm
@@ -108,7 +108,8 @@ button:
 	jmp	MAIN
 
 draw_window:
-	start_draw_window 100,70,SCREEN_WIDTH+9,SCREEN_HEIGHT+4,0x74224466,labelt;, 14;labellen-labelt
+	start_draw_window 100,70,SCREEN_WIDTH+9,SCREEN_HEIGHT+4,0x54224466,labelt;, 14;labellen-labelt
+	mov	dword [skin_h], eax
 	end_draw_window
 ret
 
@@ -130,8 +131,8 @@ copy_buffer_to_video:
 	mov	ebx, dword [delay]
 	int	0x40
 
-	mov	eax, 0 ;dword [skin_h]
-	lea	edx, [0*65536+eax]
+	mov	eax, dword [skin_h]
+	lea	edx, [5*65536+eax]
 
 	mov	eax, 65
 	mov	ebx, img
