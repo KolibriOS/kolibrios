8cdca726a042d20813a64ce8a38152e2eaba74b3
Marat Zakiyanov (Mario79) <mario79@bk.ru>
2012-03-27 09:57:38 +0000

    @RB - some optimisations and code refactoring
    git-svn-id: svn://kolibrios.org@2510 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/system/rb/trunk/build_en.bat
+++ programs/system/rb/trunk/build_en.bat
@@ -1,5 +1,6 @@
 @erase lang.inc
 @echo lang fix en >lang.inc
 @fasm @rb.asm @rb
+@kpack @rb
 @erase lang.inc
 @pause
--- programs/system/rb/trunk/build_ru.bat
+++ programs/system/rb/trunk/build_ru.bat
@@ -1,5 +1,6 @@
 @erase lang.inc
 @echo lang fix ru >lang.inc
 @fasm @rb.asm @rb
+@kpack @rb
 @erase lang.inc
 @pause
