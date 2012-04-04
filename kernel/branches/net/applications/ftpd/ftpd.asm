742df45545fc6889dd049fc5768e99b9f8476a60
hidnplayr <hidnplayr@kolibrios.org>
2012-04-04 09:24:08 +0000

    Added some more commands to the new FTP Daemon (net branch)
    git-svn-id: svn://kolibrios.org@2560 a494cfbc-eb01-0410-851d-a64ba20cac60

--- kernel/branches/net/applications/ftpd/ftpd.asm
+++ kernel/branches/net/applications/ftpd/ftpd.asm
@@ -13,6 +13,22 @@ STATE_CONNECTED         = 1
 STATE_LOGIN             = 2
 STATE_ACTIVE            = 3
 
+TYPE_UNDEF              = 0
+
+TYPE_ASCII              = 00000100b
+TYPE_EBDIC              = 00001000b
+; subtypes for ascii & ebdic (np = default)
+TYPE_NP                 = 00000001b     ; non printable
+TYPE_TELNET             = 00000010b
+TYPE_ASA                = 00000011b
+
+TYPE_IMAGE              = 01000000b     ; binary data
+TYPE_LOCAL              = 10000000b     ; bits per byte must be specified
+                                        ; lower 4 bits will hold this value
+
+MODE_NOTREADY           = 0
+MODE_ACTIVE             = 1
+MODE_PASSIVE            = 2
 
 use32
         db      'MENUET01'      ; signature
@@ -52,17 +68,22 @@ start:
 ; initialize console
         push    1
         call    [con_start]
+
         push    title
-        push    25
-        push    80
-        push    25
-        push    80
+        push    -1
+        push    -1
+        push    -1
+        push    -1
         call    [con_init]
 
         mcall   40, 1 shl 7     ; we only want network events
 
+        invoke  ini.get_int, path, str_ftpd, str_port, 21
+        mov     [sockaddr1.port], ax
+
+        push    eax
         push    str1
-        call    [con_write_asciiz]
+        call    [con_printf]
 
         mcall   socket, AF_INET4, SOCK_STREAM, 0
         cmp     eax, -1
@@ -70,24 +91,31 @@ start:
 
         mov     [socketnum], eax
 
+        push    str2
+        call    [con_write_asciiz]
+
 ;;        mcall   setsockopt, [socketnum], SOL_SOCKET, SO_REUSEADDR, &yes,
 ;;        cmp     eax, -1
 ;;        je      opt_err
-
-        invoke  ini.get_int, path, str_ftpd, str_port, 21
-        mov     [sockaddr1.port], ax
 
         mcall   bind, [socketnum], sockaddr1, sockaddr1.length
         cmp     eax, -1
         je      bind_err
 
+        push    str2
+        call    [con_write_asciiz]
+
         invoke  ini.get_int, path, str_ftpd, str_conn, 1        ; Backlog (max connections)
         mov     edx, eax
+
+        push    str2
+        call    [con_write_asciiz]
+
         mcall   listen, [socketnum]
         cmp     eax, -1
         je      listen_err
 
-        push    str2
+        push    str2b
         call    [con_write_asciiz]
 
         mcall   10
@@ -108,8 +136,17 @@ start:
         je      .loop
         push    eax
 
+        mov     byte[buffer+eax], 0
+
+
+        pushd   0x0a
+        call    [con_set_flags]
+
         push    buffer
         call    [con_write_asciiz]
+
+        pushd   0x07
+        call    [con_set_flags]
 
         pop     ecx
         mov     esi, buffer
@@ -118,21 +155,37 @@ start:
         jmp     .loop
 
 acpt_err:
+
+        pushd   0x0c
+        call    [con_set_flags]
+
         push    str8
         call    [con_write_asciiz]
         jmp     done
 
 listen_err:
+
+        pushd   0x0c
+        call    [con_set_flags]
+
         push    str3
         call    [con_write_asciiz]
         jmp     done
 
 bind_err:
+
+        pushd   0x0c
+        call    [con_set_flags]
+
         push    str4
         call    [con_write_asciiz]
         jmp     done
 
 sock_err:
+
+        pushd   0x0c
+        call    [con_set_flags]
+
         push    str6
         call    [con_write_asciiz]
         jmp     done
@@ -147,15 +200,19 @@ exit:
 
 
 ; data
-title   db      'KolibriOS FTP daemon 0.1',0
-str1    db      'Opening socket',10, 0
-str2    db      'Listening for incoming connections...',10,0
+title   db      'KolibriOS FTP daemon 0.1', 0
+str1    db      'Starting FTP daemon on port %u', 0
+str2    db      '.', 0
+str2b   db      ' OK!',10,10,0
 str3    db      'Listen error',10,10,0
 str4    db      'Bind error',10,10,0
 str5    db      'Setsockopt error.',10,10,0
 str6    db      'Could not open socket',10,10,0
 str7    db      'Got data!',10,10,0
 str8    db      'Error accepting connection',10,10,0
+
+str_logged_in db 'Login ok',10,10,0
+str_pass_ok   db 'Password ok - Logged in',10,10,0
 
 filename db '.ini', 0
 str_port db 'port', 0
@@ -186,7 +243,8 @@ import  console,        \
         con_cls,        'con_cls',\
         con_printf,     'con_printf',\
         con_getch2,     'con_getch2',\
-        con_set_cursor_pos, 'con_set_cursor_pos'
+        con_set_cursor_pos, 'con_set_cursor_pos',\
+        con_set_flags,  'con_set_flags'
 
 import  libini,         \
         ini.get_str,    'ini_get_str',\
@@ -208,9 +266,22 @@ socketnum       dd ?
 ; thread specific data
 socketnum2      dd ?
 state           dd ?
+home_dir        rb 1024
+work_dir        rb 1024
+type            db ?
+mode            db ?    ; active/passive
 
-buffer          rb BUFFERSIZE
-.length = BUFFERSIZE
+datasocketnum   dd ?
+
+datasock:
+        dw AF_INET4
+.port   dw ?
+.ip     dd ?
+        rb 10
+.length = $ - datasock
+
+buffer  rb BUFFERSIZE
+.length = $ - buffer
 
 path    rb 1024
 mem:
