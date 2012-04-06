0d44338d5d4a62b3e5bf79439aec4e1eda15776b
hidnplayr <hidnplayr@kolibrios.org>
2012-04-06 18:37:00 +0000

    Added multithread-capabilities to FTPd (net branch)
    git-svn-id: svn://kolibrios.org@2578 a494cfbc-eb01-0410-851d-a64ba20cac60

--- kernel/branches/net/applications/ftpd/ftpd.asm
+++ kernel/branches/net/applications/ftpd/ftpd.asm
@@ -38,7 +38,7 @@ use32
         dd      i_end           ; initialized size
         dd      mem+0x1000      ; required memory
         dd      mem+0x1000      ; stack pointer
-        dd      0               ; parameters
+        dd      params          ; parameters
         dd      path            ; path
 
 include '../macros.inc'
@@ -58,13 +58,15 @@ start:
         test    eax, eax
         jnz     exit
 
+        mcall   68, 11                  ; init heap
+
 ; find path to main settings file
-        mov     edi, path      ; Calculate the length of zero-terminated string
+        mov     edi, path               ; Calculate the length of zero-terminated string
         xor     al , al
         mov     ecx, 1024
         repne   scasb
         dec     edi
-        mov     esi, filename
+        mov     esi, filename           ; append it with '.ini'
         movsd
         movsb
 
@@ -79,7 +81,7 @@ start:
         push    -1
         call    [con_init]
 
-        mcall   40, 1 shl 7     ; we only want network events
+        mcall   40, 1 shl 7             ; we only want network events
 
         invoke  ini.get_int, path, str_ftpd, str_port, 21
         mov     [sockaddr1.port], ax
@@ -121,85 +123,99 @@ start:
         push    str2b
         call    [con_write_asciiz]
 
-        mcall   10
+mainloop:
+        mcall   10                              ; Wait here for incoming connections on the base socket (socketnum)
 
-        mcall   accept, [socketnum], sockaddr1, sockaddr1.length
+        mcall   51, 1, threadstart, 0           ; Start a new thread for every incoming connection
+                                                ; NOTE: upon initialisation of the thread, stack will not be available!
+        jmp     mainloop
+
+threadstart:
+        mcall   68, 12, sizeof.thread_data      ; allocate the thread data struct
         cmp     eax, -1
-        je      acpt_err
+        je      exit
 
-        mov     [socketnum2], eax
+        lea     esp, [eax + thread_data.stack]  ; init stack
+        push    eax                             ; save pointer to thread_data on stack
 
-        mcall   send, [socketnum2], str220, str220.length, 0    ; send welcome string
+        mcall   40, 1 shl 7                     ; we only want network events for this thread
 
-  .loop:
+        push    str8
+        call    [con_write_asciiz]                                              ; print on the console that we have created the new thread successfully
+
+        mcall   accept, [socketnum], sockaddr1, sockaddr1.length                ; time to accept the awaiting connection..
+        cmp     eax, -1
+        je      thread_exit
+        mov     edx, [esp]                                                      ; pointer to thread_data
+        mov     [edx + thread_data.socketnum], eax
+
+        mcall   send, [edx + thread_data.socketnum], str220, str220.length, 0   ; send welcome string to the FTP client
+
+threadloop:
         mcall   10
 
-        cmp     [mode], MODE_PASSIVE_WAIT
+        mov     edx, [esp]                                                      ; pointer to thread_data
+
+        cmp     [edx + thread_data.mode], MODE_PASSIVE_WAIT
         jne     @f
-        mcall   accept, [passivesocknum], datasock, datasock.length
+        mov     ecx, [edx + thread_data.passivesocknum]
+        lea     edx, [edx + thread_data.datasock]
+        mov     esi, sizeof.thread_data.datasock
+        mcall   accept
+        mov     edx, [esp]                                                      ; pointer to thread_data
         cmp     eax, -1
         je      @f
-        mov     [datasocketnum], eax
-        mov     [mode], MODE_PASSIVE_OK
+        mov     [edx + thread_data.datasocketnum], eax
+        mov     [edx + thread_data.mode], MODE_PASSIVE_OK
 
         push    str_datasock
-        call    [con_write_asciiz]
+        call    [con_write_asciiz]                                              ; print on the console that the datasock is now ready
        @@:
 
-        mcall   recv, [socketnum2], buffer, buffer.length
-        cmp     eax, -1
-        je      .loop
-        or      eax, eax
-        jz      .loop
-        push    eax
+        mov     ecx, [edx + thread_data.socketnum]
+        lea     edx, [edx + thread_data.buffer]
+        mov     esi, sizeof.thread_data.buffer
+        mcall   recv
+        cmp     eax, -1                                                         ; error?
+        je      threadloop
+        or      eax, eax                                                        ; 0 bytes read?
+        jz      threadloop
+        push    eax                                                             ; save number of bytes read on stack
 
-        mov     byte[buffer+eax], 0
+        mov     edx, [esp+4]                                                    ; pointer to thread_data
+        mov     byte [edx + thread_data.buffer + eax], 0                        ; append received data with a 0 byte
 
-        pushd   0x0a
+        pushd   0x0a                                                            ; print received data to console (in green color)
         call    [con_set_flags]
-        push    buffer
+        lea     eax, [edx + thread_data.buffer]
+        push    eax
         call    [con_write_asciiz]
         pushd   0x07
         call    [con_set_flags]
 
-        pop     ecx
-        mov     esi, buffer
+        pop     ecx                                                             ; number of bytes read
+        lea     esi, [edx + thread_data.buffer]
         call    parse_cmd
 
-        jmp     .loop
-
-acpt_err:
-
-        pushd   0x0c
-        call    [con_set_flags]
-
-        push    str8
-        call    [con_write_asciiz]
-        jmp     done
+        jmp     threadloop
 
 listen_err:
-
         pushd   0x0c
         call    [con_set_flags]
-
         push    str3
         call    [con_write_asciiz]
         jmp     done
 
 bind_err:
-
         pushd   0x0c
         call    [con_set_flags]
-
         push    str4
         call    [con_write_asciiz]
         jmp     done
 
 sock_err:
-
         pushd   0x0c
         call    [con_set_flags]
-
         push    str6
         call    [con_write_asciiz]
         jmp     done
@@ -212,18 +228,28 @@ exit:
         mcall   -1
 
 
+thread_exit:
+        push    str_bye
+        call    [con_write_asciiz]      ; say bye bye
+        pop     ecx                     ; get the thread_data pointer from stack
+        mcall   68, 13                  ; free the memory
+        mcall   -1                      ; and kill the thread
 
-; data
-title   db      'KolibriOS FTP daemon 0.1', 0
-str1    db      'Starting FTP daemon on port %u', 0
-str2    db      '.', 0
-str2b   db      ' OK!',10,10,0
-str3    db      'Listen error',10,10,0
-str4    db      'Bind error',10,10,0
-str5    db      'Setsockopt error.',10,10,0
-str6    db      'Could not open socket',10,10,0
-str7    db      'Got data!',10,10,0
-str8    db      'Error accepting connection',10,10,0
+
+
+; initialized data
+
+title           db 'KolibriOS FTP daemon 0.1', 0
+str1            db 'Starting FTP daemon on port %u', 0
+str2            db '.', 0
+str2b           db ' OK!',10,10,0
+str3            db 'Listen error',10,10,0
+str4            db 'Bind error',10,10,0
+;str5            db 'Setsockopt error.',10,10,0
+str6            db 'Could not open socket',10,10,0
+str7            db 'Got data!',10,10,0
+str8            db 10,'New thread created!',10,10,0
+str_bye         db 10,'Closing thread!',10,10,0
 
 str_logged_in   db 'Login ok',10,10,0
 str_pass_ok     db 'Password ok - Logged in',10,10,0
@@ -233,91 +259,78 @@ str_datasock    db 'Passive data socket connected!',10,10,0
 str_notfound    db 'ERROR: file not found',10,10,0
 str_sockerr     db 'ERROR: socket error',10,10,0
 
-str_newline     db 10,0
-
-
+str_newline     db 10, 0
 str_mask        db '*', 0
 
+months          dd 'Jan '
+                dd 'Feb '
+                dd 'Mar '
+                dd 'Apr '
+                dd 'May '
+                dd 'Jun '
+                dd 'Jul '
+                dd 'Aug '
+                dd 'Sep '
+                dd 'Oct '
+                dd 'Nov '
+                dd 'Dec '
 
-months:
-        dd     'Jan ','Feb ','Mar ','Apr ','May ','Jun '
-        dd     'Jul ','Aug ','Sep ','Oct ','Nov ','Dec '
-
-filename db '.ini', 0
-str_port db 'port', 0
-str_ftpd db 'ftpd', 0
-str_conn db 'conn', 0
+filename        db '.ini', 0
+str_port        db 'port', 0
+str_ftpd        db 'ftpd', 0
+str_conn        db 'conn', 0
 
 sockaddr1:
-        dw AF_INET4
-.port   dw 21
-.ip     dd 0
-        rb 10
-.length = $ - sockaddr1
+                dw AF_INET4
+  .port         dw 21
+  .ip           dd 0
+                rb 10
+  .length       = $ - sockaddr1
 
 ; import
+
 align 4
 @IMPORT:
 
-library console, 'console.obj', \
-        libini, 'libini.obj', \
-        libio, 'libio.obj'
+library console,                'console.obj',\
+        libini,                 'libini.obj', \
+        libio,                  'libio.obj'
 
-import  console,        \
-        con_start,      'START',        \
-        con_init,       'con_init',     \
-        con_write_asciiz,       'con_write_asciiz',     \
-        con_exit,       'con_exit',     \
-        con_gets,       'con_gets',\
-        con_cls,        'con_cls',\
-        con_printf,     'con_printf',\
-        con_getch2,     'con_getch2',\
-        con_set_cursor_pos, 'con_set_cursor_pos',\
-        con_set_flags,  'con_set_flags'
+import  console,\
+        con_start,              'START',\
+        con_init,               'con_init',\
+        con_write_asciiz,       'con_write_asciiz',\
+        con_exit,               'con_exit',\
+        con_gets,               'con_gets',\
+        con_cls,                'con_cls',\
+        con_printf,             'con_printf',\
+        con_getch2,             'con_getch2',\
+        con_set_cursor_pos,     'con_set_cursor_pos',\
+        con_set_flags,          'con_set_flags'
 
-import  libini,         \
-        ini.get_str,    'ini_get_str',\
-        ini.get_int,    'ini_get_int'
+import  libini,\
+        ini.get_str,            'ini_get_str',\
+        ini.get_int,            'ini_get_int'
 
-import  libio,          \
-        libio.init , 'lib_init'   , \
-        file.size  , 'file_size'  , \
-        file.open  , 'file_open'  , \
-        file.read  , 'file_read'  , \
-        file.close , 'file_close' , \
-        file.find.first , 'file_find_first', \
-        file.find.next ,  'file_find_next', \
-        file.find.close , 'file_find_close'
+import  libio,\
+        libio.init,             'lib_init',\
+        file.size,              'file_size',\
+        file.open,              'file_open',\
+        file.read,              'file_read',\
+        file.close,             'file_close',\
+        file.find.first,        'file_find_first',\
+        file.find.next,         'file_find_next',\
+        file.find.close,        'file_find_close'
 
 
 i_end:
 
-socketnum       dd ?
+; uninitialised data
 
+        socketnum       dd ?
+        path            rb 1024
+        params          rb 1024
 
-; thread specific data
-socketnum2      dd ?
-state           dd ?
-home_dir        db '/rd/1/', 0
-                rb 1024
-work_dir        rb 1024
-fpath           rb 1024*3
-
-type            db ?
-mode            db ?    ; active/passive
-
-passivesocknum  dd ?
-datasocketnum   dd ?
-
-datasock:
-        dw AF_INET4
-.port   dw ?
-.ip     dd ?
-        rb 10
-.length = $ - datasock
-
-buffer  rb BUFFERSIZE
-.length = $ - buffer
-
-path    rb 1024
 mem:
+
+
