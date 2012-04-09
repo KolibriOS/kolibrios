d08512f2e72be27e47fb3e67687adfc6a80a6c98
CleverMouse <theclevermouse@yandex.ru>
2012-04-09 12:05:32 +0000

    fix compilation flags for uFMOD in ac97snd
    git-svn-id: svn://kolibrios.org@2593 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ programs/media/ac97snd/ufmod-config.asm
@@ -0,0 +1,7 @@
+UF_FREQ  equ 48000   ; not used actually
+UF_RAMP  equ STRONG
+UF_MODE  equ AC97SND ; important
+DEBUG    equ 0
+NOLINKER equ 0
+include '../../develop/libraries/ufmod/eff.inc'
+include '../../develop/libraries/ufmod/fasm.asm'
