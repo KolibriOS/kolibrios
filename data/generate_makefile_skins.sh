b1f38b7e9b8350fca1de6d614a0b9e50e9d3a2ba
CleverMouse <theclevermouse@yandex.ru>
2012-06-15 11:08:31 +0000

    add skins to russian autobuild
    git-svn-id: svn://kolibrios.org@2777 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ data/generate_makefile_skins.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+echo SKIN_SOURCES:=\\
+while read skinname
+do
+	skinpath=../../skins/"$skinname"
+	for f in $skinpath/*.{asm,ASM}
+	do
+		if [ ! -f "$f" ]; then continue; fi
+		if expr "$f" : '.*\.dtp' > /dev/null; then continue; fi
+		echo -n allskins/$skinname.skn | sed 's/ /|/g'
+		echo -n ':Skins/:'
+		echo -n $f | sed 's/ /|/g'
+		echo " \\"
+	done
+	for f in ../../skins/"$skinname"/*/*.{asm,ASM}
+	do
+		if [ ! -f "$f" ]; then continue; fi
+		if expr "$f" : '.*\.dtp' > /dev/null; then continue; fi
+		g=`basename "$(dirname "$f")"`
+		echo -n allskins/$g.skn | sed 's/ /|/g'
+		echo -n ':Skins/:'
+		echo -n $f | sed 's/ /|/g'
+		echo " \\"
+	done
+done
+echo '# end of list'
