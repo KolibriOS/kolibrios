7567859abf3e7927d395b30dcdf2a4119e4f6bf8
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2012-06-23 22:31:44 +0000

    forgoten file
    git-svn-id: svn://kolibrios.org@2824 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ programs/network/htmlv/browser/include/parce_tag.h
@@ -0,0 +1,36 @@
+void GetNextParam()
+{
+	byte	kavichki = false;
+	int		i = strlen(#tagparam) - 1;
+	
+	WHILE((i > 0) && ((tagparam[i] == '"') || (tagparam[i] == ' ') || (tagparam[i] == '\'') || (tagparam[i] == '/')))
+	{
+		IF (tagparam[i] == '"') || (tagparam[i] == '\'') kavichki=tagparam[i];
+		tagparam[i] = 0x00;
+		i--;
+	}
+
+	IF (kavichki)
+	{
+		i=find_symbol(#tagparam, kavichki);
+		strcpy(#options, #tagparam + i);
+	}
+	ELSE
+	{
+		WHILE((i > 0) && (tagparam[i] <>'=')) i--; //i=find_symbol(#tagparam, '=')+1;
+		i++;
+		
+		strcpy(#options, #tagparam + i); //копируем опцию
+		WHILE (options[0] == ' ') strcpy(#options, #options+1);
+	}
+	tagparam[i] = 0x00;
+
+	FOR ( ; ((tagparam[i] <>' ') && (i > 0); i--)
+	{
+		IF (tagparam[i] == '=') //дерзкая заглушка
+			tagparam[i + 1] = 0x00;
+	}
+
+	strcpy(#parametr, #tagparam + i + 1); //копируем параметр
+	tagparam[i] = 0x00;
+}
