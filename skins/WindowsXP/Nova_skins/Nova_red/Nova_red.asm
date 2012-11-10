c29c965288b05eb4b5edbeb305b0d36e4ccb795a
Rock_maniak_forever <Rock_maniak_forever@kolibrios.org>
2012-11-10 06:15:23 +0000

    Nova skins added.
    git-svn-id: svn://kolibrios.org@3021 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ skins/WindowsXP/Nova_skins/Nova_red/Nova_red.asm
@@ -0,0 +1,39 @@
+include 'my_skin.inc'
+
+SKIN_PARAMS \
+  height          = bmp_base.height,\     ; skin height
+  margins         = [19:1:43:1],\         ; margins [left:top:right:bottom]
+  colors active   = [binner=0xDA9A9A:\    ; border inner color
+                     bouter=0xDA9A9A:\    ; border outer color
+                     bframe=0x812727],\   ; border frame color
+  colors inactive = [binner=0xE4B3B3:\    ; border inner color
+                     bouter=0xE4B3B3:\    ; border outer color
+                     bframe=0x9E4444],\   ; border frame color
+  dtp             = 'RED.DTP'   ; dtp colors
+
+SKIN_BUTTONS \
+  close    = [-32:0][26:18],\             ; buttons coordinates
+  minimize = [-49:0][17:18]               ; [left:top][width:height]
+
+SKIN_BITMAPS \
+  left active   = bmp_left,\              ; skin bitmaps pointers
+  left inactive = bmp_left1,\
+  oper active   = bmp_oper,\
+  oper inactive = bmp_oper1,\
+  base active   = bmp_base,\
+  base inactive = bmp_base1
+
+BITMAP bmp_left ,'active/left.bmp'               ; skin bitmaps
+BITMAP bmp_oper ,'active/oper.bmp'
+BITMAP bmp_base ,'active/base.bmp'
+BITMAP bmp_left1,'inactive/left.bmp'
+BITMAP bmp_oper1,'inactive/oper.bmp'
+BITMAP bmp_base1,'inactive/base.bmp'
+
+;================================;
+; Created by Rock_maniak_forever ;
+;================================;
+
+; B73737
+
+; 812727
