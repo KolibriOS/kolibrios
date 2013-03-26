7a45c40dae374fde896dc3154435f9dda0954402
Marat Zakiyanov (Mario79) <mario79@bk.ru>
2013-03-26 05:20:40 +0000

    Proc_Lib - support for ColorDialog
    git-svn-id: svn://kolibrios.org@3418 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/libraries/proc_lib/trunk/proc_lib.asm
+++ programs/develop/libraries/proc_lib/trunk/proc_lib.asm
@@ -13,7 +13,8 @@ section '.flat' code readable align 16
 include '../../../../macros.inc'
 ;include '../../../../proc32.inc'
 include 'opendial.mac'
-
+include 'colrdial.mac'
+;include 'debug.inc'
 ;-----------------------------------------------------------------------------
 
 ;--------------------------------------------------
@@ -21,6 +22,12 @@ include 'opendial.mac'
 ;--------------------------------------------------
 align 16
 use_OpenDialog
+
+;--------------------------------------------------
+; Color Dialog
+;--------------------------------------------------
+align 16
+use_ColorDialog
 
 ;--------------------------------------------------
 align 16
@@ -35,16 +42,24 @@ EXPORTS:
 dd	sz_init,			lib_init
 dd	sz_version,			0x00000001
 
-dd	sz_OpenDialog_init,		OpenDialog.init
-dd	sz_OpenDialog_start,		OpenDialog.start
-dd	szVersion_OpenDialog,		0x00010001
+dd	sz_OpenDialog_init,	OpenDialog.init
+dd	sz_OpenDialog_start,	OpenDialog.start
+dd	szVersion_OpenDialog,	0x00010001
+
+dd	sz_ColorDialog_init,	ColorDialog.init
+dd	sz_ColorDialog_start,	ColorDialog.start
+dd	szVersion_ColorDialog,	0x00010001
 
 dd	0,0
-
-
+;-----------------------------------------------------------------------------
 sz_init 			db 'lib_init',0
 sz_version			db 'version',0
 
 sz_OpenDialog_init 		db 'OpenDialog_init',0
 sz_OpenDialog_start		db 'OpenDialog_start',0
-szVersion_OpenDialog	db 'Version_OpenDialog',0
+szVersion_OpenDialog		db 'Version_OpenDialog',0
+
+sz_ColorDialog_init 		db 'ColorDialog_init',0
+sz_ColorDialog_start		db 'ColorDialog_start',0
+szVersion_ColorDialog		db 'Version_ColorDialog',0
+;-----------------------------------------------------------------------------
