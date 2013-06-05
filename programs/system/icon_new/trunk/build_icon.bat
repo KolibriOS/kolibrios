da53db0ae709cc78aac31ca0d52ad2fab090d26f
Marat Zakiyanov (Mario79) <mario79@bk.ru>
2013-06-05 22:13:54 +0000

    Removal of outdated and unused version of ICON. If anyone ever need it, then he can take it out of the old versions the SVN repository. However, the existence of this program mislead people.
    git-svn-id: svn://kolibrios.org@3608 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/system/icon_new/trunk/build_icon.bat
+++
@@ -1,6 +0,0 @@
-@erase lang.inc
-@echo lang fix ru >lang.inc
-@fasm icon.asm @icon
-@erase lang.inc
-@kpack @icon
-@pause
--- programs/system/icon_new/trunk/build_iconmngr.bat
+++
@@ -1,6 +0,0 @@
-@erase lang.inc
-@echo lang fix ru >lang.inc
-@fasm iconmngr.asm iconmngr
-@erase lang.inc
-@kpack iconmngr
-@pause
