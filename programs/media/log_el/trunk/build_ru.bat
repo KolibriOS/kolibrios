f9537aeeaeee9822ff0b3de486cca2e40678903f
IgorA <IgorA@kolibrios.org>
2013-06-27 17:35:25 +0000

    fix for the new 'box_lib.obj'
    git-svn-id: svn://kolibrios.org@3714 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/media/log_el/trunk/build_ru.bat
+++ programs/media/log_el/trunk/build_ru.bat
@@ -4,6 +4,4 @@ if not exist bin\font6x9.bmp @copy ..\..\..\fs\kfar\trunk\font6x9.bmp bin\font6x
 @copy *.txt bin\*.txt
 @fasm.exe -m 16384 log_el.asm bin\log_el.kex
 @kpack bin\log_el.kex
-if not exist bin\buf2d.obj @fasm.exe -m 16384 ..\..\..\develop\libraries\buf2d\trunk\buf2d.asm bin\buf2d.obj
-@kpack bin\buf2d.obj
 pause
