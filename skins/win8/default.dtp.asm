1dc693eed6d88bb076ca8ec2dc8aebddba071f1b
GerdtR <GerdtR@kolibrios.org>
2013-07-04 00:46:12 +0000

    Added skin Win8
    git-svn-id: svn://kolibrios.org@3754 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ skins/win8/default.dtp.asm
@@ -0,0 +1,11 @@
+;SYSTEM COLORS (.DTP) - COMPILE WITH FASM
+frame                   dd 0x5285BA
+grab                    dd 0x6AAEF7
+grab_button             dd 0x498DF7
+grab_button_text        dd 0xffffff
+grab_text               dd 0xffffff
+work                    dd 0xF3F3F3
+work_button             dd 0xf3f3f3
+work_button_text        dd 0x000000
+work_text               dd 0x000000
+work_graph              dd 0xB4CCE7
