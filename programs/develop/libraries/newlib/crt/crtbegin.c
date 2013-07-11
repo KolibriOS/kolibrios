221e4dd488fa60a7932c82c941a270a0231bb800
Sergey Semyonov (Serge) <ion2@mail.ru>
2013-07-11 10:17:41 +0000

    newlib: update
    git-svn-id: svn://kolibrios.org@3806 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ programs/develop/libraries/newlib/crt/crtbegin.c
@@ -0,0 +1,18 @@
+
+extern void _pei386_runtime_relocator (void);
+
+int DllStartup(void *module, int reason);// __attribute__ ((dllexport));
+
+
+int DllStartup(void *module, int reason)
+{
+    _pei386_runtime_relocator();
+    return 1;
+};
+
+void  ___dll_start ()
+
+{
+    _pei386_runtime_relocator();
+
+}
