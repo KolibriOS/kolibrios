00d4cb2df87bdce8f13dc631faeff003b052ff90
heavyiron <heavyiron@kolibrios.org>
2013-09-07 05:27:08 +0000

    settime area refactoring
    git-svn-id: svn://kolibrios.org@3894 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/system/calendar/trunk/calendar.asm
+++ programs/system/calendar/trunk/calendar.asm
@@ -470,14 +470,14 @@ draw_clock:
 
     mcall 3
     mov  ecx,eax
-    mcall 47,0x00020100, ,205*65536+280,0x50000000,COL_WINDOW_BG
+    mcall 47,0x00020100, ,198*65536+287,0x50000000,COL_WINDOW_BG
 
     shr  ecx,8
-    add  edx,20*65536
+    add  edx,22*65536
     mcall
 
     shr  ecx,8
-    add  edx,20*65536
+    add  edx,22*65536
     mcall
     ret
 
@@ -492,48 +492,49 @@ draw_window:
 	mcall 13,B_WBAR_X, ,COL_TOOLBAR_BG ; draw toolbar background
     call draw_week
 
-    mcall 8,205*65536+7,290*65536+10,72,COL_TIME_BUTTONS
+    mcall 8,196*65536+8,272*65536+10,72,COL_DATE_BUTTONS
 
-    mov  ebx,212*65536+7
+    mov  ebx,205*65536+8
     inc  edx
     mcall
 
-    mov  ebx,205*65536+7
-    mov  ecx,300*65536+10
+    mov  ebx,196*65536+8
+    mov  ecx,298*65536+10
     inc  edx
     mcall
 
-    mov  ebx,212*65536+7
+    mov  ebx,205*65536+8
     inc  edx
     mcall
 
-    mov  ebx,225*65536+7
-    mov  ecx,290*65536+10
+    mov  ebx,218*65536+8
+    mov  ecx,272*65536+10
     inc  edx
     mcall
 
-    mov  ebx,232*65536+7
+    mov  ebx,227*65536+8
     inc  edx
     mcall
 
-    mov  ebx,225*65536+7
-    mov  ecx,300*65536+10
+    mov  ebx,218*65536+8
+    mov  ecx,298*65536+10
     inc  edx
     mcall
 
-    mov  ebx,232*65536+7
+    mov  ebx,227*65536+8
     inc  edx
     mcall
 
-    mov  ebx,244*65536+14
-    mov  ecx,290*65536+20
+    mov  ebx,240*65536+21
+    mov  ecx,273*65536+35
     inc  edx
+    or   edx,0x40000000
     mcall
 
     mov  ebx,14*65536+110
     mov  ecx,285*65536+22
     mov  esi,COL_DATE_BUTTONS
-    inc  edx
+    mov  edx,81
     mcall
 
     mov  esi,COL_MONTH_YEAR_B   ; new style
@@ -559,10 +560,14 @@ draw_window:
     call draw_days
 
 	; draw text in window
-    mcall 4,162*65536+280,0x800000ff,sys_text
-    mcall  ,180*65536+302,0x800000ff,minus
-    mcall  ,180*65536+292,0x80ff0000,plus
+    mcall 4,153*65536+287,0x80000000,sys_text
+    mcall 4,183*65536+287, ,separator
+    mcall  ,214*65536+287
+    mcall  ,236*65536+287
+    mcall  ,188*65536+274, ,plus
+    mcall  ,188*65536+300, ,minus
     mcall  , 24*65536+292,0x00000000,set_date_t,15 ;set date text
+
 
     mov  ecx,0x10ddeeff
     mov  edx,n_style
