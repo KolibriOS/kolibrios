5528e3a3d57ee1bff945acc18382fc19b0a63bab
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2013-10-13 11:00:08 +0000

    small orthographic fix
    git-svn-id: svn://kolibrios.org@4016 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/system/shell/cmd/cmd_sleep.c
+++ programs/system/shell/cmd/cmd_sleep.c
@@ -8,7 +8,7 @@ if (!strlen(param))
 	#if LANG_ENG
 		printf("    sleep <time in the 1/100 of second>\n\r");
 	#elif LANG_RUS
-		printf("  sleep <интервал в сотых доля секунды>\n\r");
+		printf("  sleep <интервал в сотых доляx секунды>\n\r");
 	#endif
 	return TRUE;
 	}
