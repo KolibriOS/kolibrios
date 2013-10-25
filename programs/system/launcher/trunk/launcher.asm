cabf4a3250b8ccad606eea035e0449594cbe19fb
Marat Zakiyanov (Mario79) <mario79@bk.ru>
2013-10-25 21:44:42 +0000

    LAUNCHER - using of SETTINGS directory for AUTORUN.DAT
    git-svn-id: svn://kolibrios.org@4095 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/system/launcher/trunk/launcher.asm
+++ programs/system/launcher/trunk/launcher.asm
@@ -238,7 +238,7 @@ START:                           ; start of execution
                    dd 0
    .blocks         dd 16*512       ; 16*512 bytes max
    .address        dd file_data
-   db "AUTORUN.DAT",0
+   db "/SYS/SETTINGS/AUTORUN.DAT",0
 
  start_info:
    .mode           dd 7
