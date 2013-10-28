866bf8d5a9eb5ac1363b38e4b27aba37527a7115
esevece <esevece@kolibrios.org>
2013-10-28 19:39:36 +0000

    Added limits.h and stdarg.h because I got some errors about va_list undefined
    git-svn-id: svn://kolibrios.org@4117 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/libraries/menuetlibc/src/libc/ansi/stdio/printf.c
+++ programs/develop/libraries/menuetlibc/src/libc/ansi/stdio/printf.c
@@ -1,6 +1,8 @@
 /* Copyright (C) 1994 DJ Delorie, see COPYING.DJ for details */
 #include <stdio.h>
+#include <limits.h>
 #include <libc/file.h>
+#include <stdarg.h>
 
 int
 printf(const char *fmt, ...)
