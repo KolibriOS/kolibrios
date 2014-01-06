52a2c2c8e822313315fab43f5869ef53dcf7e4ea
Serhii Sakhno <sergei.sakhno@gmail.com>
2014-01-06 00:33:26 +0000

    header file to work with the clipboard
    git-svn-id: svn://kolibrios.org@4426 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ programs/cmm/lib/clipboard.h
@@ -0,0 +1,42 @@
+struct Clipboard {
+	dword GetSlotCount();
+	dword GetSlotData( ECX);
+	dword SetSlotData( EDX, ESI);
+	dword DelLastSlot();
+	dword ResetBlockingBuffer();
+};
+
+dword Clipboard::GetSlotCount()
+{
+	$mov eax, 54 
+	$mov ebx, 0
+	$int 0x40
+}
+
+dword Clipboard::GetSlotData( ECX) //ECX = slot number
+{
+	$mov eax, 54 
+	$mov ebx, 1
+	$int 0x40
+}
+
+dword Clipboard::SetSlotData( ECX, EDX) //ECX = data size, EDX - pointer to data
+{
+	$mov eax, 54 
+	$mov ebx, 2
+	$int 0x40
+}
+
+dword Clipboard::DelLastSlot()
+{
+	$mov eax, 54 
+	$mov ebx, 3
+	$int 0x40
+}
+
+dword Clipboard::ResetBlockingBuffer()
+{
+	$mov eax, 54 
+	$mov ebx, 3
+	$int 0x40
+}
