ace3d39af4dd8afbe06a0478b68c61c422b8f549
CleverMouse <theclevermouse@yandex.ru>
2014-09-15 09:45:19 +0000

    add forgotten file
    git-svn-id: svn://kolibrios.org@5124 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ programs/games/checkers/trunk/Tupfile.lua
@@ -0,0 +1,13 @@
+if tup.getconfig("NO_GCC") ~= "" then return end
+HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../../.." or tup.getconfig("HELPERDIR")
+tup.include(HELPERDIR .. "/use_gcc.lua")
+tup.include(HELPERDIR .. "/use_menuetlibc.lua")
+
+if tup.getconfig("LANG") == "ru"
+then C_LANG = "LANG_RUS"
+else C_LANG = "LANG_ENG" -- this includes default case without config
+end
+CFLAGS = CFLAGS .. " -D_KOLIBRI -DAUTOBUILD -D" .. C_LANG .. " -DNO_FILES -fwhole-program"
+
+compile_gcc{"checkers.cpp"}
+link_gcc("checkers")
