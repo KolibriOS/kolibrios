1e6a19cfe9d0eaf7599ec92e6d037716f1a5dc05
Sergey Semyonov (Serge) <ion2@mail.ru>
2014-12-06 10:12:58 +0000

    binutils LD: build script for Kolibri.
    git-svn-id: svn://kolibrios.org@5223 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ contrib/toolchain/binutils/ld/ld.sh
@@ -0,0 +1,23 @@
+#SHS
+/kolibrios/as -o deffilep.o deffilep.s
+/kolibrios/as -o ei386pe.o ei386pe.s
+/kolibrios/as -o ldcref.o ldcref.s
+/kolibrios/as -o ldctor.o ldctor.s
+/kolibrios/as -o ldemul.o ldemul.s
+/kolibrios/as -o ldexp.o ldexp.s
+/kolibrios/as -o ldfile.o ldfile.s
+/kolibrios/as -o ldgram.o ldgram.s
+/kolibrios/as -o ldlang.o ldlang.s
+/kolibrios/as -o ldlex-wrapper.o ldlex-wrapper.s
+/kolibrios/as -o ldmain.o ldmain.s
+/kolibrios/as -o ldmisc.o ldmisc.s
+/kolibrios/as -o ldver.o ldver.s
+/kolibrios/as -o ldwrite.o ldwrite.s
+/kolibrios/as -o lexsup.o lexsup.s
+/kolibrios/as -o mri.o mri.s
+/kolibrios/as -o pe-dll.o pe-dll.s
+/kolibrios/ld @ld.lnk
+/kolibrios/objcopy ld-new ld -O binary
+
+
+
