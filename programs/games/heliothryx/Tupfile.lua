cbc8e8a761860b1bc578248be2d00b7b99235d06
alpine <alpine@kolibrios.org>
2014-12-08 19:31:52 +0000

    Heliothryx Game: Changed executable name
    git-svn-id: svn://kolibrios.org@5226 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/games/heliothryx/Tupfile.lua
+++ programs/games/heliothryx/Tupfile.lua
@@ -5,4 +5,4 @@ tup.include(HELPERDIR .. "/use_sound.lua")
 LDFLAGS = LDFLAGS .. " -T kolibri.ld"
 tup.append_table(OBJS, tup.rule("start.asm", "fasm %f %o", "start.o"))
 compile_gcc{"system/kolibri.c", "game/rs/rsmicrolibc.c", "game/rs/rsplatform_kos.c", "game/rs/rsmx.c", "game/rsnoise.c", "game/rsgentex.c", "game/rsmain.c",  "game/rsgame.c", "game/rsgamedraw.c", "game/rskos.c", "game/rsgametext.c", "game/rsgamemenu.c"}
-link_gcc("helio")
+link_gcc("heliothryx")
