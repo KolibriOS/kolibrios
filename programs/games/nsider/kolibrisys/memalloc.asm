a3271aa807e9b64e7067df06062af5535c637b12
CleverMouse <theclevermouse@yandex.ru>
2014-12-29 10:38:54 +0000

    stdcall functions should restore ebx
    git-svn-id: svn://kolibrios.org@5285 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/games/nsider/kolibrisys/memalloc.asm
+++ programs/games/nsider/kolibrisys/memalloc.asm
@@ -9,30 +9,36 @@ public _realloc
 align 4
 _malloc:
 
+        push ebx
         mov eax,68
         mov ebx,12
-	mov ecx,[esp+4] ;size
+	mov ecx,[esp+8] ;size
         int 0x40
+        pop ebx
 
         ret 4
 
 align 4
 _free:
 
+        push ebx
         mov eax,68
         mov ebx,13
-	mov ecx,[esp+4]
+	mov ecx,[esp+8]
         int 0x40
+        pop ebx
 
         ret 4
 
 align 4
 _realloc:
 
+        push ebx
         mov ebx,20
         mov eax,68
-	mov ecx,[esp+4]
-	mov edx,[esp+8]
+	mov ecx,[esp+8]
+	mov edx,[esp+12]
         int 0x40
+        pop ebx
 
         ret 8
