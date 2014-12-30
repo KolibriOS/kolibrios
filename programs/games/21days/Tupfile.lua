02ccb0c6449c93c0ca63d4f9f7b3ab340fd7c6ac
igevorse <igevorse@kolibrios.org>
2014-12-30 22:32:23 +0000

    '21 days' game: get rid of 'deprecated conversion from string constant to 'char*'' warning
    git-svn-id: svn://kolibrios.org@5301 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/games/21days/Tupfile.lua
+++ programs/games/21days/Tupfile.lua
@@ -2,6 +2,6 @@ if tup.getconfig("NO_GCC") ~= "" then return end
 HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../.." or tup.getconfig("HELPERDIR")
 tup.include(HELPERDIR .. "/use_gcc.lua")
 tup.include(HELPERDIR .. "/use_menuetlibc.lua")
-CFLAGS = CFLAGS .. " -D _KOS32 "
+CFLAGS = CFLAGS .. " -Wno-write-strings -D _KOS32 "
 compile_gcc{"main.cpp", "game.cpp", "interface.cpp", "sys.cpp"}
 link_gcc("21days")
