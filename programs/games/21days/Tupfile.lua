1e9f11eb8abacfbc62e39efc58b5753769fa6aca
igevorse <igevorse@kolibrios.org>
2014-12-30 22:27:16 +0000

    '21 days' game: Tupfile.lua fixed
    git-svn-id: svn://kolibrios.org@5300 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/games/21days/Tupfile.lua
+++ programs/games/21days/Tupfile.lua
@@ -1,5 +1,5 @@
 if tup.getconfig("NO_GCC") ~= "" then return end
-HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../../.." or tup.getconfig("HELPERDIR")
+HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../.." or tup.getconfig("HELPERDIR")
 tup.include(HELPERDIR .. "/use_gcc.lua")
 tup.include(HELPERDIR .. "/use_menuetlibc.lua")
 CFLAGS = CFLAGS .. " -D _KOS32 "
