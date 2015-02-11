d8dd5a8fc054a0008059616efebb3e46ee255a08
eAndrew <eAndrew@kolibrios.org>
2015-02-11 20:47:36 +0000

    Fix autobuild #2
    git-svn-id: svn://kolibrios.org@5396 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/system/open/Tupfile.lua
+++ programs/system/open/Tupfile.lua
@@ -1,3 +1,3 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
 tup.rule("echo lang fix " .. ((tup.getconfig("LANG") == "") and "en" or tup.getconfig("LANG")) .. " > lang.inc", {"lang.inc"})
-tup.rule("open.asm", "fasm %f %o " .. tup.getconfig("KPACK_CMD"), "open")
+tup.rule({"open.asm", extra_inputs = {"lang.inc"}}, "fasm %f %o " .. tup.getconfig("KPACK_CMD"), "open")
