57b00395705d82dbdd3e439d567315bfa1eeb42d
Serhii Sakhno <sergei.sakhno@gmail.com>
2015-02-13 22:20:25 +0000

    fix for r5399
    git-svn-id: svn://kolibrios.org@5400 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/cmm/lib/lib.obj/libini.h
+++ programs/cmm/lib/lib.obj/libini.h
@@ -6,11 +6,11 @@ dword ini_enum_sections = #aini_enum_sections;
 dword ini_enum_keys = #aini_enum_keys;
 dword ini_get_str = #aini_get_str;
 dword ini_get_int = #aini_get_int;
-//dword ini_get_color = #aini_get_color;
+dword ini_get_color = #aini_get_color;
 dword ini_set_str = #aini_set_str;
 dword ini_set_int = #aini_set_int;
-//dword ini_set_color = #aini_set_color;
-//dword ini_get_shortcut = #aini_get_shortcut;
+dword ini_set_color = #aini_set_color;
+dword ini_get_shortcut = #aini_get_shortcut;
 $DD 2 dup 0
 
 char alib_init[] = "lib_init";
@@ -20,6 +20,6 @@ char aini_get_str[] = "ini_get_str";
 char aini_get_int[] = "ini_get_int";
 char aini_set_str[] = "ini_set_str";
 char aini_set_int[] = "ini_set_int";
-//char aini_get_shortcut[] = "ini_get_shortcut";
-//char aini_get_color[] = "ini_get_color";
-//char aini_set_color[] = "ini_set_color";
+char aini_get_shortcut[] = "ini_get_shortcut";
+char aini_get_color[] = "ini_get_color";
+char aini_set_color[] = "ini_set_color";
