eccb25706da82fee3e6b2bdb3d1b8ac95f091af8
Serhii Sakhno <sergei.sakhno@gmail.com>
2015-07-24 18:36:33 +0000

    fix for previous commit
    git-svn-id: svn://kolibrios.org@5613 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/cmm/Calypte/Tupfile.lua
+++ programs/cmm/Calypte/Tupfile.lua
@@ -3,4 +3,4 @@ if tup.getconfig("LANG") == "ru"
 then C_LANG = "LANG_RUS"
 else C_LANG = "LANG_ENG" -- this includes default case without config
 end
-tup.rule("Ready.c", "c-- /D=AUTOBUILD /D=$(C_LANG) %f" .. tup.getconfig("KPACK_CMD"), "Ready.com")
+tup.rule("Calypte.c", "c-- /D=AUTOBUILD /D=$(C_LANG) %f" .. tup.getconfig("KPACK_CMD"), "Calypte.com")
