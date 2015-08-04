cd42aa37be0b5863e5a8ceedf9956bffa90bc9a3
hidnplayr <hidnplayr@kolibrios.org>
2015-08-04 07:35:03 +0000

    VNC viewer: use more memory to assemble.
    git-svn-id: svn://kolibrios.org@5669 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/network/vncc/Tupfile.lua
+++ programs/network/vncc/Tupfile.lua
@@ -1,2 +1,2 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
-tup.rule("vncc.asm", "fasm %f %o " .. tup.getconfig("KPACK_CMD"), "vncc")
+tup.rule("vncc.asm", "fasm -m 32768 %f %o " .. tup.getconfig("KPACK_CMD"), "vncc")
