990c9e53b74adad7e1fc88a8776a4970f78551e4
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2015-08-09 21:31:07 +0000

    Upload Shkvorka skin
    git-svn-id: svn://kolibrios.org@5699 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ skins/_old/Leency/Shkvorka/default.dtp.asm
@@ -0,0 +1,14 @@
+struc system_colors
+ {
+  .frame            dd 0x646464
+  .grab             dd 0x000000
+  .grab_button      dd 0x000000
+  .grab_button_text dd 0x000000
+  .grab_text        dd 0x000000
+  .work             dd 0xE1E1E1
+  .work_button      dd 0x7D7D8A
+  .work_button_text dd 0xFFFFFF
+  .work_text        dd 0x000000
+  .work_graph       dd 0x6B677A
+ }
+struct system_colors
