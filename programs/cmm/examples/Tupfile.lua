30ae539a5ea78007f851ad171a72962963c2bb10
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2016-01-10 20:04:33 +0000

    fix for prior rev
    git-svn-id: svn://kolibrios.org@6051 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/cmm/examples/Tupfile.lua
+++ programs/cmm/examples/Tupfile.lua
@@ -3,6 +3,6 @@ if tup.getconfig("LANG") == "ru"
 then C_LANG = "LANG_RUS"
 else C_LANG = "LANG_ENG" -- this includes default case without config
 end
-tup.rule("example.c", "c-- /D=AUTOBUILD /D=$(C_LANG) %f" .. tup.getconfig("KPACK_CMD"), "example.com")
+tup.rule("window.c", "c-- /D=AUTOBUILD /D=$(C_LANG) %f" .. tup.getconfig("KPACK_CMD"), "window.com")
 tup.rule("collections.c", "c-- /D=AUTOBUILD /D=$(C_LANG) %f" .. tup.getconfig("KPACK_CMD"), "collections.com")
 tup.rule("menu.c", "c-- /D=AUTOBUILD /D=$(C_LANG) %f" .. tup.getconfig("KPACK_CMD"), "menu.com")
