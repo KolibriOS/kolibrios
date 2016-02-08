d43a9570e1f6853a9b66c4d41ac01e7d6e0aa3f6
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2016-02-08 14:28:25 +0000

    skincfg: remove old non-skinned window, gui fixes
    git-svn-id: svn://kolibrios.org@6170 a494cfbc-eb01-0410-851d-a64ba20cac60

--- skins/_old/Leency/Shkvorka/default.dtp.asm
+++ skins/_old/Leency/Shkvorka/default.dtp.asm
@@ -1,10 +1,10 @@
 struc system_colors
  {
-  .nonset1          dd 0x646464
-  .nonset2          dd 0x000000
-  .3d_dark          dd 0xAEA6A6
-  .3d_light         dd 0xFBFBFB
-  .grab_text        dd 0x333333
+  .unused1          dd 0x646464
+  .unused2          dd 0x000000
+  .work_3d_dark     dd 0xAEA6A6
+  .work_3d_light    dd 0xFBFBFB
+  .unused3          dd 0x333333
   .work             dd 0xDDD7CF
   .work_button      dd 0x7887A6
   .work_button_text dd 0xFEFEFE
