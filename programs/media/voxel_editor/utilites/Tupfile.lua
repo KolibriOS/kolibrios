c8cf2e532a5dc71dedfa73de72aa435def7ae9c0
IgorA <IgorA@kolibrios.org>
2016-02-21 22:17:07 +0000

    add voxel utilites in iso
    git-svn-id: svn://kolibrios.org@6279 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/media/voxel_editor/utilites/Tupfile.lua
+++ programs/media/voxel_editor/utilites/Tupfile.lua
@@ -1,2 +1,2 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
-tup.foreach_rule({"vox_creator.asm", "vox_mover.asm"}, "fasm %f %o " .. tup.getconfig("KPACK_CMD"), "%B")
+tup.foreach_rule({"vox_creator.asm", "vox_mover.asm", "vox_tgl.asm"}, "fasm %f %o " .. tup.getconfig("KPACK_CMD"), "%B")
