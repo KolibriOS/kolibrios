52fcf098d478f983f3dd85010a1cf75231d10494
nisargshah95 <nisargshah95@kolibrios.org>
2016-03-16 19:35:22 +0000

    fix gmon flickering issue
    git-svn-id: svn://kolibrios.org@6353 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/system/gmon/gmon.asm
+++ programs/system/gmon/gmon.asm
@@ -1,26 +1,26 @@
 ;
-; Ghost Monitor - утилита (стресс) тестирования и мониторинга системы
+; Ghost Monitor - СѓС‚РёР»РёС‚Р° (СЃС‚СЂРµСЃСЃ) С‚РµСЃС‚РёСЂРѕРІР°РЅРёСЏ Рё РјРѕРЅРёС‚РѕСЂРёРЅРіР° СЃРёСЃС‚РµРјС‹
 ; Copyright (C) 2005, 2006, 2007 Mihailov Ilia (ghost.nsk@mail.ru)
 ; All Right Reserved
 ;
-; ВОЗМОЖНА ПОРЧА ОБОРУДОВАНИЯ!
-; ИСПОЛЬЗУЙТЕ НА СВОЙ СТРАХ И РИСК!
+; Р’РћР—РњРћР–РќРђ РџРћР Р§Рђ РћР‘РћР РЈР”РћР’РђРќРРЇ!
+; РРЎРџРћР›Р¬Р—РЈР™РўР• РќРђ РЎР’РћР™ РЎРўР РђРҐ Р Р РРЎРљ!
 ;
-; Заметки :
-;	Дописать запуск тестов (rtst) (нет реакции на ошибку запуска)
-;	Переделать обновление графика (сделать отдельным процессом)
-;	Добавить реакцию на параметры датчиков
+; Р—Р°РјРµС‚РєРё :
+;	Р”РѕРїРёСЃР°С‚СЊ Р·Р°РїСѓСЃРє С‚РµСЃС‚РѕРІ (rtst) (РЅРµС‚ СЂРµР°РєС†РёРё РЅР° РѕС€РёР±РєСѓ Р·Р°РїСѓСЃРєР°)
+;	РџРµСЂРµРґРµР»Р°С‚СЊ РѕР±РЅРѕРІР»РµРЅРёРµ РіСЂР°С„РёРєР° (СЃРґРµР»Р°С‚СЊ РѕС‚РґРµР»СЊРЅС‹Рј РїСЂРѕС†РµСЃСЃРѕРј)
+;	Р”РѕР±Р°РІРёС‚СЊ СЂРµР°РєС†РёСЋ РЅР° РїР°СЂР°РјРµС‚СЂС‹ РґР°С‚С‡РёРєРѕРІ
 ;
 
-; Внешние файлы :
-;	hwm*.inc	- процедуры работы с датчиками мониторинга
-;	system.inc	- вычисление загрузки процессора, завершение теста, etc...
-;	tests.inc	- тесты ;-)
-;	diag.inc	- поддержка диаграм
-;	tab_*.inc	- процедуры отрисовки соответствуюших вкладок
+; Р’РЅРµС€РЅРёРµ С„Р°Р№Р»С‹ :
+;	hwm*.inc	- РїСЂРѕС†РµРґСѓСЂС‹ СЂР°Р±РѕС‚С‹ СЃ РґР°С‚С‡РёРєР°РјРё РјРѕРЅРёС‚РѕСЂРёРЅРіР°
+;	system.inc	- РІС‹С‡РёСЃР»РµРЅРёРµ Р·Р°РіСЂСѓР·РєРё РїСЂРѕС†РµСЃСЃРѕСЂР°, Р·Р°РІРµСЂС€РµРЅРёРµ С‚РµСЃС‚Р°, etc...
+;	tests.inc	- С‚РµСЃС‚С‹ ;-)
+;	diag.inc	- РїРѕРґРґРµСЂР¶РєР° РґРёР°РіСЂР°Рј
+;	tab_*.inc	- РїСЂРѕС†РµРґСѓСЂС‹ РѕС‚СЂРёСЃРѕРІРєРё СЃРѕРѕС‚РІРµС‚СЃС‚РІСѓСЋС€РёС… РІРєР»Р°РґРѕРє
 
 
-; Идентификаторы кнопок :
+; Identifier buttons :
 ;	01 - Exit
 ;	02 - Info tab
 ;	03 - Tests tab
@@ -33,7 +33,7 @@
 ;	10 - MMX test	- not used (tab_test.inc)
 ;	11 - Update time increment (tab_conf.inc)
 ;	12 - Update time decriment (tab_conf.inc)
-;	13..17 - Изменение цвета CPU_load, Mem_usage, Temp1, Temp2, Temp3
+;	13..17 - Color Changing buttons CPU_load, Mem_usage, Temp1, Temp2, Temp3
 
 include 'lang.inc'
 include 'config.inc'
@@ -44,22 +44,22 @@ atcol	equ	0x4070c0
 val_mod equ	0x3090
 
 MEOS_APP_START
-include "tests.inc"	; <-- Много выравниваний, лучше здесь
+include "tests.inc"	; <-- РњРЅРѕРіРѕ РІС‹СЂР°РІРЅРёРІР°РЅРёР№, Р»СѓС‡С€Рµ Р·РґРµСЃСЊ
 CODE
 ;--- Init ------------------------------------------------------------
-	; Очищаем неинициализированные данные
+	; РћС‡РёС‰Р°РµРј РЅРµРёРЅРёС†РёР°Р»РёР·РёСЂРѕРІР°РЅРЅС‹Рµ РґР°РЅРЅС‹Рµ
 	mov	ecx, __memory - __udata
 	mov	edi, __udata
 	xor	eax, eax
 	rep	stosb
 
 	xor	eax, eax
-	cpuid					; Узнаём производителя проца
+	cpuid					; РЈР·РЅР°С‘Рј РїСЂРѕРёР·РІРѕРґРёС‚РµР»СЏ РїСЂРѕС†Р°
 	mov	[Vendor + 0], ebx
 	mov	[Vendor + 4], edx
 	mov	[Vendor + 8], ecx
 	mov	eax, 1
-	cpuid					; Есть ли MMX ?
+	cpuid					; Р•СЃС‚СЊ Р»Рё MMX ?
 	mov	ecx, eax
 	shr	al, 4
 	and	ax, 0x0F0F
@@ -72,26 +72,26 @@ CODE
 	and	cl, 11b
 	mov	[CPU_type], cl
 
-	; ! Цвета из массива colors
-	mov	dword[d_cpu_load], 0xFF00	; инициализируем график загрузки АЛУ
+	; ! Р¦РІРµС‚Р° РёР· РјР°СЃСЃРёРІР° colors
+	mov	dword[d_cpu_load], 0xFF00	; РёРЅРёС†РёР°Р»РёР·РёСЂСѓРµРј РіСЂР°С„РёРє Р·Р°РіСЂСѓР·РєРё РђР›РЈ
 	mov	dword[d_mem_usage], 0x8BA169	; -- mem usega
-	mov	dword[d_temp1], 0xFF0000	; -- график температуры ЦПУ
-	mov	dword[d_temp2], 0xFF		; -- график температуры МП
+	mov	dword[d_temp1], 0xFF0000	; -- РіСЂР°С„РёРє С‚РµРјРїРµСЂР°С‚СѓСЂС‹ Р¦РџРЈ
+	mov	dword[d_temp2], 0xFF		; -- РіСЂР°С„РёРє С‚РµРјРїРµСЂР°С‚СѓСЂС‹ РњРџ
 	mov	dword[d_temp3], 0xFF7E23
 
-	call	hwm_init			; Инициализируем чип мониторинга
+	call	hwm_init			; Initialize monitoring chip
 
 	jmp	draw_window
 ;--- Wait & translate event ------------------------------------------
 wait_for_event:
-	mov	byte[diag_add_v], 0	; добавляем данные к графику только по таймеру
+	mov	byte[diag_add_v], 0	; РґРѕР±Р°РІР»СЏРµРј РґР°РЅРЅС‹Рµ Рє РіСЂР°С„РёРєСѓ С‚РѕР»СЊРєРѕ РїРѕ С‚Р°Р№РјРµСЂСѓ
 	mov	ebx, [update_time]
 	mov	eax, 23
 	int	0x40
 	test	eax, eax
 	jnz	@f
 	inc	byte [diag_add_v]
-	inc	eax			; перерисуем окно (eax := 1)
+	jmp	redraw
 @@:	dec	eax			; redraw request ?
 	jz	draw_window
 	dec	eax			; key in buffer ?
@@ -103,7 +103,7 @@ wait_for_event:
 key:
 	mov	eax, 2
 	int	0x40
-	cmp	ah, 27			; Esc - завершение работы
+	cmp	ah, 27			; Esc - shutdown
 	je	exit
 	jmp	wait_for_event
 ;--- Button down -----------------------------------------------------
@@ -118,25 +118,25 @@ exit:	cmp	byte[test_id], 0
 @@:	mov	eax, -1 	; close this program
 	int	0x40
 noexit:
-	; кнопки вкладок (2 .. 5)
+	; button tabs (2 .. 5)
 	cmp	ah, 5
 	jg	no_tabs
 	mov	byte[act_tab], ah
-	jmp	draw_window
+	jmp	redraw_tabs
 no_tabs:
-	; кнопки запуска тество (6 .. 10)
+	; РєРЅРѕРїРєРё Р·Р°РїСѓСЃРєР° С‚РµСЃС‚РІРѕ (6 .. 10)
 	cmp	ah, 10
 	jg	no_test_bt
-	; если нет запущеных тестов - запускаем выбранный
+	; РµСЃР»Рё РЅРµС‚ Р·Р°РїСѓС‰РµРЅС‹С… С‚РµСЃС‚РѕРІ - Р·Р°РїСѓСЃРєР°РµРј РІС‹Р±СЂР°РЅРЅС‹Р№
 	cmp	byte[test_id], 0
 	je	run_test
-	; если кнопка активного теста - останавливаем тест, иначе ничего не делаем
+	; РµСЃР»Рё РєРЅРѕРїРєР° Р°РєС‚РёРІРЅРѕРіРѕ С‚РµСЃС‚Р° - РѕСЃС‚Р°РЅР°РІР»РёРІР°РµРј С‚РµСЃС‚, РёРЅР°С‡Рµ РЅРёС‡РµРіРѕ РЅРµ РґРµР»Р°РµРј
 	cmp	[test_id], ah
 	jne	wait_for_event
 	call	stop_test
-	jmp	draw_window
+	jmp	redraw_tabs
 
-tsts:	dd	p5, p6, k6, k7
+tsts:	dd	p5, p6, k6_, k7_
 run_test:
 	push	eax
 	movzx	ecx, ah
@@ -146,41 +146,41 @@ rtst:	mov	eax, 51
 	mov	ebx, 1
 	int	0x40
 	cmp	eax, 0xFFFFFFF0 ; -16
-	je	wait_for_event	; <-- ошибка запуска теста, ПЕРЕПИСАТЬ
+	je	wait_for_event	; <-- РѕС€РёР±РєР° Р·Р°РїСѓСЃРєР° С‚РµСЃС‚Р°, РџР•Р Р•РџРРЎРђРўР¬
 	mov	[test_pid], eax
 	pop	eax
 	mov	byte [test_id], ah
-	jmp	draw_window
+	jmp	redraw_tabs
 	;-------------------------
-	; кнопки изменения времени обновления
+	; РєРЅРѕРїРєРё РёР·РјРµРЅРµРЅРёСЏ РІСЂРµРјРµРЅРё РѕР±РЅРѕРІР»РµРЅРёСЏ
 no_test_bt:
 	cmp	ah, 11
 	jne	no_ut_inc
 	add	dword[update_time], 10
-	jmp	draw_window
+	jmp	redraw_tabs
 no_ut_inc:
 	cmp	ah, 12
 	jne	no_ut_dec
 	cmp	dword[update_time], 10
 	jng	wait_for_event
 	sub	dword[update_time], 10
-	jmp	draw_window
+	jmp	redraw_tabs
 no_ut_dec:
 	;-------------------------
-	; кнопки изменения цвета графиков (13..17)
+	; color change button schedules (13..17)
 	cmp	ah, 17
 	jg	no_change_color
 	sub	ah, 13
 	movzx	ecx, ah
 	mov	esi, [d_colors + ecx * 4]
 	mov	eax, [esi]
-	mov	ecx, (colors_end - colors) / 4	; количество цветов
+	mov	ecx, (colors_end - colors) / 4	; number of colors
 @@:	dec	ecx
 	cmp	eax, [colors + ecx * 4]
 	jne	@b
 	mov	ebx, [colors + ecx * 4 - 4]
 	mov	[esi], ebx
-	jmp	draw_window
+	jmp	redraw_tabs
 no_change_color:
 	jmp	wait_for_event
 ;---------------------------------------------------------------------
@@ -188,19 +188,147 @@ draw_window:
 	mov	eax,12
 	mov	ebx,1
 	int	0x40
-	; Создаём окно
+	; РЎРѕР·РґР°С‘Рј РѕРєРЅРѕ
 	xor	eax, eax
 	mov	ebx, 300 * 65536 + 309
 	mov	ecx, 150 * 65536 + 450
 	mov	edx, tcol + 0x14000000	; color of work area RRGGBB,8->color
 	mov	edi, title		; color of frames    RRGGBB
 	int	0x40
-	; Создаём рамку графика
+	; Create a graphic frame (not for graph)
 	mov	eax, 13
 	mov	ebx, 10 * 65536 + 290
 	mov	ecx, 138 * 65536 + 105
 	mov	edx, atcol
 	int	0x40
+
+	; write parameter names (Cpu load, temper, etc ...)
+	mov	edx, msgs_mon         ; text for indicators
+	mov	ebx, 17 * 65536 + 30
+	call    show_text
+
+	mov	edx, msgs_mon2
+	mov	ebx, 166 * 65536 + 30
+	call	show_text
+
+	jmp draw_tabs ; draw tabs for the first time
+
+redraw_tabs: ; delete previously created tabs
+
+	mov	ecx, 4
+	mov	eax, 8
+	mov	edx, 0x80000000 + 2
+del_tabs:
+	int	0x40
+	inc	edx
+	loop	del_tabs
+
+	; Delete color change buttons
+	mov	eax, 8
+	mov	edx, 0x80000000 + 13
+@@: 
+	int	0x40
+	inc	edx
+	cmp	edx, 0x80000000 + 13 + 5 	; 5 (buttons)
+	jne	@b
+
+draw_tabs:
+
+	; Drawing color change buttons
+	mov	eax, 8
+	mov	ebx, 112 * 65536 + 5
+	mov	ecx, 30 * 65536 + 6
+	mov	edx, 13
+
+@@: 
+	mov	esi, [d_colors - 13 * 4 + edx * 4]
+	mov	esi, [esi]
+	int	0x40
+	add	ecx, 10 * 65536
+	inc	edx
+	cmp	edx, 13 + 5             ; 5 (buttons)
+	jne	@b
+
+	; info, test, config, about buttons
+	; Create 4 buttons
+	mov	edi, 250 * 65536 + 25
+	mov	ecx, 4
+	mov	eax, 8
+	mov	ebx, 10 * 65536 + 70
+	mov	edx, 2
+n_bt:   
+	push    ecx
+	mov	ecx, edi
+	mov	esi, tcol
+	cmp	byte [act_tab], dl
+	jne	na1
+	mov	esi, atcol
+na1:    
+	int	0x40
+	inc	edx
+	add	ebx, 73 * 65536
+	pop	ecx
+	loop    n_bt
+
+	; write name tabs
+	mov	eax, 4
+	mov	ebx, (18 shl 16) + 256
+	mov	edx, tab_lab
+	mov	ecx, 0x10000000
+	mov	esi, 4
+	int	0x40
+	add	ebx, (72 shl 16)
+	mov	edx, tab_lab.2
+	mov	esi, 5
+	int	0x40
+	add	ebx, (72 shl 16)
+	mov	edx, tab_lab.3
+	mov	esi, 7
+	int	0x40
+	add	ebx, (72 shl 16)
+	mov	edx, tab_lab.4
+	mov	esi, 5
+	int	0x40
+
+	; create a frame at the bottom of the screen
+	mov	eax, 13
+	mov	ebx, 10 * 65536 + 290
+	mov	ecx, 275 * 65536 + 160
+	mov	edx, atcol
+	int	0x40
+	mov	ebx, 12 * 65536 + 286
+	mov	ecx, 277 * 65536 + 156
+	mov	edx, tcol
+	int	0x40
+
+	; draw the selected tab
+	mov	al, [act_tab]
+	cmp	al, 2
+	je 	info_tab
+	cmp	al, 3
+	je 	test_tab
+	cmp	al, 4
+	je 	config_tab
+	jmp	about_tab
+	
+;--- Drow Info tab ---------------
+info_tab:
+        include "tab_info.inc"
+;--- Drow test tab ---------------
+test_tab:
+        include "tab_test.inc"
+;--- Drow Config tab -------------
+config_tab:
+        include "tab_conf.inc"
+;--- Drow About tab --------------
+about_tab:
+        include "tab_about.inc"
+
+redraw:
+
+	; frame for graph
+
+	mov	eax, 13
 	mov	ebx, 12 * 65536 + 286
 	mov	ecx, 140 * 65536 + 101
 	mov	edx, tcol
@@ -211,48 +339,27 @@ draw_window:
 	mov	ecx, 140 * 65536 + 140
 @@:	add	ecx, 10 * 65536 + 10
 	int	0x40
-	cmp	cx, 230 		; привязано к координатам окна
+	cmp	cx, 230 		; РїСЂРёРІСЏР·Р°РЅРѕ Рє РєРѕРѕСЂРґРёРЅР°С‚Р°Рј РѕРєРЅР°
 	jl	@b
 
-	; Пишим названия параметров (Cpu load, temper, etc...)
-	mov	edx, msgs_mon
-	mov	ebx, 17 * 65536 + 30
-	call	show_text
-
-	mov	edx, msgs_mon2
-	mov	ebx, 156 * 65536 + 30
-	call	show_text
-
-	; Рисуем кнопки смены цвета
-	mov	eax, 8
-	mov	ebx, 112 * 65536 + 5
-	mov	ecx, 30 * 65536 + 6
-	mov	edx, 13
-@@:	mov	esi, [d_colors - 13 * 4 + edx * 4]
-	mov	esi, [esi]
-	int	0x40
-	add	ecx, 10 * 65536
-	inc	edx
-	cmp	edx, 13 + 5		; 5 кнопок
-	jne	@b
-
-	; Пишим загрузку процессора
-	call	cpu_usage
-	;--- добавляем на график ---
-	mov	esi, d_cpu_load
-	call	d_add
-	call	d_show
+	; write CPU usage
+	call    cpu_usage
+	;--- РґРѕР±Р°РІР»СЏРµРј РЅР° РіСЂР°С„РёРє ---
+	mov esi, d_cpu_load
+	call    d_add
+	call    d_show
 	;---------------------------
 	movzx	ecx, al
 	mov	eax, 47
 	mov	edx, 121 * 65536 + 30
 	xor	esi, esi
 	call	digit_len
+	clear_prev_no edx
 	int	0x40
 
-	; Пишим используемую память
+	; write used memory
 	call	mem_usage
-	;--- добавляем на график ---
+	;--- РґРѕР±Р°РІР»СЏРµРј РЅР° РіСЂР°С„РёРє ---
 	mov	esi, d_mem_usage
 	call	d_add
 	call	d_show
@@ -262,12 +369,13 @@ draw_window:
 	mov	edx, 121 * 65536 + 40
 	xor	esi, esi
 	call	digit_len
+	clear_prev_no edx
 	int	0x40
 
 	cmp	byte[hwm_enable], 0
 	jne	show_mon
-	; Нет датчиков - пишем NO
-	mov	ecx, 8	;<- количество параметров
+	; РќРµС‚ РґР°С‚С‡РёРєРѕРІ - РїРёС€РµРј NO
+	mov	ecx, 8	;<- number of parameters
 	mov	eax, 4
 	mov	ebx, 121 * 65536 + 50
 	mov	edx, msg_no
@@ -276,7 +384,7 @@ draw_window:
 	add	ebx, 10
 	loop	@b
 
-	mov	ecx, 7	;<- количество параметров
+	mov	ecx, 7	;<- number of parameters
 	mov	ebx, 260 * 65536 + 30
 @@:	int	0x40
 	add	ebx, 10
@@ -284,10 +392,10 @@ draw_window:
 
 	jmp	no_monitor
 show_mon:
-	;Есть датчики пишим инфу
+	;Р•СЃС‚СЊ РґР°С‚С‡РёРєРё РїРёС€РёРј РёРЅС„Сѓ
 	call	hwm_get_params
 	;---------------------------
-	; Температуру
+	; РўРµРјРїРµСЂР°С‚СѓСЂСѓ
 	mov	ecx, 3
 	mov	esi, hwm_temps
 	mov	edx, 121 * 65536 + 50
@@ -295,7 +403,7 @@ show_mon:
 sh_temp:push	ecx
 	lodsw
 	push	esi
-	; реакция на изменения
+	; СЂРµР°РєС†РёСЏ РЅР° РёР·РјРµРЅРµРЅРёСЏ
 	xor	esi, esi
 	cmp	[old_t1 + ecx * 4 - 4], eax
 	je	@f
@@ -305,6 +413,7 @@ sh_temp:push	ecx
 	push	eax
 	mov	eax, 47
 	mov	ebx, 0x20000
+	clear_prev_no edx
 	int	0x40
 	pop	eax
 	mov	cl, ah
@@ -316,31 +425,31 @@ sh_temp:push	ecx
 	pop	esi
 	pop	ecx
 	loop	sh_temp
-	;;--- добавляем на график temp1 ---
+	;;--- Add to the chart temp1 ---
 	movzx	eax, word[hwm_temps]
 	mov	esi, d_temp1
 	call	d_add
 	call	d_show
-	;;--- добавляем на график temp2 ---
+	;;--- Add to the chart temp2 ---
 	movzx	eax, word[hwm_temps + 2]
 	mov	esi, d_temp2
 	call	d_add
 	call	d_show
-	;;--- добавляем на график temp3 ---
+	;;--- Add to the chart temp3 ---
 	movzx	eax, word[hwm_temps + 4]
 	mov	esi, d_temp3
 	call	d_add
 	call	d_show
 
 	;---------------------------
-	; Скорости врашения вентилей
+	; РЎРєРѕСЂРѕСЃС‚Рё РІСЂР°С€РµРЅРёСЏ РІРµРЅС‚РёР»РµР№
 	mov	ecx, 5
 	mov	esi, hwm_rpms
 	mov	edx, 121 * 65536 + 80
 sh_fan: push	ecx
 	lodsd
 	push	esi
-	;реакция на изменения
+	;СЂРµР°РєС†РёСЏ РЅР° РёР·РјРµРЅРµРЅРёСЏ
 	xor	esi, esi
 	cmp	[old_f1 + ecx * 4 - 4], eax
 	je	@f
@@ -349,6 +458,7 @@ sh_fan: push	ecx
 @@:	mov	ecx, eax
 	call	digit_len
 	mov	eax, 47
+	clear_prev_no edx
 	int	0x40
 	add	edx, 10
 	pop	esi
@@ -356,7 +466,7 @@ sh_fan: push	ecx
 	loop	sh_fan
 
 	;---------------------------
-	; Напруги
+	; РќР°РїСЂСѓРіРё
 	mov	esi, hwm_voltages
 	mov	edi, old_volts
 	mov	ecx, 7
@@ -366,7 +476,7 @@ volt_nxt:
 	push	ecx
 	push	esi
 	call	ftoa
-	;реакция на изменения
+	;СЂРµР°РєС†РёСЏ РЅР° РёР·РјРµРЅРµРЅРёСЏ
 	xor	ecx, ecx
 	fld	dword [esi]
 	fsub	dword [edi]
@@ -381,7 +491,7 @@ volt_nxt:
 	sahf
 	jc	@f
 	mov	ecx, val_mod
-@@:	mov	esi, Data_String	; считаем кол-во символов
+@@:	mov	esi, Data_String	; СЃС‡РёС‚Р°РµРј РєРѕР»-РІРѕ СЃРёРјРІРѕР»РѕРІ
 @@:	lodsb
 	test	al, al
 	jnz	@b
@@ -397,78 +507,16 @@ volt_nxt:
 
 no_monitor:
 	;---------------------------
-	; Создаём кнопки вкладок
-	mov	edi, 250 * 65536 + 25
-	mov	ecx, 4
-	mov	eax, 8
-	mov	ebx, 10 * 65536 + 70
-	mov	edx, 2
-n_bt:	push	ecx
-	mov	ecx, edi
-	mov	esi, tcol
-	cmp	byte [act_tab], dl
-	jne	na1
-	mov	esi, atcol
-na1:	int	0x40
-	inc	edx
-	add	ebx, 73 * 65536
-	pop	ecx
-	loop	n_bt
-	; Пишим названия вкладок
-	mov	eax, 4
-	mov	ebx, (18 shl 16) + 256
-	mov	edx, tab_lab
-	mov	ecx, 0x10000000
-	mov	esi, 4
-	int	0x40
-	add ebx, (72 shl 16)
-	mov edx, tab_lab.2
-	mov	esi, 5
-	int	0x40
-	add ebx, (72 shl 16)
-	mov edx, tab_lab.3
-	mov	esi, 7
-	int	0x40
-	add ebx, (72 shl 16)
-	mov edx, tab_lab.4
-	mov	esi, 5
-	int	0x40
-	; создаём рамку внизу экрана
-	mov	eax, 13
-	mov	ebx, 10 * 65536 + 290
-	mov	ecx, 275 * 65536 + 160
-	mov	edx, atcol
-	int	0x40
-	mov	ebx, 12 * 65536 + 286
-	mov	ecx, 277 * 65536 + 156
-	mov	edx, tcol
-	int	0x40
-	; рисуем выбранную вкладку
-	mov	al, [act_tab]
+	mov	al, [act_tab]   ; check if info tab is active
 	cmp	al, 2
-	je	info_tab
-	cmp	al, 3
-	je	test_tab
-	cmp	al, 4
-	je	config_tab
-	jmp	about_tab
+	jne	end_drow_tab
+	jmp	free_mem    ; in tab_info.inc
+
 end_drow_tab:
 	mov	eax, 12
 	mov	ebx, 2
 	int	0x40
 	jmp	wait_for_event
-;--- Drow Info tab ---------------
-info_tab:
-	include "tab_info.inc"
-;--- Drow test tab ---------------
-test_tab:
-	include "tab_test.inc"
-;--- Drow Config tab -------------
-config_tab:
-	include "tab_conf.inc"
-;--- Drow About tab --------------
-about_tab:
-	include "tab_about.inc"
 ;---------------------------------------------------------------------
 	include "system.inc"
 	include "hwm.inc"
@@ -476,7 +524,7 @@ about_tab:
 
 
 DATA
-act_tab 	db	2			; Номер активной вкладки
+act_tab 	db	2			; active tab number
 tab_lab:
 .1: db 'Info'
 .2: db 'Tests'
@@ -508,19 +556,19 @@ msgs_mon2 mls \
 msg_yes 	db	'Yes'
 msg_no		db	'N/A'
 
-update_time	dd	300			; период обновления в сотых долях секунды
+update_time	dd	300			; РїРµСЂРёРѕРґ РѕР±РЅРѕРІР»РµРЅРёСЏ РІ СЃРѕС‚С‹С… РґРѕР»СЏС… СЃРµРєСѓРЅРґС‹
 d_colors	dd	d_cpu_load, d_mem_usage, d_temp1, d_temp2, d_temp3
 c_eps		dd	0.01
 
-; Цвета графиков
-colors: 	dd	0xff0000, 0xff00, 0xff, 0xffffff, tcol, 0xFF7E23, 0x8BA169, 0xff0000	; первый и последний равны
+; Р¦РІРµС‚Р° РіСЂР°С„РёРєРѕРІ
+colors: 	dd	0xff0000, 0xff00, 0xff, 0xffffff, tcol, 0xFF7E23, 0x8BA169, 0xff0000	; РїРµСЂРІС‹Р№ Рё РїРѕСЃР»РµРґРЅРёР№ СЂР°РІРЅС‹
 colors_end:
 ;---------------------------------------------------------------------
 UDATA
 
-test_pid	dd	?			; если test_id == 0 - не имеет смысла
-test_id 	db	?			; 0 - тест не запушен (соответствует № кнопки 6..10)
-diag_add_v	db	?			; 0 - не добавлять данные на график, !0 - добавлять
+test_pid	dd	?			; РµСЃР»Рё test_id == 0 - РЅРµ РёРјРµРµС‚ СЃРјС‹СЃР»Р°
+test_id 	db	?			; 0 - С‚РµСЃС‚ РЅРµ Р·Р°РїСѓС€РµРЅ (СЃРѕРѕС‚РІРµС‚СЃС‚РІСѓРµС‚ в„– РєРЅРѕРїРєРё 6..10)
+diag_add_v	db	?			; 0 - РЅРµ РґРѕР±Р°РІР»СЏС‚СЊ РґР°РЅРЅС‹Рµ РЅР° РіСЂР°С„РёРє, !0 - РґРѕР±Р°РІР»СЏС‚СЊ
 
 Vendor: 	times	12 db ?
 CPU_fam:	db	?
@@ -528,8 +576,8 @@ CPU_mod:	db	?
 CPU_type:	db	?
 CPU_mmx:	db	?
 
-; Для реакции на изменения
-old_t1: 	dd	?	; последнее значение temp1
+; Р”Р»СЏ СЂРµР°РєС†РёРё РЅР° РёР·РјРµРЅРµРЅРёСЏ
+old_t1: 	dd	?	; РїРѕСЃР»РµРґРЅРµРµ Р·РЅР°С‡РµРЅРёРµ temp1
 old_t2: 	dd	?	; ...
 old_t3: 	dd	?
 old_f1: 	dd	?
@@ -537,7 +585,7 @@ old_f2: 	dd	?
 old_f3: 	dd	?
 old_volts:	times	7 dd ?
 
-; Грфики
+; Р“СЂС„РёРєРё
 diag_beg:
 d_cpu_load:	dd	?
 		times 286 db ?
@@ -555,7 +603,7 @@ d_temp3:	dd	?
 		times 286 db ?
 diag_end:
 
-; --- стек теста, воизбежание глюков не переносить в другое место ----
+; --- СЃС‚РµРє С‚РµСЃС‚Р°, РІРѕРёР·Р±РµР¶Р°РЅРёРµ РіР»СЋРєРѕРІ РЅРµ РїРµСЂРµРЅРѕСЃРёС‚СЊ РІ РґСЂСѓРіРѕРµ РјРµСЃС‚Рѕ ----
 th_stack:	times	1024 db ?
 stp:
 ;---------------------------------------------------------------------
