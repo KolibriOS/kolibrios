fe26455cecb1357bd8d53f2e7bc92cc8c4aac6ce
CleverMouse <theclevermouse@yandex.ru>
2016-03-22 18:21:42 +0000

    revert r6361 since it breaks autobuild
    git-svn-id: svn://kolibrios.org@6363 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/games/FindNumbers/trunk/Tupfile.lua
+++ programs/games/FindNumbers/trunk/Tupfile.lua
@@ -1,6 +1,2 @@
 if tup.getconfig("NO_CMM") ~= "" then return end
-if tup.getconfig("LANG") == "ru"
-then C_LANG = "LANG_RUS"
-else C_LANG = "LANG_ENG" -- this includes default case without config
-end
-tup.rule("FindNumbers.c--", "c-- /D=AUTOBUILD /D=$(C_LANG) %f" .. tup.getconfig("KPACK_CMD"), "FindNumbers.com")
+tup.rule("FindNumbers.c--", "c-- %f" .. tup.getconfig("KPACK_CMD"), "FindNumbers")
