b718363009b934982907c5f93783385d432bfad4
Artem Jerdev (art_zh) <artem@jerdev.co.uk>
2016-06-07 15:14:38 +0000

    VHDL code generator for PCI BIOS extension ROMs
    git-svn-id: svn://kolibrios.org@6442 a494cfbc-eb01-0410-851d-a64ba20cac60

--- kernel/branches/Kolibri-A/trunk/bootloader/bios-bev/bev.bat
+++ kernel/branches/Kolibri-A/trunk/bootloader/bios-bev/bev.bat
@@ -1,1 +1,11 @@
-fasm bev.asm
+fasm pcirom.asm
+fasm step1.asm
+fasm step2.asm
+ren step2.bin pcirom.vhd
+del step1.bin
+del pcirom.bin
+echo off
+echo ********************************
+echo     (C) Artem Jerdev, 2014
+echo PCI ROM VHDL Template generated
+pause
