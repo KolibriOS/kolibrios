d47fdfc83ffc47c453c071c15680e9568c824e54
hidnplayr <hidnplayr@kolibrios.org>
2016-08-15 18:57:21 +0000

    Fixed rshell tupfile
    git-svn-id: svn://kolibrios.org@6478 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/network/rshell/Tupfile.lua
+++ programs/network/rshell/Tupfile.lua
@@ -1,2 +1,2 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
-tup.rule("tcpserv.asm", "fasm %f %o " .. tup.getconfig("KPACK_CMD"), "tcpserv")
+tup.rule("rshell.asm", "fasm %f %o " .. tup.getconfig("KPACK_CMD"), "rshell")
