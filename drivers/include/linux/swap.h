640dae6ddffdffef3e095ae97423b96db2564fd8
Sergey Semyonov (Serge) <ion2@mail.ru>
2016-09-08 12:18:08 +0000

    ddk-v4.4.20
    git-svn-id: svn://kolibrios.org@6514 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ drivers/include/linux/swap.h
@@ -0,0 +1,22 @@
+#ifndef _LINUX_SWAP_H
+#define _LINUX_SWAP_H
+
+#include <linux/spinlock.h>
+
+struct notifier_block;
+
+struct bio;
+
+#define SWAP_FLAG_PREFER	0x8000	/* set if swap priority specified */
+#define SWAP_FLAG_PRIO_MASK	0x7fff
+#define SWAP_FLAG_PRIO_SHIFT	0
+#define SWAP_FLAG_DISCARD	0x10000 /* enable discard for swap */
+#define SWAP_FLAG_DISCARD_ONCE	0x20000 /* discard swap area at swapon-time */
+#define SWAP_FLAG_DISCARD_PAGES 0x40000 /* discard page-clusters after use */
+
+#define SWAP_FLAGS_VALID	(SWAP_FLAG_PRIO_MASK | SWAP_FLAG_PREFER | \
+				 SWAP_FLAG_DISCARD | SWAP_FLAG_DISCARD_ONCE | \
+				 SWAP_FLAG_DISCARD_PAGES)
+
+
+#endif /* _LINUX_SWAP_H */
