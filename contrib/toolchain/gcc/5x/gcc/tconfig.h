c7fc8e91d00bc9ea1139f33b192783dedeb38b54
Sergey Semyonov (Serge) <ion2@mail.ru>
2016-09-08 17:51:39 +0000

    libgcc-5.4.0 initial commit
    git-svn-id: svn://kolibrios.org@6515 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ contrib/toolchain/gcc/5x/gcc/tconfig.h
@@ -0,0 +1,11 @@
+#ifndef GCC_TCONFIG_H
+#define GCC_TCONFIG_H
+#ifndef USED_FOR_TARGET
+# define USED_FOR_TARGET
+#endif
+#include "auto-host.h"
+#ifdef IN_GCC
+# include "ansidecl.h"
+# include "config/i386/xm-mingw32.h"
+#endif
+#endif /* GCC_TCONFIG_H */
