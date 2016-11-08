430ba6e3a74e92a0f9f6a3e6dc0479c80140a687
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2016-11-08 12:39:23 +0000

    autobuild tooltip_demo.asm
    git-svn-id: svn://kolibrios.org@6692 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/libraries/box_lib/asm/trunk/Tupfile.lua
+++ programs/develop/libraries/box_lib/asm/trunk/Tupfile.lua
@@ -1,2 +1,2 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
-tup.foreach_rule({"ctrldemo.asm", "editbox_ex.asm"}, "fasm %f %o " .. tup.getconfig("KPACK_CMD"), "%B")
+tup.foreach_rule({"ctrldemo.asm", "editbox_ex.asm", "tooltip_demo.asm"}, "fasm %f %o " .. tup.getconfig("KPACK_CMD"), "%B")
--- programs/develop/libraries/box_lib/asm/trunk/build.bat
+++ programs/develop/libraries/box_lib/asm/trunk/build.bat
@@ -1,12 +1,11 @@
-REM compiling examples
+@REM compiling examples
+@echo off
 if not exist bin mkdir bin
-
-@fasm.exe  -m 16384 ctrldemo.asm bin\ctrldemo.kex
-@kpack bin\ctrldemo.kex
 
 @copy reload_16x16_8b.png bin\reload_16x16_8b.png
 
+@fasm.exe  -m 16384 ctrldemo.asm bin\ctrldemo.kex
 @fasm.exe  -m 16384 editbox_ex.asm bin\editbox_ex.kex
-@kpack bin\editbox_ex.kex
+@fasm.exe  -m 16384 tooltip_demo.asm bin\tooltip_demo.kex
 
 @pause
