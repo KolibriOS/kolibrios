733620f72e982899c5c5d0d23b85a695ab0eda05
Ivan Baravy <dunkaist@gmail.com>
2017-01-20 14:06:09 +0000

    Fix ROOT path variable
    git-svn-id: svn://kolibrios.org@6841 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/system/os/Tupfile.lua
+++ programs/system/os/Tupfile.lua
@@ -1,3 +1,3 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
-ROOT="../../../.."
+ROOT="../../.."
 tup.rule("kolibri.asm", "fasm %f %o " .. tup.getconfig("PESTRIP_CMD") .. tup.getconfig("KPACK_CMD"), "kolibri.dll")
