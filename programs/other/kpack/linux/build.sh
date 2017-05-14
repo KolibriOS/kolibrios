7d2d9814de686c3bfba4ab7c1a049f3c88755c67
ashmew2 <ashmew2@kolibrios.org>
2017-05-14 13:00:06 +0000

    Fix building kpack on x86_64 linuxes
    git-svn-id: svn://kolibrios.org@6896 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/other/kpack/linux/build.sh
+++ programs/other/kpack/linux/build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 # This script does for linux the same as build.bat for DOS,
-# it compiles the KoOS kernel, hopefully ;-)
+# it compiles the kpack program, hopefully ;-)
 
 fasm -m 16384 kpack.asm kpack.o
-gcc -s -nostdlib kpack.o -o kpack -lc
+gcc -m32 -s -nostdlib -march=i386 kpack.o -o kpack -lc
 strip -R .comment -R .gnu.version kpack
-exit 0
-
-
-
-
+rm -f kpack.o
