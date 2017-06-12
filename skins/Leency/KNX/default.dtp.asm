30022f1ed8e01de9206c5caaeaef4e43a1ba9fdf
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2017-06-12 11:57:30 +0000

    upload skin KNX
    git-svn-id: svn://kolibrios.org@6925 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ skins/Leency/KNX/default.dtp.asm
@@ -0,0 +1,14 @@
+struc system_colors
+ {
+  .taskbar          dd 0x586786
+  .taskbar_text     dd 0xFEFEFE
+  .work_3d_dark     dd 0xC0BBB4
+  .work_3d_light    dd 0xECE9E5
+  .window_title     dd 0xFFFfff
+  .work             dd 0xDDD7CF
+  .work_button      dd 0x999999
+  .work_button_text dd 0xFEFEFE
+  .work_text        dd 0x000000
+  .work_graph       dd 0x7E7E7E
+ }
+struct system_colors
