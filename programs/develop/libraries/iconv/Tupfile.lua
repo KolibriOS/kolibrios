bd2c40668cc5004f9dc7a90807e2e64438290e76
ashmew2 <ashmew2@kolibrios.org>
2017-10-03 22:00:44 +0000

    Fix Tupfile.lua : Use sources from contrib newlib
    git-svn-id: svn://kolibrios.org@7047 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/libraries/iconv/Tupfile.lua
+++ programs/develop/libraries/iconv/Tupfile.lua
@@ -1,2 +1,2 @@
 if tup.getconfig("NO_GCC") ~= "" then return end
-tup.rule("iconv.c", "kos32-gcc -c -I../../include -fno-ident -Os -c -o %o %f " .. tup.getconfig("KPACK_CMD"), "iconv.obj")
+tup.rule("iconv.c", "kos32-gcc -c -I../../../../contrib/sdk/sources/newlib/libc/include/ -fno-ident -Os -c -o %o %f " .. tup.getconfig("KPACK_CMD"), "iconv.obj")
