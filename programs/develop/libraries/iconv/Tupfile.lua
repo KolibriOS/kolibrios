879f0b8aa07bfe8c12779de8a9f18eab39618c8a
ashmew2 <ashmew2@kolibrios.org>
2017-10-03 21:58:42 +0000

    Fix Tupfile.lua
    git-svn-id: svn://kolibrios.org@7046 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/libraries/iconv/Tupfile.lua
+++ programs/develop/libraries/iconv/Tupfile.lua
@@ -1,2 +1,2 @@
 if tup.getconfig("NO_GCC") ~= "" then return end
-tup.rule("iconv.c", "kos32-gcc -fno-ident -Os -c -o %o %f " .. tup.getconfig("KPACK_CMD"), "iconv.obj")
+tup.rule("iconv.c", "kos32-gcc -c -I../../include -fno-ident -Os -c -o %o %f " .. tup.getconfig("KPACK_CMD"), "iconv.obj")
