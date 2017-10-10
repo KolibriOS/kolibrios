0aefd474b5e2d3c0ca958c34eba6e78a079f1437
0CodErr <0CodErr@kolibrios.org>
2017-10-10 13:01:17 +0000

    try add Tupfile.lua for RUN application
    git-svn-id: svn://kolibrios.org@7077 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/system/RunOD/1/Tupfile.lua
+++ programs/system/RunOD/1/Tupfile.lua
@@ -10,6 +10,6 @@ else
   tup.definerule{command = "echo LANG_EN = 1 > lang.inc", outputs = {"lang.inc"}}
 end
 
-tup.rule({"RUN.asm", extra_inputs = {"lang.inc"}}, "jwasm -zt0 -coff -Fi lang.inc RUN.asm -Fo RUN.o", "RUN.o")
-tup.rule("RUN.o", "ld -T LScript.x RUN.o -o RUN.kex -L ../../../../contrib/sdk/lib -l KolibriOS", "RUN.kex")
-tup.rule("RUN.kex", "ocopy RUN.kex RUN -O binary -j .all", "RUN")
+tup.rule({"RUN.asm", extra_inputs = {"lang.inc"}}, "jwasm -zt0 -coff -Fi lang.inc RUN.asm -Fo RUN.obj", "RUN.obj")
+tup.rule("RUN.obj", "ld -T LScript.x RUN.obj -o RUN -L ../../../../contrib/sdk/lib -l KolibriOS", "RUN")
+tup.rule("RUN", "objcopy RUN RUN -O binary -j .all", "RUN")
