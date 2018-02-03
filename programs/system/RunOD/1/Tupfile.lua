ba018197c694903e3f0ba4478aedd14eb43adb9a
0CodErr <0CodErr@kolibrios.org>
2018-02-03 09:10:21 +0000

    add CONFIG_NO_JWASM to tup.config.template; check GCC in RunOD tupfile
    git-svn-id: svn://kolibrios.org@7145 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/system/RunOD/1/Tupfile.lua
+++ programs/system/RunOD/1/Tupfile.lua
@@ -1,4 +1,4 @@
-if tup.getconfig('NO_JWASM') ~= "" then return end
+if tup.getconfig('NO_JWASM') ~= "" or tup.getconfig("NO_GCC") ~= "" then return end
 
 if     tup.getconfig("LANG") == "it" then
   tup.definerule{command = "echo LANG_IT = 1 > lang.inc", outputs = {"lang.inc"}}
