b63aa3871785c3833c69a5e1a638e407be41a5a0
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2018-09-10 14:27:14 +0000

    trying to fix LANG 5
    git-svn-id: svn://kolibrios.org@7342 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/games/kosilka/Tupfile.lua
+++ programs/games/kosilka/Tupfile.lua
@@ -2,8 +2,8 @@ if tup.getconfig("NO_FASM") ~= "" or tup.getconfig("NO_MSVC") ~= "" then return 
 HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../.." or tup.getconfig("HELPERDIR")
 tup.include(HELPERDIR .. "/use_msvc.lua")
 if tup.getconfig("LANG") == "ru"
-then CFLAGS = CFLAGS .. "/DLANG_RUS"
-else CFLAGS = CFLAGS .. "/DLANG_ENG"
+then CFLAGS = CFLAGS .. " /DLANG_RUS"
+else CFLAGS = CFLAGS .. " /DLANG_ENG"
 end
 compile_msvc{"kosilka.cpp", "KosFile.cpp", "kosSyst.cpp", "mcsmemm.cpp"}
 link_msvc("kosilka")
