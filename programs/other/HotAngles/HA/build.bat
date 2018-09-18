5a8f5fe87196095f46821097ad90ba42cd85f6f3
theonlymirage <theonlymirage@kolibrios.org>
2018-09-18 17:00:37 +0000

    Updating directory for "Hot Angles".
    git-svn-id: svn://kolibrios.org@7387 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/other/HotAngles/HA/build.bat
+++
@@ -1,5 +0,0 @@
-@echo lang fix ru >lang.inc
-@fasm.exe -m 16384 @HOTANGLES.asm @HOTANGLES
-@erase lang.inc
-@kpack @HOTANGLES
-@pause
--- programs/other/HotAngles/HAconfig/build.bat
+++
@@ -1,5 +0,0 @@
-@echo lang fix ru >lang.inc
-@fasm.exe -m 16384 HACONFIG.asm HACONFIG
-@erase lang.inc
-@kpack HACONFIG
-@pause
