02e226cd9a6fbf94c24bfe656c5f55de8cca9eb6
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2018-10-14 18:05:52 +0000

    ftpc 0.12a: - bigger fonts in editbox - press enter to login - compressed images: 3 sectors instead of 5, system icons
    git-svn-id: svn://kolibrios.org@7464 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/network/ftpc/ftpc.asm
+++ programs/network/ftpc/ftpc.asm
@@ -749,8 +749,8 @@ exit2:
 
 
 ; data
-str_title       db 'FTP client',0
-str_welcome     db 'FTP client for KolibriOS v0.12',10
+str_title       db 'FTP client for KolibriOS',0
+str_welcome     db 'FTP client for KolibriOS v0.12a',10
                 db 10,0
 str_srv_addr    db 'Please enter ftp server address.',10,0
 
