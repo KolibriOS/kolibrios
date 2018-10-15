7cbe11191ac2218b2600823469e67fea1ea72f50
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2018-10-15 14:47:15 +0000

    t_edit: compress images so binary file size decreased from 11558 to 9237, better INI color scheme, set INI associations to TextEdit
    git-svn-id: svn://kolibrios.org@7467 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/other/t_edit/info/ini_files_syn.asm
+++ programs/other/t_edit/info/ini_files_syn.asm
@@ -8,21 +8,26 @@ db p3
 }
 count_colors_text dd (text-color_wnd_text)/4
 count_key_words dd (f1-text)/48
-color_cursor dd 0xffd000
-color_wnd_capt dd 0x0000a0
-color_wnd_work dd 0x000000
-color_wnd_bord dd 0xc0c0c0
-color_select dd 0x000080
-color_cur_text dd 0x8080ff
+color_cursor dd 0xf1fcd0
+color_wnd_capt dd 0x080808 ;sidebar bg
+color_wnd_work dd 0x1C1C1C ;main work area bg
+color_wnd_bord dd 0x606060 ;sidebar text
+color_select dd 0x3E3D32
+color_cur_text dd 0x808080
 color_wnd_text:
-	dd 0x808080
-	dd 0xffffff
+	dd 0xD0D0D0 ;F8F8F2
 	dd 0xffff00
-	dd 0x008080
-	dd 0x0000ff
-	dd 0x0080ff
+	dd 0x00ff00
+	dd 0x00ffff
+	dd 0x808080
+	dd 0xff40ff
+	dd 0x4080ff
+	dd 0xff0000
+	dd 0x8080ff
+	dd 0x00ccff
 text:
 wo<'"'>,0,4,34,3
+wo<';'>,0,4,13,4
 wo<'#'>,0,4,13,4
 wo<'0'>,0,24,,3
 wo<'1'>,0,24,,3
