8313a51c5f923f587dd30b44d0533542def29327
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2018-10-18 20:08:52 +0000

    fix file name
    git-svn-id: svn://kolibrios.org@7478 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/games/rforces/trunk/Tupfile.lua
+++ programs/games/rforces/trunk/Tupfile.lua
@@ -1,5 +1,5 @@
 if tup.getconfig("NO_FASM") ~= "" or tup.getconfig("NO_MSVC") ~= "" then return end
 HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../../.." or tup.getconfig("HELPERDIR")
 tup.include(HELPERDIR .. "/use_msvc.lua")
-compile_msvc{"forces.cpp", "kosFile.cpp", "kosSyst.cpp", "mcsmemm.cpp"}
+compile_msvc{"rforces.cpp", "kosFile.cpp", "kosSyst.cpp", "mcsmemm.cpp"}
 link_msvc("rforces")
