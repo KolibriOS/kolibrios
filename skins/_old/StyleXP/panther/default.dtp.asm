169c7e063975d98a4e2a730f4ca973a30082cde5
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2019-03-10 21:38:01 +0000

    fix 3d light in skins Panther and Lightskin
    git-svn-id: svn://kolibrios.org@7596 a494cfbc-eb01-0410-851d-a64ba20cac60

--- skins/_old/StyleXP/panther/default.dtp.asm
+++ skins/_old/StyleXP/panther/default.dtp.asm
@@ -2,10 +2,10 @@
 frame            	dd 0xCACACA
 grab             	dd 0xE8E8E8
 grab_button      	dd 0xCCCCCC
-grab_button_text 	dd 0x000000
+grab_button_text 	dd 0xF7F5F5
 grab_text        	dd 0x000000
 work             	dd 0xE8E8E8
-work_button      	dd 0xE8E8E8
+work_button      	dd 0xD5CFD8
 work_button_text 	dd 0x000000
 work_text        	dd 0x000000
 work_graph       	dd 0xAAAAAA
--- skins/_old/lightskin/default.dtp.asm
+++ skins/_old/lightskin/default.dtp.asm
@@ -2,7 +2,7 @@
 frame			dd 0x006393
 grab			dd 0x0077b0
 grab_button		dd 0xc0c0c0
-grab_button_text	dd 0x000000
+grab_button_text	dd 0xEEEeee
 grab_text		dd 0xFFFFFF
 work			dd 0xCACACA
 work_button		dd 0xCACACA
