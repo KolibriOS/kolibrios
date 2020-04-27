78c0bb2701f84d2097d079b10239c1e6636e5b5f
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2020-04-27 11:06:08 +0000

    fix vaporwave tupfile
    git-svn-id: svn://kolibrios.org@7825 a494cfbc-eb01-0410-851d-a64ba20cac60

--- skins/Leency/Vaporwave/Tupfile.lua
+++ skins/Leency/Vaporwave/Tupfile.lua
@@ -1,2 +1,3 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
-tup.rule("default.asm", 'fasm "%f" "%o" ' .. tup.getconfig("KPACK_CMD"), "Vaporwave.skn")
+tup.rule("dtp.asm", 'fasm "%f" "%o"', "dtp.dtp")
+tup.rule({"default.asm", extra_inputs = {"dtp.dtp"}}, 'fasm "%f" "%o" ' .. tup.getconfig("KPACK_CMD"), "vaporwave.skn")
