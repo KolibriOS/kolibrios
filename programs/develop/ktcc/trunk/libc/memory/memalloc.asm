46051e254ff72ed9b9bcb39467b6a5d6b8c626a8
Magomed Kostoev (mkostoevr) <mkostoevr@yandex.ru>
2020-04-30 08:47:52 +0000

    Add <dlfcn.h> functions to libck.a (these functions initialize COFF library the same way as dll.inc)
    git-svn-id: svn://kolibrios.org@7847 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/ktcc/trunk/libc/memory/memalloc.asm
+++ programs/develop/ktcc/trunk/libc/memory/memalloc.asm
@@ -9,30 +9,38 @@ public sysrealloc
 align 4
 sysmalloc:
     push ebx
-        mov eax,68
-        mov ebx,12
-	mov ecx,[esp+8] ;size
-        int 0x40
+    push ecx
+    mov eax,68
+    mov ebx,12
+    mov ecx,[esp+12] ;size
+    int 0x40
+    pop ecx
     pop ebx
-        ret 4
+    ret 4
 
 align 4
 sysfree:
     push ebx
-        mov eax,68
-        mov ebx,13
-	mov ecx,[esp+8]
-        int 0x40
+    push ecx
+    mov eax,68
+    mov ebx,13
+    mov ecx,[esp+12]
+    int 0x40
+    pop ecx
     pop ebx
-        ret 4
+    ret 4
 
 align 4
 sysrealloc:
     push ebx
-        mov ebx,20
-        mov eax,68
-	mov edx,[esp+8]  ; pointer
-	mov ecx,[esp+12]  ; size
-        int 0x40
+    push ecx
+    push edx
+    mov eax,68
+    mov ebx,20
+    mov ecx,[esp+20]  ; size
+    mov edx,[esp+16]  ; pointer
+    int 0x40
+    pop edx
+    pop ecx
     pop ebx
-        ret 8
+    ret 8
