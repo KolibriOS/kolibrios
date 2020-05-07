40267e753ce301e662ddaf0100ca7e966bf57160
pavelyakov <pavelyakov@kolibrios.org>
2020-05-07 08:16:59 +0000

    Upd: function indexArray, add: new example
    git-svn-id: svn://kolibrios.org@7881 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/cmm/lib/array.h
+++ programs/cmm/lib/array.h
@@ -173,13 +173,13 @@
 	RETURN array.init(size);
 }
 
-dword indexArray(dword address, key)
+:dword indexArray(dword address, key)
 {
-	dword offset = key&11b;
+	dword offset = key&0x1FF;
 	dword offsetAddress = offset*4+address;
-	IF (key==offset) RETURN 4*4+offsetAddress;
-	IF (!DSDWORD[offsetAddress]) DSDWORD[offsetAddress] = malloc(4*4*2);
-	RETURN indexArray(DSDWORD[offsetAddress], key>>2);
+	IF (key==offset) RETURN 4*0x200+offsetAddress;
+	IF (!DSDWORD[offsetAddress]) DSDWORD[offsetAddress] = malloc(0x1000);
+	RETURN indexArray(DSDWORD[offsetAddress], key>>9);
 }
 
 #endif
