8e1c5f3ee01ed386235c857866d10c6849d490bf
ConLenov <ConLenov@kolibrios.org>
2020-05-14 13:23:01 +0000

    skins: add FlowerMoon by ConLenov
    git-svn-id: svn://kolibrios.org@7917 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ skins/ConLenov/FlowerMoon/default.dtp.asm
@@ -0,0 +1,11 @@
+;SYSTEM COLORS (.DTP) - COMPILE WITH FASM
+frame            dd 0x29272f
+grab             dd 0x504d59
+grab_button      dd 0xC6C9CB
+grab_button_text dd 0xffffff
+grab_text        dd 0xFFFFFF
+work             dd 0xe8e6ec
+work_button      dd 0xeceaef
+work_button_text dd 0x000000
+work_text        dd 0x000000
+work_graph       dd 0xafa9b9
