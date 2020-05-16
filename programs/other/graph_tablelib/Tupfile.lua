ffdb75ee7fed6aea8c6a3d3b664e24d145bb61f6
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2020-05-16 19:31:04 +0000

    graph_tablelib: remove, non-actual anymore
    git-svn-id: svn://kolibrios.org@7934 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/other/graph_tablelib/Tupfile.lua
+++
@@ -1,7 +0,0 @@
-if tup.getconfig("NO_FASM") ~= "" or tup.getconfig("NO_MSVC") ~= "" then return end
-HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../.." or tup.getconfig("HELPERDIR")
-tup.include(HELPERDIR .. "/use_msvc.lua")
-tup.append_table(OBJS,
-  tup.foreach_rule("*.asm", "fasm %f %o", "%B.obj")
-)
-compile_msvc{"*.cpp"}
