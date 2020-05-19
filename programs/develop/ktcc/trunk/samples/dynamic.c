01b4a611afcc528f61975445e063ac3f6a71fe10
Magomed Kostoev (mkostoevr) <mkostoevr@yandex.ru>
2020-05-19 09:38:59 +0000

    Make examples more presentable.
    git-svn-id: svn://kolibrios.org@7948 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/ktcc/trunk/samples/dynamic.c
+++ programs/develop/ktcc/trunk/samples/dynamic.c
@@ -3,17 +3,18 @@
 #include <kos/inputbox.h>
 
 int main() {
+    con_write_asciiz("Wait, I'll ask you... when I'll done to fetch one site...\n");
     con_set_title("Dynamicaly linked app");
-    con_write_asciiz("Wait, I'll ask you... when I'll done to fetch one site");
-    http_msg *h = get("http://boppan.org", 0, HTTP_FLAG_BLOCK, "");
+    http_msg *h = get("http://example.com", 0, HTTP_FLAG_BLOCK, "");
     if (!receive(h)) {
-    	con_printf("%s\n", h->content_ptr);
+        con_printf("%s\n", h->content_ptr);
     } else {
-        con_write_asciiz("Danmit! Can't access to the page.");
+        con_write_asciiz("Oops! Can't access to the page.\n");
     }
     char buffer[256];
-    InputBox(buffer, "Hay!", "How do you do?", "Everything sucks", 0, 256, 0);
-    con_write_asciiz(buffer);
+    InputBox(buffer, "Hay!", "How do you do?", "Hmm?", 0, 256, 0);
+    con_printf("Your answer is \"%s\"\n", buffer);
+    con_write_string("It's surprising, isn't it?", 26);
     con_exit(0);
     return 0;
 }
