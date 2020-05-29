6c853d0a9c5df1b5b247e23fcf861587344ecbb9
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2020-05-29 22:23:10 +0000

    refine Luna skin
    git-svn-id: svn://kolibrios.org@8007 a494cfbc-eb01-0410-851d-a64ba20cac60

--- skins/Leency/Luna/build.bat
+++ skins/Leency/Luna/build.bat
@@ -1,3 +1,5 @@
+fasm default.dtp.asm default.dtp
 fasm default.asm default.skn
 kpack default.skn
+del default.dtp
 pause
---
+++ skins/Leency/Luna/default.dtp.asm
@@ -0,0 +1,14 @@
+struc system_colors
+ {
+  .null1       dd 0x000000
+  .null2       dd 0x006393
+  .dark        dd 0xD1CFC7
+  .light       dd 0xF2F2F0
+  .grab_text   dd 0xFFFFFF
+  .work        dd 0xE3E2DD
+  .button      dd 0x7794CA
+  .button_text dd 0xFFFfff
+  .work_text   dd 0x000000
+  .graph       dd 0x6380B1
+ }
+struct system_colors
