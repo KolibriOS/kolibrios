8e7422faef3913e4d70bd714df662cb55d589439
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2020-05-29 22:26:59 +0000

    autobuild default.dtp
    git-svn-id: svn://kolibrios.org@8009 a494cfbc-eb01-0410-851d-a64ba20cac60

--- skins/Leency/Luna/Tupfile.lua
+++ skins/Leency/Luna/Tupfile.lua
@@ -1,2 +1,3 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
-tup.rule("default.asm", 'fasm "%f" "%o" ' .. tup.getconfig("KPACK_CMD"), "Luna.skn")
+tup.rule("default.dtp.asm", 'fasm "%f" "%o"', "default.dtp")
+tup.rule({"default.asm", extra_inputs = {"default.dtp"}}, 'fasm "%f" "%o" ' .. tup.getconfig("KPACK_CMD"), "Luna.skn")
