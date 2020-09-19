51715b6c5549ab34407dcd7a8270ff4be9b39e3a
ConLenov <ConLenov@kolibrios.org>
2020-09-19 08:54:34 +0000

    Skins: add Guzel and Guzel_magic by ConLenov
    git-svn-id: svn://kolibrios.org@8065 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ skins/ConLenov/Guzel/build.bat
@@ -0,0 +1,5 @@
+@del default.skn
+@fasm default.dtp.asm default.dtp
+@fasm Guzel.asm default.skn
+@del default.dtp
+ 
---
+++ skins/ConLenov/Guzel/default.dtp.asm
@@ -0,0 +1,14 @@
+struc system_colors
+{
+.frame            dd 0x006393
+.grab             dd 0x0077b0
+.work_dark        dd 0xA5A5A5
+.work_light       dd 0xE4E4E4
+.title_text       dd 0x31363D
+.work             dd 0xE6E6E6
+.work_button      dd 0xEAEFF3
+.work_button_text dd 0x000000
+.work_text        dd 0x000000
+.work_graph       dd 0xC8C8C8
+}
+struct system_colors
