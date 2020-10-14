df0d0be7d420c24f953ef37966f8226e2ce22acf
superturbocat2001 <superturbocat2001@kolibrios.org>
2020-10-14 00:28:46 +0000

    Added "libmsgbox" to ktcc. Added examples for working with coff-libraries and "libmsgbox" to ktcc. Modified binutils to work normally via console.obj.
    git-svn-id: svn://kolibrios.org@8098 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/ktcc/trunk/samples/build_all.sh
+++ programs/develop/ktcc/trunk/samples/build_all.sh
@@ -4,4 +4,6 @@
 ../tcc files.c /kolibrios/develop/tcc/lib/libck.a -o files
 ../tcc winbasics.c /kolibrios/develop/tcc/lib/libck.a -o winbasics
 ../tcc dynamic.c -lconsole -lhttp -linputbox -o dynamic
+../tcc load_coff.c -o load_coff -lck
+../tcc msgbox.c  -lck -lmsgbox -o msgbox
 exit
---
+++ programs/develop/ktcc/trunk/samples/msgbox.c
@@ -0,0 +1,8 @@
+#include <kos/msgbox.h>
+
+int main()
+{
+    msgbox *msg1=NULL;
+    msg1 = kolibri_new_msgbox("MsgBoxTest", "Hello world!", 0, "ok");
+    kolibri_start_msgbox(msg1, NULL);
+}
