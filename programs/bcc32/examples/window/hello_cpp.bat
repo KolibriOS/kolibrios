3fa2b1128511be37c83624567b0f310629d106bf
maxcodehack <maxcodehack@kolibrios.org>
2020-11-06 15:46:26 +0000

    Fix bat
    git-svn-id: svn://kolibrios.org@8147 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/bcc32/examples/window/hello_cpp.bat
+++ programs/bcc32/examples/window/hello_cpp.bat
@@ -1,9 +1,10 @@
-kos32-bcc -S -v- -R- -6 -a4 -O2 -Og -Oi -Ov -OS -k- -D__KOLIBRI__ -Iinclude hello.cpp
+kos32-bcc -S -v- -R- -6 -a4 -O2 -Og -Oi -Ov -OS -k- -D__KOLIBRI__ -I..\..\..\bcc32\include hello.cpp
 
-echo STACKSIZE equ 102400 \n HEAPSIZE equ 102400 \n include "..\..\..\proc32.inc" \ninclude "$(INCLUDE)/kos_start.inc" \n include "$(INCLUDE)/kos_func.inc" \n include "$(INCLUDE)/kos_heap.inc" > kos_make.inc
+echo STACKSIZE equ 102400 \n HEAPSIZE equ 102400 \n include "..\..\..\proc32.inc" \ninclude "..\..\..\bcc32\include\kos_start.inc" \n include "..\..\..\bcc32\include\kos_func.inc" \n include "..\..\..\bcc32\include\kos_heap.inc" > kos_make.inc
 
 echo include "kos_make.inc" > f_hello.asm
 t2fasm < hello.asm >> f_hello.asm
 fasm f_hello.asm hello.kex
 kpack hello.kex
+del kos_make.inc
 pause
