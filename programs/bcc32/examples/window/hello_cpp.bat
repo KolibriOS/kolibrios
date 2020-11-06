d54f7bb7b15579c8de5ddf83d81b5b036b9fcf29
maxcodehack <maxcodehack@kolibrios.org>
2020-11-06 14:59:59 +0000

    Remove hardcoded kos_make.inc, now it will generate via Makefile or hello_cpp.bat
    git-svn-id: svn://kolibrios.org@8145 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/bcc32/examples/window/hello_cpp.bat
+++ programs/bcc32/examples/window/hello_cpp.bat
@@ -1,4 +1,7 @@
 kos32-bcc -S -v- -R- -6 -a4 -O2 -Og -Oi -Ov -OS -k- -D__KOLIBRI__ -Iinclude hello.cpp
+
+echo STACKSIZE equ 102400 \n HEAPSIZE equ 102400 \n include "..\..\..\proc32.inc" \ninclude "$(INCLUDE)/kos_start.inc" \n include "$(INCLUDE)/kos_func.inc" \n include "$(INCLUDE)/kos_heap.inc" > kos_make.inc
+
 echo include "kos_make.inc" > f_hello.asm
 t2fasm < hello.asm >> f_hello.asm
 fasm f_hello.asm hello.kex
