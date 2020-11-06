22c8fa42729bb0ffeab0962cfb433d77b8b92d19
maxcodehack <maxcodehack@kolibrios.org>
2020-11-06 13:39:50 +0000

    Remove binarie
    git-svn-id: svn://kolibrios.org@8143 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/bcc32/examples/hello_cpp.bat
+++
@@ -1,6 +0,0 @@
-kos32-bcc -S -v- -R- -6 -a4 -O2 -Og -Oi -Ov -OS -k- -D__KOLIBRI__ -Iinclude hello.cpp
-echo include "kos_make.inc" > f_hello.asm
-t2fasm < hello.asm >> f_hello.asm
-fasm f_hello.asm hello.kex
-kpack hello.kex
-pause
