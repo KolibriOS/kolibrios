97f79f243eb357b458df0f0272d1bfdf8d6bbe1a
maxcodehack <maxcodehack@kolibrios.org>
2020-11-09 10:04:30 +0000

    Update in example BCC
    git-svn-id: svn://kolibrios.org@8166 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/bcc32/examples/window/window.cpp
+++ programs/bcc32/examples/window/window.cpp
@@ -4,27 +4,27 @@
 
 using namespace Kolibri;
 
-const char header[] = "Title";
-const char string[] = "Exit";
+const char header[] = "Colors";
 
 bool KolibriOnStart(TStartData &kos_start, TThreadData th)
 {
 	kos_start.Left = 10;
 	kos_start.Top = 40;
-	kos_start.Width = 150;
+	kos_start.Width = 135;
 	kos_start.Height = 80;
 	kos_start.WinData.WindowColor = 0xFFFFFF;
+	kos_start.WinData.WindowType = 0x34; // 0x34 - fixed, 0x33 - not fixed
 	kos_start.WinData.Title = header;
 	return true;
 }
 
 void KolibriOnPaint(void)
 {
-	DrawButton(1,0xB0B0B0, 10,10,50,20);
-	DrawText(15, 15, 0, string);
+	// If button have ID 1, this is close button
 	DrawButton(2,0xff0000, 10,40,50,20);
 	DrawButton(3,0x00ff00, 70,10,50,20);
 	DrawButton(4,0x0000ff, 70,40,50,20);
+	DrawButton(5,0xFFFE00, 10,10,50,20);
 }
 
 void KolibriOnButton(long id, TThreadData th)
@@ -38,14 +38,13 @@ void KolibriOnButton(long id, TThreadData th)
 		break;
 	case 4:
 		SetWindowCaption("Blue");
+		break;
+	case 5:
+		SetWindowCaption("Yellow");
 		//break;
 	};
 }
 /*
-bool KolibriOnClose(TThreadData th)
-{
-	return true;
-}
 int KolibriOnIdle(TThreadData th)
 {
 	return -1;
--- programs/bcc32/examples/window/window_cpp.bat
+++ programs/bcc32/examples/window/window_cpp.bat
@@ -1,14 +1,16 @@
-kos32-bcc -S -v- -R- -6 -a4 -O2 -Og -Oi -Ov -OS -k- -D__KOLIBRI__ -I..\..\..\bcc32\include window.cpp
+Set NAME=hello
+Set BCC_DIR=..\..\..\bcc32
+kos32-bcc -S -v- -R- -6 -a4 -O2 -Og -Oi -Ov -OS -k- -D__KOLIBRI__ -I..\..\..\bcc32\include %NAME%.cpp
 
 echo STACKSIZE equ 102400> kos_make.inc
 echo HEAPSIZE equ 102400>> kos_make.inc
-echo include "..\..\..\bcc32\include\kos_start.inc">> kos_make.inc
-echo include "..\..\..\bcc32\include\kos_func.inc">> kos_make.inc
-echo include "..\..\..\bcc32\include\kos_heap.inc">> kos_make.inc
+echo include "%BCC_DIR%\include\kos_start.inc">> kos_make.inc
+echo include "%BCC_DIR%\include\kos_func.inc">> kos_make.inc
+echo include "%BCC_DIR%\include\kos_heap.inc">> kos_make.inc
 
-echo include "kos_make.inc" > f_window.asm
-t2fasm < window.asm >> f_window.asm
-fasm f_window.asm window.kex
-if exist window.kex kpack window.kex
-if exist window.kex del kos_make.inc
+echo include "kos_make.inc" > f_%NAME%.asm
+t2fasm < %NAME%.asm >> f_%NAME%.asm
+fasm f_%NAME%.asm %NAME%.kex
+if exist %NAME%.kex kpack %NAME%.kex
+if exist %NAME%.kex del kos_make.inc
 pause
