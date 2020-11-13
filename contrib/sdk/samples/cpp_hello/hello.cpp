d4a127c1cb3943b31347d6118e1a4f954ebc9e8d
maxcodehack <maxcodehack@kolibrios.org>
2020-11-13 08:35:06 +0000

    C++ example: Makefile: add 'strip' console_obj.h: console.obj loading hello.cpp: add console.obj functions
    git-svn-id: svn://kolibrios.org@8182 a494cfbc-eb01-0410-851d-a64ba20cac60

--- contrib/sdk/samples/cpp_hello/hello.cpp
+++ contrib/sdk/samples/cpp_hello/hello.cpp
@@ -1,9 +1,21 @@
+// for 'cout'
 #include <iostream>
+
+// for 'console.obj' functions
+#include "console_obj.h"
 
 using namespace std;
 
 int main() 
 {
+	// load console.obj
+	load_console();
+	
+	con_set_title("C++ Console.obj example");
+	
+	cout << "Press any key...\n";
+	con_getch();
 	cout << "Hello, KolibriOS!\n";
+	con_set_title("Hello");
 	return 0;
 }
