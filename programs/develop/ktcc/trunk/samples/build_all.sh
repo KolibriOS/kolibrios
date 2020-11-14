25cf1adde0acd0101fdeb72f72d42d6101ac032a
maxcodehack <maxcodehack@kolibrios.org>
2020-11-14 17:57:14 +0000

    Fix 'dynamic.c 'autobuild (build_all.sh) Add 'console.c' to autobuild
    git-svn-id: svn://kolibrios.org@8195 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/ktcc/trunk/samples/build_all.sh
+++ programs/develop/ktcc/trunk/samples/build_all.sh
@@ -3,11 +3,13 @@
 ../tcc consoleio.c -lck -o consoleio
 ../tcc files.c -lck -o files
 ../tcc winbasics.c -lck -o winbasics
-../tcc dynamic.c -lhttp -linputbox -o dynamic
+../tcc dynamic.c -lck -lhttp -linputbox -o dynamic
 ../tcc load_coff.c -o load_coff -lck
 ../tcc clayer/msgbox.c  -lck -lmsgbox -o msgbox
 ../tcc graphics.c -lck -lgb -o graphics
-../tcc clayer/rasterworks.c -lck -lrasterworks -o rasterworks
-../tcc clayer/boxlib.c -lck -lbox -o boxlib
-../tcc clayer/libimg.c -lck -limg -o libimg
+../tcc clayer/rasterworks.c -lck -lrasterworks -o clayer/rasterworks
+../tcc clayer/boxlib.c -lck -lbox -o clayer/boxlib
+../tcc clayer/libimg.c -lck -limg -o clayer/libimg
+
+../tcc console/console.c -lck -limg -o console/console
 exit
