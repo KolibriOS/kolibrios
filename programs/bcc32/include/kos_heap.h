8b59b0bc76b7ad3f56cb06a716d782b49e090639
IgorA <IgorA@kolibrios.org>
2020-11-14 19:00:55 +0000

    fix Alloc function, clean code
    git-svn-id: svn://kolibrios.org@8196 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/bcc32/include/kos_heap.h
+++ programs/bcc32/include/kos_heap.h
@@ -2,31 +2,15 @@
 #define __KOLIBRI_HEAP_H_INCLUDED_
 
 #include "kolibri.h"
-#include "memheap.h"
 
 // Kolibri memory heap interface.
 
 namespace Kolibri   // All kolibri functions, types and data are nested in the (Kolibri) namespace.
 {
-	long _HeapInit()
-	{
-		return MemoryHeap::mem_Init();
-	}
-
-	void *Alloc(unsigned int size)
-	{
-		return MemoryHeap::mem_Alloc(size);
-	}
-
-	void *ReAlloc(void *mem, unsigned int size)
-	{
-		return MemoryHeap::mem_ReAlloc(size, mem);
-	}
-
-	void Free(void *mem)
-	{
-		MemoryHeap::mem_Free(mem);
-	}
+	long HeapInit();
+	void *Alloc(unsigned long int size);
+	void *ReAlloc(void *mem, unsigned long int size);
+	void Free(void *mem);
 }
 
 #endif  // ndef __KOLIBRI_HEAP_H_INCLUDED_
