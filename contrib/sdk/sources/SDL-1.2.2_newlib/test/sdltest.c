055b9d04c40c8a57bd2defe575844063b114b2bf
maxcodehack <maxcodehack@kolibrios.org>
2020-11-17 19:28:51 +0000

    №2
    git-svn-id: svn://kolibrios.org@8215 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ contrib/sdk/sources/SDL-1.2.2_newlib/test/sdltest.c
@@ -0,0 +1,28 @@
+#include "SDL.h"
+#include <stdlib.h>
+
+SDL_Surface* screen;
+static int done = 0;
+
+int main()
+{
+	SDL_Event event;
+	if(SDL_Init(SDL_INIT_VIDEO) < 0) exit(0);
+	atexit(SDL_Quit);
+	screen = SDL_SetVideoMode(320, 200, 8, SDL_SWSURFACE);
+	while(!done)
+		{
+		while(SDL_PollEvent(&event))
+		{
+			switch(event.type)
+			{
+				case SDL_KEYDOWN:
+				case SDL_QUIT:
+				 done=1;
+				 break;
+				default:
+				 break;
+			}
+		}
+	}
+}
