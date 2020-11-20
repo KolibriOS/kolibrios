b9e8e3f7350998a27574e3bb57fcce05ad544e0e
IgorA <IgorA@kolibrios.org>
2020-11-20 19:47:45 +0000

    add new version 'load_lib.mac', 'animage' use new macros 'load_lib.mac'
    git-svn-id: svn://kolibrios.org@8227 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/media/animage/trunk/animage.asm
+++ programs/media/animage/trunk/animage.asm
@@ -35,7 +35,7 @@
 ; design by golus
 
 use32
-org     0x0
+org     0
         db 'MENUET01'
         dd 1, START, IM_END, I_END
         dd stacktop, file_path, cur_dir_path
@@ -44,13 +44,13 @@ include '../../../config.inc' ;for nightbuild
 include '../../../macros.inc'
 include '../../../proc32.inc'
 include '../../../KOSfuncs.inc'
-include '../../../develop/libraries/box_lib/load_lib.mac'
+include '../../../load_lib.mac'
 include '../../../dll.inc'
 include '../../../develop/libraries/libs-dev/libio/libio.inc'
 include '../../../develop/libraries/libs-dev/libimg/libimg.inc'
 ;include '../../../debug.inc'
 
-@use_library_mem mem.Alloc,mem.Free,mem.ReAlloc,dll.Load
+@use_library mem.Alloc,mem.Free,mem.ReAlloc,dll.Load
 
 ;---------------------------------------------------------
 ; *** константы для интерфейса ***
