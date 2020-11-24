2e992f24ea75593ded879169d1125e926cb48aff
superturbocat2001 <superturbocat2001@kolibrios.org>
2020-11-24 15:13:42 +0000

    - Added TEAtool source files - Compressed kos32-tcc - Changed the paths of the output files in buid_all.sh
    git-svn-id: svn://kolibrios.org@8251 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/ktcc/trunk/samples/build_all.sh
+++ programs/develop/ktcc/trunk/samples/build_all.sh
@@ -1,15 +1,14 @@
 #SHS
-../tcc asm_ex.c -lck -o asm_ex
-../tcc consoleio.c -lck -o consoleio
-../tcc files.c -lck -o files
-../tcc winbasics.c -lck -o winbasics
-../tcc dynamic.c -lck -lhttp -linputbox -o dynamic
-../tcc load_coff.c -o load_coff -lck
-../tcc clayer/msgbox.c  -lck -lmsgbox -o msgbox
-../tcc graphics.c -lck -lgb -o graphics
-../tcc clayer/rasterworks.c -lck -lrasterworks -o clayer/rasterworks
-../tcc clayer/boxlib.c -lck -lbox -o clayer/boxlib
-../tcc clayer/libimg.c -lck -limg -o clayer/libimg
-
-../tcc console/console.c -lck -limg -o console/console
+../tcc asm_ex.c -lck -o /tmp0/1/asm_ex
+../tcc consoleio.c -lck -o /tmp0/1/consoleio
+../tcc files.c -lck -o /tmp0/1/files
+../tcc winbasics.c -lck -o /tmp0/1/winbasics
+../tcc dynamic.c -lck -lhttp -linputbox -o /tmp0/1/dynamic
+../tcc load_coff.c -o /tmp0/1/load_coff -lck
+../tcc clayer/msgbox.c  -lck -lmsgbox -o /tmp0/1/msgbox
+../tcc graphics.c -lck -lgb -o /tmp0/1/graphics
+../tcc clayer/rasterworks.c -lck -lrasterworks -o /tmp0/1/rasterworks
+../tcc clayer/boxlib.c -lck -lbox -o /tmp0/1/boxlib_ex
+../tcc clayer/libimg.c -lck -limg -o /tmp0/1/libimg_ex
+../tcc console/console.c -lck -limg -o /tmp0/1/console
 exit
