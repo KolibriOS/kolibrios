9fc78476141b4d86f2702869b643706441c2ca15
superturbocat2001 <superturbocat2001@kolibrios.org>
2020-12-01 19:57:08 +0000

    - Added full dark theme
    git-svn-id: svn://kolibrios.org@8294 a494cfbc-eb01-0410-851d-a64ba20cac60

--- skins/maxcodehack/blacky/default.dtp.asm
+++ skins/maxcodehack/blacky/default.dtp.asm
@@ -1,14 +1,14 @@
 struc system_colors
  {
-  .taskbar          dd 0x586786
-  .taskbar_text     dd 0xFEFEFE
-  .work_dark        dd 0xC5BDB9
-  .work_light       dd 0xECE9E5
-  .window_title     dd 0xFFFFFF
-  .work             dd 0xDDD7CF
-  .work_button      dd 0x7887A6
-  .work_button_text dd 0xFEFEFE
-  .work_text        dd 0x000000
-  .work_graph       dd 0x7E7E7E
+  .taskbar          dd 0xAAAAAA
+  .taskbar_text     dd 0xFFFFFF
+  .work_dark        dd 0x505050
+  .work_light       dd 0x828282
+  .window_title     dd 0xE3E3E3
+  .work             dd 0x626262
+  .work_button      dd 0x4E4E4E
+  .work_button_text dd 0xFFFFFF
+  .work_text        dd 0xF5EEee
+  .work_graph       dd 0x222222
  }
 struct system_colors
