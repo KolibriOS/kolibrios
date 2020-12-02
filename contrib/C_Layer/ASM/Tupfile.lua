cd81d0fc9a93900b56ca000b159cc6367c7c2b54
maxcodehack <maxcodehack@kolibrios.org>
2020-12-02 07:17:45 +0000

    fix autobuild
    git-svn-id: svn://kolibrios.org@8300 a494cfbc-eb01-0410-851d-a64ba20cac60

--- contrib/C_Layer/ASM/Tupfile.lua
+++ contrib/C_Layer/ASM/Tupfile.lua
@@ -1,2 +1,2 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
-tup.foreach_rule("*.asm", "fasm %f ../OBJ/%o " .. tup.getconfig("KPACK_CMD"), "../OBJ/%B.obj")
+tup.foreach_rule("*.asm", "fasm %f ../OBJ/%o ")
