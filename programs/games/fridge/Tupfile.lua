d83fcc8a3cc94bdf7b5a3f11184f594f9656bdb6
maxcodehack <maxcodehack@kolibrios.org>
2020-12-02 07:02:45 +0000

    Add 'fridge' autobuild
    git-svn-id: svn://kolibrios.org@8296 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ programs/games/fridge/Tupfile.lua
@@ -0,0 +1,11 @@
+if tup.getconfig("NO_GCC") ~= "" then return end
+HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../.." or tup.getconfig("HELPERDIR")
+tup.include(HELPERDIR .. "/use_gcc.lua")
+tup.include(HELPERDIR .. "/use_newlib.lua")
+
+-- C_Layer
+CFLAGS = CFLAGS .. " -I ../../../contrib/C_Layer/INCLUDE"
+LDFLAGS = LDFLAGS .. " ../../../contrib/C_Layer/OBJ/loadlibimg.obj"
+
+compile_gcc{"fridge.c"}
+link_gcc("fridge")
