eb251004eab56c2da22a8b9fe7c441004bb0cb16
maxcodehack <maxcodehack@kolibrios.org>
2020-12-03 10:31:14 +0000

    Fix 3
    git-svn-id: svn://kolibrios.org@8313 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/other/tte/Tupfile.lua
+++ programs/other/tte/Tupfile.lua
@@ -1,1 +1,1 @@
-tup.rule("", "/var/kolibri-repos/programs/develop/ktcc/trunk/bin/kos32-tcc tte.c getline.c -lck -o tte -I ../../programs/develop/ktcc/trunk/libc/include", "")
+tup.rule("tte.c", "/var/kolibri-repos/programs/develop/ktcc/trunk/bin/kos32-tcc %f getline.c -lck -o %o -I ../../programs/develop/ktcc/trunk/libc/include", "tte")
