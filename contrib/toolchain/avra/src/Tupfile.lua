64deb8c4ae711841d860d711d94895879c29d5af
Serhii Sakhno <sergei.sakhno@gmail.com>
2020-12-08 22:47:40 +0000

    avra: add to autobuild
    git-svn-id: svn://kolibrios.org@8348 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ contrib/toolchain/avra/src/Tupfile.lua
@@ -0,0 +1,7 @@
+if tup.getconfig("NO_GCC") ~= "" then return end
+HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../../../programs" or tup.getconfig("HELPERDIR")
+tup.include(HELPERDIR .. "/use_gcc.lua")
+tup.include(HELPERDIR .. "/use_newlib.lua")
+
+compile_gcc{"args.c", "avra.c", "coff.c", "device.c", "directiv.c", "expr.c", "file.c", "macro.c", "map.c", "mnemonic.c", "parser.c", "stdextra.c"}
+link_gcc("avra")
