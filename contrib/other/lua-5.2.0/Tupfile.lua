8a2618b31988beecf7d799a31eeb4604eb4f1ab9
maxcodehack <maxcodehack@kolibrios.org>
2020-12-11 17:32:59 +0000

    Fix LUA
    git-svn-id: svn://kolibrios.org@8371 a494cfbc-eb01-0410-851d-a64ba20cac60

--- contrib/other/lua-5.2.0/Tupfile.lua
+++ contrib/other/lua-5.2.0/Tupfile.lua
@@ -4,7 +4,7 @@ tup.include(HELPERDIR .. "/use_gcc.lua")
 tup.include(HELPERDIR .. "/use_newlib.lua")
 CFLAGS = CFLAGS .. " -UWIN32 -U_WIN32 -U__WIN32__ -DLUA_COMPAT_ALL -DLUA_ANSI"
 
-LDFLAGS = "-call_shared -S -nostdlib -T$(NEWLIB_BASE)/app-dynamic.lds --image-base 0"
+LDFLAGS = "-call_shared -S -nostdlib -T$(NEWLIB_BASE)/app-dynamic.lds --image-base 0 -L$(NEWLIB_LIB)"
 LDFLAGS = LDFLAGS .. " --disable-runtime-pseudo-reloc --subsystem native"
 compile_gcc{
   "lapi.c",
