9df944e10503c8fb590ec09c06492dfdcbeee535
maxcodehack <maxcodehack@kolibrios.org>
2020-12-13 11:46:08 +0000

    Add tte to autobuild
    git-svn-id: svn://kolibrios.org@8387 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ programs/other/tte/Tupfile.lua
@@ -0,0 +1,10 @@
+if tup.getconfig("NO_GCC") ~= "" then return end
+HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../../.." or tup.getconfig("HELPERDIR")
+tup.include(HELPERDIR .. "/use_gcc.lua")
+tup.include(HELPERDIR .. "/use_newlib.lua")
+
+CFLAGS = CFLAGS .. " -DGCC_BUILD"
+LDFLAGS = LDFLAGS .. " --subsystem native"
+
+compile_gcc{"tte.c", "notify.c", "getline.c"}
+link_gcc("tte")
