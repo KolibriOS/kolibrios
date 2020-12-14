36539c2d68a006ca4ce885bb2d0d4dd746ab52b2
maxcodehack <maxcodehack@kolibrios.org>
2020-12-14 10:33:48 +0000

    Upload uPDF port to newlib
    git-svn-id: svn://kolibrios.org@8401 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ contrib/media/updf_newlib/compile.sh
@@ -0,0 +1,28 @@
+cd SYSCALL/src
+make
+cd ../..
+
+cd fitz
+make 
+cd ..
+
+cd pdf
+make
+cd ..
+
+cd libopenjpeg
+make
+cd ..
+
+cd libjbig2dec
+make
+cd ..
+
+cd draw
+make
+cd ..
+
+cd apps
+make
+
+sleep 100
