561e5c5b1cc85ff9f8a6cf1f44a7c6ff455e84a9
superturbocat2001 <superturbocat2001@kolibrios.org>
2020-12-14 15:18:31 +0000

    - Fixed math.h
    git-svn-id: svn://kolibrios.org@8406 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/ktcc/trunk/libc/include/math.h
+++ programs/develop/ktcc/trunk/libc/include/math.h
@@ -1,3 +1,7 @@
+/* Copyright (C) 1999 DJ Delorie, see http://www.delorie.com/copyright.html for details */
+/* Copyright (C) 1998 DJ Delorie, see http://www.delorie.com/copyright.html for details */
+/* Copyright (C) 1995 DJ Delorie, see http://www.delorie.com/copyright.html for details */
+
 #ifndef _MATH_H
 #define _MATH_H
 
@@ -5,15 +9,20 @@ extern double   acos(double _x);
 extern double   asin(double _x);
 extern double   atan(double _x);
 extern double   atan2(double _y, double _x);
+extern float    ceilf(float);
 extern double   ceil(double _x);
 extern double   cos(double _x);
 extern double   cosh(double _x);
 extern double   exp(double _x);
+extern double   exp2(double _x);
 extern double   fabs(double _x);
+extern float    fabsf(float);
 extern double   floor(double _x);
+extern float    floorf(float);
 extern double   fmod(double _x, double _y);
 extern double   frexp(double _x, int *_pexp);
 extern double   ldexp(double _x, int _exp);
+extern double   remainder(double, double);
 extern double   log(double _y);
 extern double   log10(double _x);
 extern double   modf(double _x, double *_pint);
@@ -21,8 +30,11 @@ extern double   pow(double _x, double _y);
 extern double   sin(double _x);
 extern double   sinh(double _x);
 extern double   sqrt(double _x);
+extern float    sqrtf(float);
 extern double   tan(double _x);
 extern double   tanh(double _x);
+extern double   round(double x);
+extern long double roundl  (long double x);
 
 #define M_E             2.7182818284590452354
 #define M_LOG2E         1.4426950408889634074
