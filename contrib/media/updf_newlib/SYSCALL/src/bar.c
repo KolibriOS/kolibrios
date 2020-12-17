fa071aa312acd9bc595a2869fd351f1ec8869a8f
maxcodehack <maxcodehack@kolibrios.org>
2020-12-17 09:06:52 +0000

    Remove updf_newlib
    git-svn-id: svn://kolibrios.org@8437 a494cfbc-eb01-0410-851d-a64ba20cac60

--- contrib/media/updf_newlib/SYSCALL/src/bar.c
+++
@@ -1,9 +0,0 @@
-#include<menuet/os.h>
-
-void __menuet__bar(__u16 x1,__u16 y1,__u16 xsize,__u16 ysize,__u32 color)
-{
- __u32 a,b;
- a=(x1<<16)|xsize;
- b=(y1<<16)|ysize;
- __asm__ __volatile__("int $0x40"::"a"(13),"b"(a),"c"(b),"d"(color));
-}
--- contrib/media/updf_newlib/SYSCALL/src/param.c
+++
@@ -1,7 +0,0 @@
-#include<menuet/os.h>
-
-#define APP_PARAM_SZ		(1024)
-#define APP_PATH_SZ             (1024)
-
-char __menuet__app_param_area[APP_PARAM_SZ];
-char __menuet__app_path_area[APP_PATH_SZ];
