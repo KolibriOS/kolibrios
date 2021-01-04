2082d3c93f312e66d3c7da6a8ac3c5af95aabf66
maxcodehack <maxcodehack@kolibrios.org>
2021-01-04 17:50:18 +0000

    Fix Lua autobuild (now it isn't crash)
    git-svn-id: svn://kolibrios.org@8513 a494cfbc-eb01-0410-851d-a64ba20cac60

--- contrib/other/lua-5.2.0/Tupfile.lua
+++ contrib/other/lua-5.2.0/Tupfile.lua
@@ -2,9 +2,7 @@ if tup.getconfig("NO_GCC") ~= "" then return end
 HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../../../programs" or tup.getconfig("HELPERDIR")
 tup.include(HELPERDIR .. "/use_gcc.lua")
 tup.include(HELPERDIR .. "/use_newlib.lua")
-CFLAGS = CFLAGS .. " -UWIN32 -U_WIN32 -U__WIN32__ -DLUA_COMPAT_ALL -DLUA_ANSI"
-
-LDFLAGS = "-call_shared -S -nostdlib -T$(NEWLIB_BASE)/app-dynamic.lds --image-base 0 -L$(NEWLIB_LIB)"
+CFLAGS = "-fno-ident -O2 -fomit-frame-pointer -U__WIN32__ -U_Win32 -U_WIN32 -U__MINGW32__ -UWIN32  -DLUA_COMPAT_ALL -DLUA_ANSI"
 LDFLAGS = LDFLAGS .. " --disable-runtime-pseudo-reloc --subsystem native"
 compile_gcc{
   "lapi.c",
