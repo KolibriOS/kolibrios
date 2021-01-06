4ca88acc2557afd27512af0ea797f30f8054c686
maxcodehack <maxcodehack@kolibrios.org>
2021-01-06 13:31:58 +0000

    - SDL Demo - fix Tupfile.lua - Move sysxtree and mfar to outdated
    git-svn-id: svn://kolibrios.org@8524 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/demos/SDL/Tupfile.lua
+++ programs/demos/SDL/Tupfile.lua
@@ -2,12 +2,9 @@ if tup.getconfig("NO_GCC") ~= "" then return end
 HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../.." or tup.getconfig("HELPERDIR")
 tup.include(HELPERDIR .. "/use_gcc.lua")
 tup.include(HELPERDIR .. "/use_newlib.lua")
-tup.include(HELPERDIR .. "/use_sound.lua")
+tup.include(HELPERDIR .. "/use_sdl_newlib.lua")
 
 CFLAGS = CFLAGS .. " -std=c99"
-INCLUDES = INCLUDES .. " -I../../../contrib/sdk/sources/SDL-1.2.2_newlib/include"
-table.insert(LIBDEPS, "../../../contrib/sdk/lib/<libSDLn>")
-LIBS = "-lSDLn " .. LIBS
 
 -- Subsystem native
 LDFLAGS = LDFLAGS .. " --subsystem native"
