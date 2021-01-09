762358225bfe054deb871cbd5faa190fff902fac
maxcodehack <maxcodehack@kolibrios.org>
2021-01-09 17:27:55 +0000

    C--: clean tupfile.lua
    git-svn-id: svn://kolibrios.org@8531 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/cmm/Tupfile.lua
+++ programs/develop/cmm/Tupfile.lua
@@ -1,15 +1,10 @@
 if tup.getconfig("NO_GCC") ~= "" then return end
 HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../.." or tup.getconfig("HELPERDIR")
 tup.include(HELPERDIR .. "/use_gcc.lua")
+tup.include(HELPERDIR .. "/use_newlib.lua")
 
-SDK_DIR = "../../../contrib/sdk"
-LDFLAGS = LDFLAGS .. " -static -S -nostdlib -T" .. SDK_DIR .. "/sources/newlib/app-dynamic.lds --image-base 0 -L" .. SDK_DIR .. "/lib --stack 0x100000 "
-LIBS = LIBS .. " -lgcc -ldll -lc.dll "
-CFLAGS_cpp = CFLAGS_cpp .. " -Dstricmp=strcasecmp -Dstrnicmp=strncasecmp -fno-exceptions -D_KOS_ -U_Win32 -U_WIN32 -U__MINGW32__ -mno-ms-bitfields -Wno-write-strings "
-INCLUDES = INCLUDES .. " -I" .. SDK_DIR .. "/sources/newlib/libc/include "
-
-tup.append_table(LIBDEPS, {SDK_DIR .. "/lib/<libc.dll.a>"})
-tup.append_table(LIBDEPS, {SDK_DIR .. "/lib/<libdll.a>"})
+LDFLAGS = LDFLAGS .. " --stack 0x100000"
+CFLAGS = CFLAGS .. " -Dstricmp=strcasecmp -Dstrnicmp=strncasecmp -D_KOS_ -U_Win32 -U_WIN32 -U__MINGW32__"
 
 compile_gcc("*.cpp")
 link_gcc("cmm")
