f1cd74c5c8d8edb31215353e3a0690e89e21ac1d
superturbocat2001 <superturbocat2001@kolibrios.org>
2021-02-04 23:19:41 +0000

    - Restored revision with http_free fix
    git-svn-id: svn://kolibrios.org@8575 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/libraries/http/http.asm
+++ programs/develop/libraries/http/http.asm
@@ -201,8 +201,8 @@ proc HTTP_free identifier ;/////////////////////////////////////////////////////
         mcall   close, [ebp + http_msg.socket]
 
   .not_connected:
+        invoke  mem.free, [ebp + http_msg.content_ptr]
         invoke  mem.free, ebp
-
         popa
         ret
 
