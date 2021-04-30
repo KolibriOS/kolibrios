8797117c1d7cc7ca9d6c008e4b5f3e03f9a8b4ac
turbocat <turbocat@kolibrios.org>
2021-04-30 22:19:36 +0000

    Whois: - Reconfiguring autobuild
    git-svn-id: svn://kolibrios.org@8706 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/network/whois/Tupfile.lua
+++ programs/network/whois/Tupfile.lua
@@ -2,8 +2,8 @@ if tup.getconfig("NO_TCC") ~= "" then return end
 
 TCC="kos32-tcc"
 
-CFLAGS  = "-I../../develop/ktcc/trunk/kolibri-libc/include"
-LDFLAGS = "-nostdlib ../../develop/ktcc/trunk/kolibri-libc/crt0.o -L ../../develop/ktcc/trunk/kolibri-libc -L../../develop/ktcc/trunk/bin/lib " 
+CFLAGS  = "-I../../develop/libraries/kolibri-libc/include"
+LDFLAGS = "-nostdlib ../../develop/libraries/kolibri-libc/lib/crt0.o -L ../../develop/libraries/kolibri-libc/lib -L../../develop/ktcc/trunk/bin/lib " 
 LIBS = "-lnetwork -lc.obj"
 
 COMMAND=string.format("%s %s %s %s %s ", TCC, CFLAGS, "%f -o %o", LDFLAGS, LIBS)
