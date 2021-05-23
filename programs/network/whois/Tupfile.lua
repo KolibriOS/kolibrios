69eda3cf655d0b0cb7f8acbf2133ccbfb24ae661
turbocat <turbocat@kolibrios.org>
2021-05-23 15:44:58 +0000

    whois: - update build
    git-svn-id: svn://kolibrios.org@8731 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/network/whois/Tupfile.lua
+++ programs/network/whois/Tupfile.lua
@@ -4,7 +4,7 @@ TCC="kos32-tcc"
 
 CFLAGS  = "-I../../develop/libraries/kolibri-libc/include"
 LDFLAGS = "-nostdlib ../../develop/libraries/kolibri-libc/lib/crt0.o -L ../../develop/libraries/kolibri-libc/lib -L../../develop/ktcc/trunk/bin/lib " 
-LIBS = "-lnetwork -lc.obj"
+LIBS = "-ltcc -lnetwork -lc.obj"
 
 COMMAND=string.format("%s %s %s %s %s ", TCC, CFLAGS, "%f -o %o", LDFLAGS, LIBS)
 tup.rule("whois.c", COMMAND .. tup.getconfig("KPACK_CMD"), "whois")
