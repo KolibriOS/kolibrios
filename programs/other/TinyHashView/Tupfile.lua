4a09257a8fb511b4237d82830856bd0fd42a4de4
turbocat <turbocat@kolibrios.org>
2021-05-23 15:49:26 +0000

    TinyHashView: - update build
    git-svn-id: svn://kolibrios.org@8732 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/other/TinyHashView/Tupfile.lua
+++ programs/other/TinyHashView/Tupfile.lua
@@ -4,7 +4,7 @@ TCC="kos32-tcc"
 
 CFLAGS  = "-I../../develop/libraries/kolibri-libc/include -I../../develop/ktcc/trunk/libc/include"
 LDFLAGS = "-nostdlib ../../develop/libraries/kolibri-libc/lib/crt0.o -L../../develop/libraries/kolibri-libc/lib -L../../develop/ktcc/trunk/bin/lib"
-LIBS = "-lcryptal -ldialog -lbox -lc.obj"
+LIBS = "-ltcc -lcryptal -ldialog -lbox -lc.obj"
 
 COMMAND=string.format("%s %s %s %s %s ", TCC, CFLAGS, "%f -o %o", LDFLAGS, LIBS)
 tup.rule("thashview.c", COMMAND .. tup.getconfig("KPACK_CMD"), "thashview")
