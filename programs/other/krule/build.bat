fac22c1e7300f6e268d1aa921f2414f891a43333
ConLenov <ConLenov@kolibrios.org>
2021-05-31 10:08:20 +0000

    Fixed build.bat
    git-svn-id: svn://kolibrios.org@8755 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/other/krule/build.bat
+++ programs/other/krule/build.bat
@@ -1,9 +1,9 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"  
  
 @cl /c /O2 /nologo /GS- /GR- /fp:fast *.cpp
-@link /nologo /manifest:no /entry:crtStartUp /subsystem:native /base:0 /fixed /align:16 /nodefaultlib hello.obj *.obj
+@link /nologo /manifest:no /entry:crtStartUp /subsystem:native /base:0 /fixed /align:16 /nodefaultlib krule.obj *.obj
 @del krule
-@pe2kos hello.exe krule
-@del hello.exe
+@pe2kos krule.exe krule
+@del krule.exe
 @del *.obj
 pause
