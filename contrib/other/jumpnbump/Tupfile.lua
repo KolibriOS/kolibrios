3b35056c502d22f11cf201e5df49a7bafc0a2b29
turbocat <turbocat@kolibrios.org>
2021-06-05 21:20:22 +0000

    Jumpnbump: - Now uses zlib dynamically
    git-svn-id: svn://kolibrios.org@8769 a494cfbc-eb01-0410-851d-a64ba20cac60

--- contrib/other/jumpnbump/Tupfile.lua
+++ contrib/other/jumpnbump/Tupfile.lua
@@ -9,9 +9,9 @@ SDK_DIR = "../../sdk"
 CFLAGS = CFLAGS .. " -Dstricmp=strcasecmp -DZLIB_SUPPORT -Dstrnicmp=strncasecmp -DUSE_SDL -DNDEBUG -U__WIN32__ -U_Win32 -U_WIN32 -U__MINGW32__ -UWIN32"
 INCLUDES = INCLUDES .. " -I. -I " .. SDK_DIR .. "/sources/zlib"
 LDFLAGS = LDFLAGS .. " -Lsdl --subsystem native"
-LIBS = "-ls -lz " .. LIBS
+LIBS = "-ls -lz.dll " .. LIBS
 
-table.insert(LIBDEPS, SDK_DIR .. "/lib/<libz>")
+table.insert(LIBDEPS, SDK_DIR .. "/lib/<libz.dll.a>")
 table.insert(LIBDEPS, "sdl/<libs>")
 
 compile_gcc{"filter.c", "fireworks.c", "main.c", "menu.c", "stub.c"}
