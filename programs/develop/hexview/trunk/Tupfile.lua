b1b61a4f192fbf863af0af1905f00abad677ff0e
Doczom <Doczom@kolibrios.org>
2021-06-07 11:06:59 +0000

    delete hexview
    git-svn-id: svn://kolibrios.org@8775 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/hexview/trunk/Tupfile.lua
+++
@@ -1,3 +0,0 @@
-if tup.getconfig("NO_FASM") ~= "" then return end
-tup.rule("echo lang fix " .. ((tup.getconfig("LANG") == "") and "en" or tup.getconfig("LANG")) .. " > lang.inc", {"lang.inc"})
-tup.rule({"hexview.asm", extra_inputs = {"lang.inc"}}, "fasm %f %o " .. tup.getconfig("KPACK_CMD"), "hexview")
--- programs/develop/hexview/trunk/build_ru.bat
+++
@@ -1,5 +0,0 @@
-@erase lang.inc
-@echo lang fix ru >lang.inc
-@fasm hexview.asm hexview
-@erase lang.inc
-@pause
