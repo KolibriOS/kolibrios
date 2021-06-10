01b89a999e302b930a5eb3b073ff5e3b2a5b6e1a
turbocat <turbocat@kolibrios.org>
2021-06-10 14:56:44 +0000

    Whois: Fixed build
    git-svn-id: svn://kolibrios.org@8798 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/network/whois/Tupfile.lua
+++ programs/network/whois/Tupfile.lua
@@ -1,10 +1,10 @@
 if tup.getconfig("NO_TCC") ~= "" then return end
 
-TCC="kos32-tcc"
+TCC="../../develop/ktcc/trunk/bin/kos32-tcc"
 
-CFLAGS  = "-I../../develop/libraries/kolibri-libc/include"
-LDFLAGS = "-nostdlib ../../develop/libraries/kolibri-libc/lib/crt0.o -L ../../develop/libraries/kolibri-libc/lib -L../../develop/ktcc/trunk/bin/lib " 
+CFLAGS  = "-I../../develop/ktcc/trunk/libc.obj/include"
+
 LIBS = "-ltcc -lnetwork -lc.obj"
 
-COMMAND=string.format("%s %s %s %s %s ", TCC, CFLAGS, "%f -o %o", LDFLAGS, LIBS)
+COMMAND=string.format("%s %s %s %s", TCC, CFLAGS, "%f -o %o",  LIBS)
 tup.rule("whois.c", COMMAND .. tup.getconfig("KPACK_CMD"), "whois")
