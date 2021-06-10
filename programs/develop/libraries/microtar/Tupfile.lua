813dc9abc75e1d1825f795a999f68af837ebd91f
turbocat <turbocat@kolibrios.org>
2021-06-10 12:17:09 +0000

    Added microtar library(mtar.obj) (Library for working with tar archives)
    git-svn-id: svn://kolibrios.org@8791 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ programs/develop/libraries/microtar/Tupfile.lua
@@ -0,0 +1,8 @@
+if tup.getconfig("NO_GCC") ~= "" then return end
+HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../../../" or tup.getconfig("HELPERDIR")
+tup.include(HELPERDIR .. "/use_gcc.lua")
+
+CFLAGS = " -c -w -nostdinc -DGNUC -DMTAR_OBJ -Os -fno-common -fno-builtin -fno-leading-underscore -fno-pie" 
+INCLUDES = " -I../include -I../kolibri-libc/include"
+
+tup.rule("microtar.c", "kos32-gcc" .. CFLAGS .. INCLUDES .. " -o %o %f " .. tup.getconfig("KPACK_CMD"), "mtar.obj")
