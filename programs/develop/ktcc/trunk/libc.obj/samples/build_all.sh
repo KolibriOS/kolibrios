899135aa6d7eef422a3f688572d1209e53b6436e
turbocat <turbocat@kolibrios.org>
2021-06-10 23:57:15 +0000

    Added build_all.sh
    git-svn-id: svn://kolibrios.org@8820 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ programs/develop/ktcc/trunk/libc.obj/samples/build_all.sh
@@ -0,0 +1,18 @@
+#SHS
+mkdir /tmp0/1/tcc_samples
+/kolibrios/develop/tcc/tcc file_io.c -o /tmp0/1/tcc_samples/file_io -lc.obj
+/kolibrios/develop/tcc/tcc whois.c -o /tmp0/1/tcc_samples/whois -ltcc -lnetwork -lc.obj
+/kolibrios/develop/tcc/tcc stdio_test.c -o /tmp0/1/tcc_samples/stdio_test -lc.obj
+/kolibrios/develop/tcc/tcc basic_gui.c -o /tmp0/1/tcc_samples/basic_gui -ltcc -lc.obj
+/kolibrios/develop/tcc/tcc consoleio.c -o /tmp0/1/tcc_samples/consoleio -lc.obj
+/kolibrios/develop/tcc/tcc dir_example.c -o /tmp0/1/tcc_samples/ls_dir -lc.obj
+/kolibrios/develop/tcc/tcc http_tcp_demo.c -o /tmp0/1/tcc_samples/http_tcp_demo -ltcc -lnetwork -lc.obj
+/kolibrios/develop/tcc/tcc math_test.c -o /tmp0/1/tcc_samples/math_test -ltcc -lc.obj
+/kolibrios/develop/tcc/tcc string_test.c -o /tmp0/1/tcc_samples/string_test -ltcc -lc.obj
+/kolibrios/develop/tcc/tcc tmpdisk_work.c -o /tmp0/1/tcc_samples/tmpdisk_work -ltcc -lc.obj
+/kolibrios/develop/tcc/tcc clayer/boxlib.c -o /tmp0/1/tcc_samples/boxlib -ltcc -lbox -lc.obj
+/kolibrios/develop/tcc/tcc clayer/dialog.c -o /tmp0/1/tcc_samples/dialog -ltcc -ldialog -lc.obj
+cp /kolibrios/develop/tcc/samples/clayer/logo.png /tmp0/1/tcc_samples/logo.png
+/kolibrios/develop/tcc/tcc clayer/libimg.c -o /tmp0/1/tcc_samples/libimg -ltcc -limg -lc.obj
+/kolibrios/develop/tcc/tcc clayer/msgbox.c -o /tmp0/1/tcc_samples/msgbox -ltcc -lmsgbox -lc.obj
+/kolibrios/develop/tcc/tcc clayer/rasterworks.c -o /tmp0/1/tcc_samples/rasterworks -ltcc -lrasterworks -lc.obj
