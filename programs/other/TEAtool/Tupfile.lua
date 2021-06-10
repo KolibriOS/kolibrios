070372c1f319e617138601925b13e1193d65dbd2
turbocat <turbocat@kolibrios.org>
2021-06-10 16:23:00 +0000

    TEAtool: fix Tupefile.lua
    git-svn-id: svn://kolibrios.org@8810 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/other/TEAtool/Tupfile.lua
+++ programs/other/TEAtool/Tupfile.lua
@@ -3,7 +3,7 @@ if tup.getconfig("NO_TCC") ~= "" then return end
 TCC="kos32-tcc"
 
 CFLAGS  = "-I../../develop/ktcc/trunk/libc/include"
-LDFLAGS = "-nostdlib ../../develop/ktcc/trunk/bin/lib/start.o -L../../develop/ktcc/trunk/bin/lib"
+LDFLAGS = "-nostdlib ../../develop/ktcc/trunk/bin/lib/crt0.o -L../../develop/ktcc/trunk/bin/lib"
 LIBS = "-lck"
 
 COMMAND=string.format("%s %s %s %s %s ", TCC, CFLAGS, "%f -o %o", LDFLAGS, LIBS)
