7f24e45508d1b5b60a641c6c35c6f3a091962ae5
turbocat <turbocat@kolibrios.org>
2021-06-10 16:19:45 +0000

    TinyBasic: Fix build
    git-svn-id: svn://kolibrios.org@8808 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/tinybasic/Tupfile.lua
+++ programs/develop/tinybasic/Tupfile.lua
@@ -3,7 +3,7 @@ if tup.getconfig("NO_TCC") ~= "" then return end
 TCC="kos32-tcc"
 
 CFLAGS  = "-I../ktcc/trunk/libc/include"
-LDFLAGS = "-nostdlib ../ktcc/trunk/bin/lib/start.o -L../ktcc/trunk/bin/lib"
+LDFLAGS = "-nostdlib ../ktcc/trunk/bin/lib/crt0.o -L../ktcc/trunk/bin/lib"
 LIBS = "-lck"
 
 COMMAND=string.format("%s %s %s %s %s ", TCC, CFLAGS, "%f -o %o", LDFLAGS, LIBS)
