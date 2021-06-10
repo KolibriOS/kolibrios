2d49a6e150f0c91bc1c7869e5de7b1b2a5dd01a8
turbocat <turbocat@kolibrios.org>
2021-06-10 14:55:26 +0000

    Fixed mtar.obj build
    git-svn-id: svn://kolibrios.org@8797 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/develop/libraries/microtar/Tupfile.lua
+++ programs/develop/libraries/microtar/Tupfile.lua
@@ -3,6 +3,6 @@ HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../../../" or tup.getconfig(
 tup.include(HELPERDIR .. "/use_gcc.lua")
 
 CFLAGS = " -c -w -nostdinc -DGNUC -DMTAR_OBJ -Os -fno-common -fno-builtin -fno-leading-underscore -fno-pie" 
-INCLUDES = " -I../include -I../kolibri-libc/include"
+INCLUDES = " -I../include -I../../ktcc/trunk/libc.obj/include"
 
 tup.rule("microtar.c", "kos32-gcc" .. CFLAGS .. INCLUDES .. " -o %o %f " .. tup.getconfig("KPACK_CMD"), "mtar.obj")
