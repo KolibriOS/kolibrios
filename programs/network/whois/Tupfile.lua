5da66621320fca6b21347a50eb21395aa68eb917
turbocat <turbocat@kolibrios.org>
2021-06-10 16:15:44 +0000

    Whois: Fix tupefile.lua
    git-svn-id: svn://kolibrios.org@8807 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/network/whois/Tupfile.lua
+++ programs/network/whois/Tupfile.lua
@@ -7,5 +7,5 @@ LDFAGS  = "-nostdlib -L../../develop/ktcc/trunk/bin/lib ../../develop/ktcc/trunk
 
 LIBS = "-ltcc -lnetwork -lc.obj"
 
-COMMAND=string.format("%s %s %s %s", TCC, CFLAGS, "%f -o %o",  LIBS)
+COMMAND=string.format("%s %s %s %s %s", TCC, CFLAGS, LDFAGS, "%f -o %o",  LIBS)
 tup.rule("whois.c", COMMAND .. tup.getconfig("KPACK_CMD"), "whois")
