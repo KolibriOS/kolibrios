be0f2e1aefd759537adf24d9fb22191e4314fcda
turbocat <turbocat@kolibrios.org>
2021-06-10 16:27:34 +0000

    Fix build
    git-svn-id: svn://kolibrios.org@8811 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/other/TinyHashView/Tupfile.lua
+++ programs/other/TinyHashView/Tupfile.lua
@@ -1,8 +1,9 @@
 if tup.getconfig("NO_TCC") ~= "" then return end
 
-TCC="../../develop/ktcc/trunk/bin/kos32-tcc"
+TCC="kos32-tcc"
 CFLAGS  = "-I../../develop/ktcc/trunk/libc.obj/include"
+LDFLAGS = "-nostdlib ../../develop/ktcc/trunk/bin/lib/crt0.o -L../../develop/ktcc/trunk/bin/lib"
 LIBS = "-ltcc -lcryptal -ldialog -lbox -lc.obj"
 
-COMMAND=string.format("%s %s %s %s ", TCC, CFLAGS, "%f -o %o",  LIBS)
+COMMAND=string.format("%s %s %s %s %s ", TCC, CFLAGS, LDFLAGS, "%f -o %o",  LIBS)
 tup.rule("thashview.c", COMMAND .. tup.getconfig("KPACK_CMD"), "thashview")
