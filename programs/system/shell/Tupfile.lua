4c5c636e9a1c655a30434b7a0638095959a52f52
Rustem Gimadutdinov (rgimad) <rustem.gimadutdinov@gmail.com>
2021-06-12 21:38:18 +0000

    fix shell tupfile
    git-svn-id: svn://kolibrios.org@8828 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/system/shell/Tupfile.lua
+++ programs/system/shell/Tupfile.lua
@@ -4,6 +4,18 @@ TCC="kos32-tcc"
 CFLAGS  = "-I../../develop/ktcc/trunk/libc.obj/include"
 LDFLAGS = "-nostdlib ../../develop/ktcc/trunk/bin/lib/crt0.o -L../../develop/ktcc/trunk/bin/lib"
 
+if tup.getconfig("LANG") == "ru"
+then C_LANG = "LANG_RUS"
+else C_LANG = "LANG_ENG" -- this includes default case without config
+end
+
+if tup.getconfig("TUP_PLATFORM") == "win32"
+-- on win32 '#' is not a special character, but backslash and quotes would be printed as is
+then tup.rule('echo #define ' .. C_LANG .. ' 1 > %o', {"lang.h"})
+-- on unix '#' should be escaped
+else tup.rule('echo "#define" ' .. C_LANG .. ' 1 > %o', {"lang.h"})
+end
+
 LIBS = "-ltcc -lc.obj"
 COMMAND=string.format("%s %s %s %s %s", TCC, CFLAGS, LDFLAGS , "%f -o %o",  LIBS)
 
