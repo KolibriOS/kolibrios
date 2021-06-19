40a3bf76f0499279f532c24efae6c4a2bab271df
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2021-06-19 22:18:41 +0000

    clean skins
    git-svn-id: svn://kolibrios.org@8885 a494cfbc-eb01-0410-851d-a64ba20cac60

--- skins/_old/humanoid_OSX/Humanoid_OSX_dark_night/default.asm
+++ skins/_old/humanoid_OSX/Humanoid_OSX_dark_night/default.asm
@@ -4,18 +4,18 @@ include '../skin.inc'
 
 SKIN_PARAMS \
   height          = bmp_base.height,\     ; skin height
-  margins         = [5:1:72:1],\          ; margins [left:top:right:bottom]
+  margins         = [5:1:45:1],\          ; margins [left:top:right:bottom]
   colors active   = [binner=0x101010:\    ; border inner color
                      bouter=0x101010:\    ; border outer color
-                     bframe=0x3E3E3E],\   ; border frame color
+                     bframe=0x131313],\   ; border frame color
   colors inactive = [binner=0x101010:\    ; border inner color
                      bouter=0x101010:\    ; border outer color
-                     bframe=0x3E3E3E],\   ; border frame color
+                     bframe=0x131313],\   ; border frame color
   dtp             = 'default.dtp'          ; dtp colors
 
 SKIN_BUTTONS \
-  close    = [-20:3][16:16],\             ; buttons coordinates
-  minimize = [-40:3][16:16]               ; [left:top][width:height]
+  close    = [-20:4][16:16],\             ; buttons coordinates
+  minimize = [-40:4][16:16]               ; [left:top][width:height]
 
 SKIN_BITMAPS \
   left active   = bmp_left,\              ; skin bitmaps pointers
@@ -28,6 +28,6 @@ SKIN_BITMAPS \
 BITMAP bmp_left ,'left.bmp'               ; skin bitmaps
 BITMAP bmp_oper ,'oper.bmp'
 BITMAP bmp_base ,'base.bmp'
-BITMAP bmp_left1,'left.bmp'
+BITMAP bmp_left1,'left.bmp'               ;the same
 BITMAP bmp_oper1,'oper_1.bmp'
 BITMAP bmp_base1,'base_1.bmp'
--- skins/_old/neutral/pack_build.bat
+++
@@ -1,4 +0,0 @@
-kpack neutral_blue.skn neutral_blue.skn
-kpack neutral_green.skn neutral_green.skn
-kpack neutral_lilac.skn neutral_lilac.skn
-pause
