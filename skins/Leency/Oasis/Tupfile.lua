dcd94dca69d28c092ca6f8dc55ae782988619bd9
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2021-06-20 00:19:11 +0000

    sorry
    git-svn-id: svn://kolibrios.org@8894 a494cfbc-eb01-0410-851d-a64ba20cac60

--- skins/Leency/Oasis/Tupfile.lua
+++ skins/Leency/Oasis/Tupfile.lua
@@ -1,3 +1,3 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
 tup.rule("default.dtp.asm", 'fasm "%f" "%o"', "default.dtp")
-tup.rule({"default.asm", extra_inputs = {"default.dtp"}}, 'fasm "%f" "%o" ' .. tup.getconfig("KPACK_CMD"), "Aurum.skn")
+tup.rule({"default.asm", extra_inputs = {"default.dtp"}}, 'fasm "%f" "%o" ' .. tup.getconfig("KPACK_CMD"), "Oasis.skn")
