cef3da714c6e2b35d2146352764e8894448c2f3e
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2021-07-04 22:45:25 +0000

    Guzel_magic/default.dtp.asm: fix light/dark color and update button color for better contrast
    git-svn-id: svn://kolibrios.org@9001 a494cfbc-eb01-0410-851d-a64ba20cac60

--- skins/ConLenov/Guzel_magic/default.dtp.asm
+++ skins/ConLenov/Guzel_magic/default.dtp.asm
@@ -1,14 +1,14 @@
 struc system_colors
 {
-.frame            dd 0x006393
-.grab             dd 0x0077b0
-.work_dark        dd 0xA5A5A5
-.work_light       dd 0xE4E4E4
+.unused1          dd 0x006393
+.unused2          dd 0x0077b0
+.work_dark        dd 0xFDFFFF
+.work_light       dd 0x434141
 .title_text       dd 0xFFFFFF
 .work             dd 0x161925
-.work_button      dd 0x181B28
+.work_button      dd 0x184053
 .work_button_text dd 0xFFFFFF
 .work_text        dd 0xFFFFFF
-.work_graph       dd 0x0D1017
+.work_graph       dd 0xF2EFE8
 }
 struct system_colors
