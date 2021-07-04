7a6fe22066d5b82ebe8a502881f3b4330b7a0ffc
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2021-07-04 18:01:23 +0000

    skins: clean and update
    git-svn-id: svn://kolibrios.org@8998 a494cfbc-eb01-0410-851d-a64ba20cac60

--- skins/Leency/Luna/Tupfile.lua
+++
@@ -1,3 +0,0 @@
-if tup.getconfig("NO_FASM") ~= "" then return end
-tup.rule("default.dtp.asm", 'fasm "%f" "%o"', "default.dtp")
-tup.rule({"default.asm", extra_inputs = {"default.dtp"}}, 'fasm "%f" "%o" ' .. tup.getconfig("KPACK_CMD"), "Luna.skn")
--- skins/Leency/Luna/build.bat
+++
@@ -1,5 +0,0 @@
-fasm default.dtp.asm default.dtp
-fasm default.asm default.skn
-kpack default.skn
-del default.dtp
-pause
