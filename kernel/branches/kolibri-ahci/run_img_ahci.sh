a17d3b76532a4dcf8d0c20eca1f273d40b7cabe5
Rustem Gimadutdinov (rgimad) <rustem.gimadutdinov@gmail.com>
2021-08-30 10:59:18 +0000

    kolibri-ahci: - fix bug with identical names for all disks
    git-svn-id: svn://kolibrios.org@9168 a494cfbc-eb01-0410-851d-a64ba20cac60

--- kernel/branches/kolibri-ahci/run_img_ahci.sh
+++ kernel/branches/kolibri-ahci/run_img_ahci.sh
@@ -1,1 +1,1 @@
-qemu-system-i386 -m 256 -M q35 -fda kolibri_test2.img -boot a -vga vmware -net nic,model=rtl8139 -net user -soundhw ac97 -usb -usbdevice tablet -enable-kvm -drive file=fat:rw:.
+qemu-system-i386 -m 256 -M q35 -fda kolibri_test2.img -boot a -vga vmware -net nic,model=rtl8139 -net user -soundhw ac97 -usb -usbdevice tablet -enable-kvm -drive file=fat:rw:. -drive file=fat:rw:two_hard
