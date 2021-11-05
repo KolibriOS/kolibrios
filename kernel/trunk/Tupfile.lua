7454368c785758b66d94e3de95957eef65d7ab8c
Magomed Kostoev (mkostoevr) <mkostoevr@yandex.ru>
2021-11-05 00:28:48 +0000

    [KERNEL][TEST] Do not pack test kernel (test speedup)
    git-svn-id: svn://kolibrios.org@9252 a494cfbc-eb01-0410-851d-a64ba20cac60

--- kernel/trunk/Tupfile.lua
+++ kernel/trunk/Tupfile.lua
@@ -5,5 +5,5 @@ tup.rule({"bootbios.asm", extra_inputs = {"lang.inc"}}, "fasm %f %o -dextended_p
 tup.rule({"bootbios.asm", extra_inputs = {"lang.inc"}}, "fasm %f %o -dpretest_build=1", "bootbios.bin.pretest")
 tup.rule({"kernel.asm", extra_inputs = {"bootbios.bin", "lang.inc"}}, "fasm -m 65536 %f %o " .. tup.getconfig("KERPACK_CMD"), "kernel.mnt")
 tup.rule({"kernel.asm", extra_inputs = {"bootbios.bin.ext_loader", "lang.inc"}}, "fasm -m 65536 %f %o -dextended_primary_loader=1" .. tup.getconfig("KERPACK_CMD"), "kernel.mnt.ext_loader")
-tup.rule({"kernel.asm", extra_inputs = {"bootbios.bin.pretest", "lang.inc"}}, "fasm -m 65536 %f %o -dpretest_build=1 -ddebug_com_base=0xe9" .. tup.getconfig("KERPACK_CMD"), "kernel.mnt.pretest")
+tup.rule({"kernel.asm", extra_inputs = {"bootbios.bin.pretest", "lang.inc"}}, "fasm -m 65536 %f %o -dpretest_build=1 -ddebug_com_base=0xe9", "kernel.mnt.pretest")
 tup.rule({"kernel.asm", extra_inputs = {"lang.inc"}}, "fasm -m 65536 %f %o -dUEFI=1 -dextended_primary_loader=1", "kolibri.krn")
