5e64693dd292544f76e0aca8877cb0cdf0f8ea04
Doczom <Doczom@kolibrios.org>
2021-11-28 17:21:33 +0000

    fix header
    git-svn-id: svn://kolibrios.org@9345 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/games/pong/trunk/pong.asm
+++ programs/games/pong/trunk/pong.asm
@@ -21,14 +21,23 @@ CK_DOWN2 equ 80
 
 use32
 
-                org     0x0
+                ;org     0x0
+                ;
+                ;db      'MENUET00'              ; 8 byte id
+                ;dd      38                      ; required os
+                ;dd      START                   ; program start
+                ;dd      I_END                   ; program image size
+                ;dd      0x2000                  ; required amount of memory
+                ;dd      0x00000000              ; reserved=no extended header
+                db     'MENUET01'
+                dd     1
+                dd     START
+                dd     I_END
+                dd     MEM
+                dd     STACKTOP
+                dd     0
+                dd     0
 
-                db      'MENUET00'              ; 8 byte id
-                dd      38                      ; required os
-                dd      START                   ; program start
-                dd      I_END                   ; program image size
-                dd      0x2000                  ; required amount of memory
-                dd      0x00000000              ; reserved=no extended header
 
 include '../../../macros.inc'
 
@@ -36,7 +45,7 @@ START:
 
     mov  eax, 66
     mov  ebx, 1
-    mov  ecx, 1
+    mov  ecx, ebx
     mcall               ; установить режим сканкодов
 
     call draw_window
@@ -468,3 +477,6 @@ paddle:
 file "paddle.raw"
 
 I_END:
+        rb 256
+STACKTOP:
+MEM:
