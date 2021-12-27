fa4a2aeb72257bfdefeb4164d0cb087743c5d5a6
Kirill Lipatov (Leency) <lipatov.kiril@gmail.com>
2021-12-27 17:49:00 +0000

    trying to just kpack a file #6
    git-svn-id: svn://kolibrios.org@9496 a494cfbc-eb01-0410-851d-a64ba20cac60

--- data/common/File Managers/fNav/Tupfile.lua
+++ data/common/File Managers/fNav/Tupfile.lua
@@ -1,3 +1,3 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
-tup.rule("ABOUT.TXT", "fasm %f %o " .. tup.getconfig("KPACK_CMD"), "ABOUT.TXT")
+tup.rule("ABOUT.TXT", "kpack %f %o ", "ABOUT.TXT.KPACK")
 
