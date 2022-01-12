2ed999004a90f9b7fccf58f78a72ac4ce2cd87df
turbocat <turbocat@kolibrios.org>
2022-01-12 19:18:50 +0000

    PasswordGen: now uses autoimport(fixed tup).
    git-svn-id: svn://kolibrios.org@9627 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/other/PasswordGen/Tupfile.lua
+++ programs/other/PasswordGen/Tupfile.lua
@@ -1,10 +1,10 @@
 if tup.getconfig("NO_TCC") ~= "" then return end
+if tup.getconfig("HELPERDIR") == ""
+then
+  HELPERDIR = "../../../programs"
+end
+tup.include(HELPERDIR .. "/use_tcc.lua")
 
-TCC="kos32-tcc"
-CFLAGS  = "-I../../develop/ktcc/trunk/libc.obj/include"
-LDFLAGS = "-nostdlib ../../develop/ktcc/trunk/bin/lib/crt0.o -L../../develop/ktcc/trunk/bin/lib"
+LIBS = " -lbox_lib"
 
-LIBS = "-ltcc -lbox -lc.obj"
-COMMAND=string.format("%s %s %s %s %s", TCC, CFLAGS, LDFLAGS , "%f -o %o",  LIBS)
-
-tup.rule("passwordgen.c", COMMAND .. tup.getconfig("KPACK_CMD"), "passwordgen")
+link_tcc({"passwordgen.c"}, "passwordgen"); 
