22a11dad2bcb2309290e3b676e6452e8f4d6e4e5
turbocat <turbocat@kolibrios.org>
2022-01-18 19:18:24 +0000

    Kmatrix: Now uses auto import.
    git-svn-id: svn://kolibrios.org@9657 a494cfbc-eb01-0410-851d-a64ba20cac60

--- programs/demos/kmatrix/trunk/Tupfile.lua
+++ programs/demos/kmatrix/trunk/Tupfile.lua
@@ -1,10 +1,8 @@
 if tup.getconfig("NO_TCC") ~= "" then return end
+if tup.getconfig("HELPERDIR") == ""
+then
+  HELPERDIR = "../../../../programs"
+end
+tup.include(HELPERDIR .. "/use_tcc.lua")
 
-TCC="kos32-tcc"
-CFLAGS  = "-I../../../develop/ktcc/trunk/libc.obj/include"
-LDFLAGS = "-nostdlib ../../../develop/ktcc/trunk/bin/lib/crt0.o -L../../../develop/ktcc/trunk/bin/lib"
-LIBS = "-ltcc -lc.obj"
-
-COMMAND=string.format("%s %s %s %s %s", TCC, CFLAGS, LDFLAGS , "%f -o %o",  LIBS)
-
-tup.rule({"main.c"}, COMMAND .. tup.getconfig("KPACK_CMD"), "kmatrix")
+link_tcc("main.c", "kmatrix");
