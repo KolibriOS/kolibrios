038ab4898ce1d6b783893654da0ac0309bde727e
ConLenov <ConLenov@kolibrios.org>
2022-01-28 09:49:03 +0000

    Add GX_Red skin from ConLenov
    git-svn-id: svn://kolibrios.org@9672 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ skins/ConLenov/GX/GX_Red/default.dtp.asm
@@ -0,0 +1,14 @@
+struc system_colors
+{
+.frame            dd 0xfa1e4e
+.grab             dd 0xfa1e4e
+.work_dark        dd 0xA5A5A5
+.work_light       dd 0xE4E4E4
+.title_text       dd 0xFFFFFF
+.work             dd 0x121019
+.work_button      dd 0x2e273f
+.work_button_text dd 0xFFFFFF
+.work_text        dd 0xFFFFFF
+.work_graph       dd 0x4a3e65
+}
+struct system_colors
