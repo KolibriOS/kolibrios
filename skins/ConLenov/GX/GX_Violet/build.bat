6e21af17370e022c7e3c0a03adbc30593d34c4ab
ConLenov <ConLenov@kolibrios.org>
2022-03-29 12:15:58 +0000

    Add GX_Violet skin from ConLenov
    git-svn-id: svn://kolibrios.org@9752 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ skins/ConLenov/GX/GX_Violet/build.bat
@@ -0,0 +1,4 @@
+@del default.skn
+@fasm default.dtp.asm default.dtp
+@fasm GX_Violet.asm default.skn
+@del default.dtp
---
+++ skins/ConLenov/GX/GX_Violet/default.dtp.asm
@@ -0,0 +1,14 @@
+struc system_colors
+{
+.frame            dd 0xa970ff
+.grab             dd 0xa970ff
+.work_dark        dd 0xA5A5A5
+.work_light       dd 0xE4E4E4
+.title_text       dd 0xFFFFFF
+.work             dd 0x0e0c1d
+.work_button      dd 0x241f47
+.work_button_text dd 0xFFFFFF
+.work_text        dd 0xFFFFFF
+.work_graph       dd 0x3a3172
+}
+struct system_colors
