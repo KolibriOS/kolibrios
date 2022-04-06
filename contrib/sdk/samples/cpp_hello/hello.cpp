87d53e5e7465842932b6e57686c0db198646cade
maxcodehack <maxcodehack@kolibrios.org>
2022-04-06 15:24:16 +0000

    c/c++ samples refactoring
    git-svn-id: svn://kolibrios.org@9758 a494cfbc-eb01-0410-851d-a64ba20cac60

--- contrib/sdk/samples/cpp_hello/hello.cpp
+++ contrib/sdk/samples/cpp_hello/hello.cpp
@@ -1,21 +1,16 @@
-// for 'cout'
 #include <iostream>
-
-// for 'console.obj' functions
-#include <sys/kos_LoadConsole.h>
+#include <sys/kos_LoadConsole.h> // to load console.obj
 
 using namespace std;
 
-int main() 
-{
-	// load console.obj
+int main() {
 	load_console();
 	
 	con_set_title("C++ Console.obj example");
-	
 	cout << "Press any key...\n";
 	con_getch();
 	cout << "Hello, KolibriOS!\n";
 	con_set_title("Hello");
+	
 	return 0;
 }
--- contrib/sdk/samples/hello/hello.c
+++ contrib/sdk/samples/hello/hello.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
-
-    printf("Hello, world!\n");
-
-    return 0;
-};
+int main() {
+	printf("Hello, world!\n");
+	return 0;
+}
