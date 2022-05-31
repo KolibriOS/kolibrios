b222a617be187f00d2cdfa5fa337874b26b13276
ConLenov <ConLenov@kolibrios.org>
2022-05-31 14:29:36 +0000

    Add HotFox skin from ConLenov
    git-svn-id: svn://kolibrios.org@9824 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ skins/ConLenov/HotFox/build.bat
@@ -0,0 +1,4 @@
+@del default.skn
+@fasm default.dtp.asm default.dtp
+@fasm HotFox.asm default.skn
+@del default.dtp
---
+++ skins/ConLenov/HotFox/default.dtp.asm
@@ -0,0 +1,11 @@
+;SYSTEM COLORS (.DTP) - COMPILE WITH FASM
+frame            dd 0x5d7489
+grab             dd 0x5d7489
+grab_button      dd 0xDEE3EA
+grab_button_text dd 0xFFFFFF
+grab_text        dd 0xFFFFFF
+work             dd 0xf0f0f0
+work_button      dd 0x5d7489
+work_button_text dd 0xffffff
+work_text        dd 0x000000
+work_graph       dd 0xd7d7d7
