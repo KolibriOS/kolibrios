2d0631cba2714d378c2fd59b14039377fba9e9eb
ConLenov <ConLenov@kolibrios.org>
2022-07-12 11:22:08 +0000

    Add Arina skin from ConLenov
    git-svn-id: svn://kolibrios.org@9856 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ skins/ConLenov/Arina/build.bat
@@ -0,0 +1,4 @@
+@del default.skn
+@fasm default.dtp.asm default.dtp
+@fasm Arina.asm default.skn
+@del default.dtp
---
+++ skins/ConLenov/Arina/default.dtp.asm
@@ -0,0 +1,11 @@
+;SYSTEM COLORS (.DTP) - COMPILE WITH FASM
+unused1            	dd 0xFEFEFE
+unused2            	dd 0xFEEBFE
+grab_button      	dd 0xFF69F7
+grab_button_text 	dd 0xFFE4FE
+grab_text        	dd 0xFE34F2
+work             	dd 0xFFF4FF
+work_button      	dd 0xFEEEFE
+work_button_text 	dd 0xFF33F4
+work_text        	dd 0xFF33F4
+work_graph       	dd 0xF08CF0
