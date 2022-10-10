dbe746e58da7d37d8a0e00f4ca9ab788585378bc
turbocat <turbocat@kolibrios.org>
2022-10-10 22:06:24 +0000

    Adding VirtualBox driver to ISO: done.
    git-svn-id: svn://kolibrios.org@9881 a494cfbc-eb01-0410-851d-a64ba20cac60

--- data/Tupfile.lua
+++ data/Tupfile.lua
@@ -243,6 +243,7 @@ extra_files = {
  {"kolibrios/drivers/i915/", "common/drivers/i915/*"},
  {"kolibrios/drivers/test/", "common/drivers/test/*"},
  {"kolibrios/drivers/vmware/", "common/drivers/vmware/*"},
+ {"kolibrios/drivers/virtualbox/", "common/drivers/virtualbox/*"},
  {"kolibrios/KolibriNext/", "common/KolibriNext/*"},
  {"kolibrios/KolibriNext/settings/", "common/KolibriNext/settings/*"},
  {"kolibrios/lib/avcodec-56.dll", "common/lib/avcodec-56.dll"},
