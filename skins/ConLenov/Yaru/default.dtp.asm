4dd5556f1ddd22b487c76adf5ce888888d309399
ConLenov <ConLenov@kolibrios.org>
2024-03-04 08:20:33 +0000

    Add Yaru skin from ConLenov
    git-svn-id: svn://kolibrios.org@9982 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ skins/ConLenov/Yaru/default.dtp.asm
@@ -0,0 +1,14 @@
+struc system_colors
+{
+.frame            dd 0x323030
+.grab             dd 0x323030
+.work_dark        dd 0xA5A5A5
+.work_light       dd 0xE4E4E4
+.title_text       dd 0xFFFFFF
+.work             dd 0xFAFAFA
+.work_button      dd 0xFFFFFF
+.work_button_text dd 0x3D3D3D
+.work_text        dd 0x3D3D3D
+.work_graph       dd 0xC7C7C7
+}
+struct system_colors
