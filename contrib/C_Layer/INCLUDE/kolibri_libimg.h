84dcfbb2fea477e38ef9f239f1f27d5856b0b80d
Егор <148970641+Egor00f@users.noreply.github.com>
2024-03-23 12:59:27 +0500

    Update kolibri_libimg.h
    + Заменил #define на enum, вроде так посимпатичнее смотрится
    + Добавил больше информции

--- contrib/C_Layer/INCLUDE/kolibri_libimg.h
+++ contrib/C_Layer/INCLUDE/kolibri_libimg.h
@@ -9,125 +9,174 @@ modified by Egor00f
 #include <stddef.h>
 #include <stdbool.h>
 
-/// @breif Initialize library libimg
-/// @return -1 if unsuccessful
-extern int kolibri_libimg_init(void);
-
-#define _stdcall __attribute__((__stdcall__))
-
-//list of format id's
-#define LIBIMG_FORMAT_BMP       1
-#define LIBIMG_FORMAT_ICO       2
-#define LIBIMG_FORMAT_CUR       3
-#define LIBIMG_FORMAT_GIF       4
-#define LIBIMG_FORMAT_PNG       5
-#define LIBIMG_FORMAT_JPEG      6
-#define LIBIMG_FORMAT_TGA       7
-#define LIBIMG_FORMAT_PCX       8
-#define LIBIMG_FORMAT_XCF       9
-#define LIBIMG_FORMAT_TIFF      10
-#define LIBIMG_FORMAT_PNM       11
-#define LIBIMG_FORMAT_WBMP      12
-#define LIBIMG_FORMAT_XBM       13
-#define LIBIMG_FORMAT_Z80       14
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #pragma pack(push, 1)
 typedef struct {
-  /// @breif ((Width ROL 16) OR Height) XOR Data[0]
-  /// @note ignored so far
+  /// @brief ((Width ROL 16) OR Height) XOR Data[0]
+  /// @note Ignored so far
   uint32_t Checksum;
 
-  /// @breif Image width
+  /// @brief Image width
   uint32_t Width;
 
-  /// @breif image height
+  /// @brief Image height
   uint32_t Height;
 
   uint32_t Next;
 
   uint32_t Previous;
 
-  /// @breif one of Image.bppN
+  /// @brief One of Image.bppN
   uint32_t Type;
 
   /// @brief Image, array of colors
   uint32_t* Data;
 
-  /// @breif used iff Type eq Image.bpp1, Image.bpp2, Image.bpp4 or Image.bpp8i
+  /// @breif Used iff Type eq Image.bpp1, Image.bpp2, Image.bpp4 or Image.bpp8i
   uint32_t Palette;
 
   uint32_t Extended;
 
-  /// @breif bitfield
+  /// @brief Bitfield
   uint32_t Flags;
 
-  /// @breif used iff Image.IsAnimated is set in Flags
+  /// @brief Used iff Image.IsAnimated is set in Flags
   uint32_t Delay;
 
 } Image;
 #pragma pack(pop)
 
-#define IMAGE_BPP8i  1  // indexed
-#define IMAGE_BPP24  2
-#define IMAGE_BPP32  3
-#define IMAGE_BPP15  4
-#define IMAGE_BPP16  5
-#define IMAGE_BPP1   6
-#define IMAGE_BPP8g  7  // grayscale
-#define IMAGE_BPP2i  8
-#define IMAGE_BPP4i  9
-#define IMAGE_BPP8a 10 
+enum BPP
+{
+	/// @breif indexed
+	IMAGE_BPP8i = 1,
+	IMAGE_BPP24 = 2,
+	IMAGE_BPP32 = 3,
+	IMAGE_BPP15 = 4,
+	IMAGE_BPP16 = 5,
+	IMAGE_BPP1 = 6,
+	/// @breif grayscale
+	IMAGE_BPP8g = 7,
+	IMAGE_BPP2i = 8,
+	IMAGE_BPP4i = 9,
+	IMAGE_BPP8a = 10
+};
 
-// scale type
-#define LIBIMG_SCALE_NONE       0 
-#define LIBIMG_SCALE_INTEGER    1   
-#define LIBIMG_SCALE_TILE       2    
-#define LIBIMG_SCALE_STRETCH    3  
-#define LIBIMG_SCALE_FIT_BOTH   LIBIMG_SCALE_STRETCH
-#define LIBIMG_SCALE_FIT_MIN    4
-#define LIBIMG_SCALE_FIT_RECT   LIBIMG_SCALE_FIT_MIN
-#define LIBIMG_SCALE_FIT_WIDTH  5  
-#define LIBIMG_SCALE_FIT_HEIGHT 6 
-#define LIBIMG_SCALE_FIT_MAX    7     
+/// @brief list of format id's
+enum Formats
+{
+	LIBIMG_FORMAT_BMP = 1,
+	LIBIMG_FORMAT_ICO = 2,
+	LIBIMG_FORMAT_CUR = 3,
+	LIBIMG_FORMAT_GIF = 4,
+	LIBIMG_FORMAT_PNG = 5,
+	LIBIMG_FORMAT_JPEG = 6,
+	LIBIMG_FORMAT_TGA = 7,
+	LIBIMG_FORMAT_PCX = 8,
+	LIBIMG_FORMAT_XCF = 9,
+	LIBIMG_FORMAT_TIFF = 10,
+	LIBIMG_FORMAT_PNM = 11,
+	LIBIMG_FORMAT_WBMP = 12,
+	LIBIMG_FORMAT_XBM = 13,
+	LIBIMG_FORMAT_Z80 = 14
+};
 
-// interpolation algorithm
-#define LIBIMG_INTER_NONE       0     // use it with LIBIMG_SCALE_INTEGER, LIBIMG_SCALE_TILE, etc
-#define LIBIMG_INTER_BILINEAR   1
-#define LIBIMG_INTER_BICUBIC    2
-#define LIBIMG_INTER_LANCZOS    3
-#define LIBIMG_INTER_DEFAULT   LIBIMG_INTER_BILINEAR
+/// @brief List of scale type
+enum Scale
+{
+	LIBIMG_SCALE_NONE = 0,
+	LIBIMG_SCALE_INTEGER = 1,
+	LIBIMG_SCALE_TILE = 2,
+	LIBIMG_SCALE_STRETCH = 3,
+	LIBIMG_SCALE_FIT_BOTH = LIBIMG_SCALE_STRETCH,
+	LIBIMG_SCALE_FIT_MIN = 4,
+	LIBIMG_SCALE_FIT_RECT = LIBIMG_SCALE_FIT_MIN,
+	LIBIMG_SCALE_FIT_WIDTH = 5,
+	LIBIMG_SCALE_FIT_HEIGHT = 6,
+	LIBIMG_SCALE_FIT_MAX = 7
+};   
 
-//error codes
-#define LIBIMG_ERROR_OUT_OF_MEMORY      1
-#define LIBIMG_ERROR_FORMAT             2
-#define LIBIMG_ERROR_CONDITIONS         3
-#define LIBIMG_ERROR_BIT_DEPTH          4
-#define LIBIMG_ERROR_ENCODER            5
-#define LIBIMG_ERROR_SRC_TYPE           6
-#define LIBIMG_ERROR_SCALE              7
-#define LIBIMG_ERROR_INTER              8
-#define LIBIMG_ERROR_NOT_INPLEMENTED    9
-#define LIBIMG_ERROR_INVALID_INPUT      10
+/// @brief List of interpolation algorithms
+enum Inter
+{
+	/// @note use it with LIBIMG_SCALE_INTEGER, LIBIMG_SCALE_TILE, etc
+	LIBIMG_INTER_NONE = 0,
+	LIBIMG_INTER_BILINEAR = 1,
+	LIBIMG_INTER_BICUBIC = 2,
+	LIBIMG_INTER_LANCZOS = 3,
+	LIBIMG_INTER_DEFAULT = LIBIMG_INTER_BILINEAR
+};
 
-//encode flags (byte 0x02 of _common option)
-#define LIBIMG_ENCODE_STRICT_SPECIFIC   0x01
-#define LIBIMG_ENCODE_STRICT_BIT_DEPTH  0x02
-#define LIBIMG_ENCODE_DELETE_ALPHA      0x08
-#define LIBIMG_ENCODE_FLUSH_ALPHA       0x10
 
-#define FLIP_VERTICAL   0x01
-#define FLIP_HORIZONTAL 0x02
+/// @brief error codes
+enum Errors
+{
+	LIBIMG_ERROR_OUT_OF_MEMORY = 1,
+	LIBIMG_ERROR_FORMAT = 2,
+	LIBIMG_ERROR_CONDITIONS = 3,
+	LIBIMG_ERROR_BIT_DEPTH = 4,
+	LIBIMG_ERROR_ENCODER = 5,
+	LIBIMG_ERROR_SRC_TYPE = 6,
+	LIBIMG_ERROR_SCALE = 7,
+	LIBIMG_ERROR_INTER = 8,
+	LIBIMG_ERROR_NOT_INPLEMENTED = 9,
+	LIBIMG_ERROR_INVALID_INPUT = 10
+};
 
-#define ROTATE_90_CW    0x01
-#define ROTATE_180      0x02
-#define ROTATE_270_CW   0x03
-#define ROTATE_90_CCW   ROTATE_270_CW
-#define ROTATE_270_CCW  ROTATE_90_CW
+/// @breif encode flags (byte 0x02 of _common option)
+enum Encode
+{
+	LIBIMG_ENCODE_STRICT_SPECIFIC = 0x01,
+	LIBIMG_ENCODE_STRICT_BIT_DEPTH = 0x02,
+	LIBIMG_ENCODE_DELETE_ALPHA = 0x08,
+	LIBIMG_ENCODE_FLUSH_ALPHA = 0x10
+};
 
+enum Flip
+{
+	FLIP_VERTICAL = 0x01,
+	FLIP_HORIZONTAL = 0x02
+};
+
+enum Rotate
+{
+	ROTATE_90_CW = 0x01,
+	ROTATE_180 = 0x03,
+	ROTATE_270_CW = 0x03,
+	ROTATE_90_CCW = ROTATE_270_CW,
+	ROTATE_270_CCW = ROTATE_90_CW
+
+};
+
+/// @breif Initialize library libimg
+/// @return -1 if unsuccessful
+extern int kolibri_libimg_init(void);
+
+#define _stdcall __attribute__((__stdcall__))
+
+/// @brief Decode RAW data to Image data
+/// @param file_data
+/// @param size image size
+/// @param b_color 
+/// @return Pointer to image
 extern Image*   (*img_decode)(void* file_data, uint32_t size, uint32_t b_color) _stdcall;
 
+
+/// @brief 
+/// @param img
+/// @param length
+/// @param option Value from enum @link Encode
+/// @return
 extern Image*   (*img_encode)(Image* img, uint32_t length, uint32_t option) _stdcall;
 
+/// @brief Create image
+/// @param width New image width
+/// @param height New image height
+/// @param type value from enum @link Formats
+/// @return Pointer to image
 extern Image*   (*img_create)(uint32_t width, uint32_t height, uint32_t type) _stdcall;
 
 extern void     (*img_to_rgb2)(Image* img, void *rgb_data) _stdcall;
@@ -142,30 +191,61 @@ extern bool     (*img_rotate)(Image *img, uint32_t rotate) _stdcall;
 
 extern bool     (*img_rotate_layer)(Image* data, uint32_t rotate) _stdcall;
 
+/// @brief
 extern void     (*img_draw)(Image *img, uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint32_t xoff,  uint32_t yoff) _stdcall;
 
+/// @breif 
+/// @param img
+/// @return 
 extern int32_t  (*img_count)(Image *img) _stdcall;
 
+/// @brief Destroy image
+/// @param img Image to be deleted
+/// @return 
 extern bool     (*img_destroy)(Image *img) _stdcall;
 
 extern bool     (*img_destroy_layer)(Image* img) _stdcall;
 
-/// @breif "Blend" two images
+/// @brief "Blend" two images
 extern Image*   (*img_blend)(Image* dst, Image* src, uint32_t out_x, uint32_t out_y, uint32_t in_x, uint32_t in_y, uint32_t width, uint32_t height) _stdcall; 
 
+/// @brief Convert image to other format
+/// @param src Pointer to the image to convert
+/// @param dst
+/// @param dst_type
+/// @return Pointer to the converted image
 extern Image*   (*img_convert)(Image *src, Image *dst, uint32_t dst_type, uint32_t, uint32_t) _stdcall; 
 
 extern Image*   (*img_resize_data)(Image *src, uint32_t width, uint32_t height) _stdcall;
 
+/// @brief Resize image
+/// @param src Image to be resized
+/// @param crop_x 0
+/// @param crop_y 0
+/// @param crop_width Width image to be resized
+/// @param crop_height Height image to be resized
+/// @param dst NULL
+/// @param scale_type Scale type, value from enum @link Scale
+/// @param inter Interpolation algorithm, value from enum @link Inter. 
+/// @param new_width new image width
+/// @param new_height new image heidth
+/// @return Pointer to resized image
 extern Image*   (*img_scale)(Image* src, uint32_t crop_x, uint32_t crop_y, uint32_t crop_width, uint32_t crop_height, Image* dst, uint32_t scale_type, uint32_t inter, uint32_t new_width, uint32_t new_height) _stdcall;
+
+
+#ifdef __cplusplus
+}
+#endif
+
 
 /// @breif Fill the image with color
 /// @param img - image
 /// @param width width of the filled area
 /// @param height The height of the filled area
 /// @param color Сolor
-void img_fill_color(Image* img, uint32_t width, uint32_t height, uint32_t color) {
-    uint32_t i;
+void img_fill_color(Image* img, uint32_t width, uint32_t height, uint32_t color) 
+{
+	uint32_t i;
     for (i = 0; i < width*height; i++) {
         img->Data[i] = color;
     }
@@ -175,10 +255,13 @@ void img_fill_color(Image* img, uint32_t width, uint32_t height, uint32_t color)
 /// @param width width of the filled area
 /// @param height The height of the filled area
 /// @param color Сolor
-void img_fill_color(Image* img, uint32_t color) {
+void img_fill_color(Image* img, uint32_t color) 
+{
     for (uint32_t i = 0; i < img->Width * img->Height; i++) {
         img->Data[i] = color;
     }
 }
+
+#undef _stdcall
 
 #endif /* KOLIBRI_LIBIMG_H */
