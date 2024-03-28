5bf9207b4bd0311b0f494f874e5bb6cda24af2ae
ConLenov <ConLenov@kolibrios.org>
2024-03-28 15:26:15 +0000

    Add Yaru-dark skin from ConLenov
    git-svn-id: svn://kolibrios.org@10003 a494cfbc-eb01-0410-851d-a64ba20cac60

---
+++ skins/ConLenov/Yark-dark/build.bat
@@ -0,0 +1,4 @@
+@del default.skn
+@fasm default.dtp.asm default.dtp
+@fasm Yaru-dark.asm default.skn
+@del default.dtp
---
+++ skins/ConLenov/Yark-dark/default.dtp.asm
@@ -0,0 +1,14 @@
+struc system_colors
+{
+.frame            dd 0x323030
+.grab             dd 0x323030
+.work_dark        dd 0xA5A5A5
+.work_light       dd 0xE4E4E4
+.title_text       dd 0xFFFFFF
+.work             dd 0x2C2C2C
+.work_button      dd 0x373737
+.work_button_text dd 0xF7F7F7
+.work_text        dd 0xF7F7F7
+.work_graph       dd 0x181818
+}
+struct system_colors
