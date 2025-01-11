c8e9e5a2a711debdccbfb14d4ad6a24ea6340a36
Ivan Baravy <dunkaist@gmail.com>
2025-01-11 20:32:29 +0000

    Fix autobuild for dino

--- programs/games/dino/Tupfile.lua
+++ programs/games/dino/Tupfile.lua
@@ -6,17 +6,17 @@ end
 tup.include(HELPERDIR .. "/use_tcc.lua")
 
 SRCS = {
-    "cloud.c"
-    "game_over_panel.c"
-    "horizon.c"
-    "main.c"
-    "obstacle.c"
-    "trex.c"
-    "distance_meter.c"
-    "graphics.c"
-    "horizon_line.c"
-    "misc.c"
-    "runner.c"
+    "cloud.c",
+    "game_over_panel.c",
+    "horizon.c",
+    "main.c",
+    "obstacle.c",
+    "trex.c",
+    "distance_meter.c",
+    "graphics.c",
+    "horizon_line.c",
+    "misc.c",
+    "runner.c",
     "ulist.c"
 }
 
