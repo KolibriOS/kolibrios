3f7e1051c4c928513a5a6effa64420fc3b1792d9
leency <lipatov.kiril@gmail.com>
2025-03-22 02:14:55 +0200

    revert coff build to a common build

--- programs/cmm/eolite/Tupfile.lua
+++ programs/cmm/eolite/Tupfile.lua
@@ -1,16 +1,8 @@
 if tup.getconfig("NO_CMM") ~= "" then return end
-if tup.getconfig("NO_TCC") ~= "" then return end
-HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../.." or tup.getconfig("HELPERDIR")
-tup.include(HELPERDIR .. "/use_tcc.lua")
 
 if tup.getconfig("LANG") == "ru_RU"
 then C_LANG = "LANG_RUS"
 else C_LANG = "LANG_ENG" -- this includes default case without config
 end
 
-LFLAGS = LFLAGS .. " -stack=20480"
-LIBS += " ../../develop/ktcc/trunk/bin/lib/tiny.o -nostdlib -lbox_lib -lini -limg"
-
-tup.rule("Eolite.c", "c-- -coff /D=" .. C_LANG .. " /OPATH=%o %f", "%B.obj")
-tup.rule("Eolite.obj", "objconv -felf32 %f %o", "%B.o")
-link_tcc("Eolite.o", "%B.com")
+tup.rule("Eolite.c", "c-- /D=$(C_LANG) %f" .. tup.getconfig("KPACK_CMD"), "Eolite.com")
