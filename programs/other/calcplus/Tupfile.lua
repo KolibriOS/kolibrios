64aa9fafdff83a15a1ad4346761f25323d6a36f5
Kiril Lipatov <lipatov.kiril@gmail.com>
2025-03-31 22:56:58 +0200

    Update programs/other/calcplus/Tupfile.lua
    fix

--- programs/other/calcplus/Tupfile.lua
+++ programs/other/calcplus/Tupfile.lua
@@ -1,2 +1,2 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
-tup.rule("pipet.asm", FASM .. " -dlang=" .. tup.getconfig("LANG") .. " %f %o" .. tup.getconfig("KPACK_CMD"), "%B")
+tup.rule("calcplus.asm", "fasm %f %o -dlang=" .. tup.getconfig("LANG") .. " %f %o" .. tup.getconfig("KPACK_CMD"), "%B")
