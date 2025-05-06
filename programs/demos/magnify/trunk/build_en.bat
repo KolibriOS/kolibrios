b8d7f4a022a496517bd7657c495b5b63f557839f
Burer <burer@kolibrios.org>
2025-05-06 16:46:27 +0300

    [Apps/Magnify] Delete build.bat files

--- programs/demos/magnify/trunk/build_en.bat
+++
@@ -1,6 +0,0 @@
-@erase lang.inc
-@echo lang fix en_US >lang.inc
-@fasm magnify.asm magnify
-@kpack magnify
-@erase lang.inc
-@pause
--- programs/demos/magnify/trunk/build_es.bat
+++
@@ -1,6 +0,0 @@
-@erase lang.inc
-@echo lang fix es_ES >lang.inc
-@fasm magnify.asm magnify
-@kpack magnify
-@erase lang.inc
-@pause
