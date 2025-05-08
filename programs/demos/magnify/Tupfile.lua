ff60bedc093a9c584c619b1351836f02e9e7dae6
Burer <burer@kolibrios.org>
2025-05-08 15:11:04 +0300

    [Apps/Magnify] Fix to previous

--- programs/demos/magnify/Tupfile.lua
+++ programs/demos/magnify/Tupfile.lua
@@ -1,4 +1,4 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
-HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../../.." or tup.getconfig("HELPERDIR")
+HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../.." or tup.getconfig("HELPERDIR")
 tup.include(HELPERDIR .. "/use_fasm.lua")
 tup.rule("magnify.asm", FASM .. " -dlang=" .. tup.getconfig("LANG") .. " %f %o" .. tup.getconfig("KPACK_CMD"), "%B")
