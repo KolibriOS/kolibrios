5049d6b0b469f1041970658d887ebf00af9e2a71
Burer <burer@kolibrios.org>
2025-12-24 19:10:11 +0200

    Piano refacoring, part 1

--- programs/media/piano/Tupfile.lua
+++ programs/media/piano/Tupfile.lua
@@ -1,7 +1,4 @@
 if tup.getconfig("NO_FASM") ~= "" then return end
 HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../.." or tup.getconfig("HELPERDIR")
 tup.include(HELPERDIR .. "/use_fasm.lua")
-add_include(tup.getvariantdir())
-
-tup.rule("echo lang fix " .. ((tup.getconfig("LANG") == "") and "en_US" or tup.getconfig("LANG")) .. " > %o", {"lang.inc"})
-tup.rule({"piano.asm", extra_inputs = {"lang.inc"}}, FASM .. " %f %o " .. tup.getconfig("KPACK_CMD"), "piano")
+tup.rule("piano.asm", FASM .. " -dlang=" .. tup.getconfig("LANG") .. " %f %o" .. tup.getconfig("KPACK_CMD"), "%B")
