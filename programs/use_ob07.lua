864210679ca760546293962a8e50805ba315a9bf
Max Logaev <maxlogaev@proton.me>
2026-01-30 14:43:02 +0300

    oberon07: Option `-nochk a` by default
    This option disables runtime checks to reduce binary file size.
    
    Signed-off-by: Max Logaev <maxlogaev@proton.me>

--- programs/use_ob07.lua
+++ programs/use_ob07.lua
@@ -1,5 +1,5 @@
 OB07 = tup.getcwd() .. "/develop/oberon07/compiler"
-OB07_FLAGS = "-stk 1 "
+OB07_FLAGS = "-stk 1 -nochk a"
 
 function build_ob07(input, output)
   tup.rule(input, OB07 .. " %f kosexe -out %o " .. OB07_FLAGS .. " " .. tup.getconfig("KPACK_CMD"),  output)
