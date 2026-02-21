6f2a947deb6add78781762d3d8cbad7bf1ef0e3d
Max Logaev <maxlogaev@proton.me>
2026-02-22 00:09:14 +0300

    Remove broken unused and unfinished apps and libs
    - TEAtool: A useless utility for encryption with the TEA algorithm,
      written for fun by me during my school years;
    - GameHack: not a working prototype of a program like Cheat Engine;
    - microtar: port of a portable library with a reduced functionality
      for working with TAR in C that is not used
    
    Signed-off-by: Max Logaev <maxlogaev@proton.me>

--- programs/develop/libraries/microtar/Tupfile.lua
+++
@@ -1,8 +0,0 @@
-if tup.getconfig("NO_GCC") ~= "" then return end
-HELPERDIR = (tup.getconfig("HELPERDIR") == "") and "../../../" or tup.getconfig("HELPERDIR")
-tup.include(HELPERDIR .. "/use_gcc.lua")
-
-CFLAGS = " -c -w -nostdinc -DGNUC -DMTAR_OBJ -Os -fno-common -fno-builtin -fno-leading-underscore -fno-pie" 
-INCLUDES = " -I../include -I../../ktcc/trunk/libc.obj/include"
-
-tup.rule("microtar.c", "kos32-gcc" .. CFLAGS .. INCLUDES .. " -o %o %f " .. tup.getconfig("KPACK_CMD"), "mtar.obj")
